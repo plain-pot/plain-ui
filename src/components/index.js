f18fb247be92ecaa835d2e5359a690f5553fb7fb
韦胜健 <martsforever@aliyun.com>
2019-04-18 17:35:38 +0800

    button group

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import icon from './pl-icon'
 import scroll from './pl-scroll'
 import navTab from './pl-nav-tab'
 import button from './pl-button'
+import buttonGroup from './pl-button-group'
 import loading from './pl-loading'
 
 export default {
@@ -10,4 +11,5 @@ export default {
     navTab,
     button,
     loading,
+    buttonGroup,
 }
---
+++ src/components/pl-button-group.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="pl-button-group" :class="classes">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'pl-button-group',
+        props: {
+            shape: {
+                type: String,
+                default: 'none',
+            },
+            size: {
+                type: String,
+                default: 'default',
+            },
+            color: {
+                type: String,
+                default: 'primary',
+            },
+            vertical: {
+                type: Boolean
+            },
+        },
+        computed: {
+            classes() {
+                return [
+                    {
+                        [`pl-button-group-shape-${this.shape}`]: !!this.shape,
+                        [`pl-button-group-color-${this.color}`]: !!this.color,
+                        [`pl-button-group-size-${this.size}`]: !!this.size,
+                    },
+                    `pl-button-group-${!!this.vertical ? 'vertical' : 'horizontal'}`,
+                ];
+            },
+        },
+    };
+</script>
