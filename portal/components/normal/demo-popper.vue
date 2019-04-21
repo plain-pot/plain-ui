e7153f4530f59936ab6377c2afcae166f5dafd00
martsforever <martsforever@aliyun.com>
2019-04-21 11:16:50 +0800

    ready to import scroll

--- portal/components/normal/demo-popper.vue
+++ portal/components/normal/demo-popper.vue
@@ -2,12 +2,9 @@
     <div class="demo-popper">
         <im-demo-row title="基本用法">
             <im-button label="show dom" @click="test1(1)" ref="button1"/>
-            <im-input v-model="text"/>
-            <div>
+            <div class="box-wrapper">
                 <div class="box" ref="popper1">
                     <im-icon icon="pad-star"/>
-                    <im-button label="close" @click="!!popper[1] && popper[1].hide()"/>
-                    <im-input v-model="text"/>
                 </div>
             </div>
         </im-demo-row>
@@ -28,7 +25,7 @@
             </im-button-group>
             <im-button :active="arrow" label="arrow" @click="arrow = !arrow"/>
             <im-button @click="test2(2)" ref="button2" label="toggle"/>
-            <div>
+            <div class="box-wrapper">
                 <div class="box" ref="popper2">
                     <im-icon icon="pad-star"/>
                     <im-button label="close" @click="!!popper[2] && popper[2].hide()"/>
@@ -36,6 +33,40 @@
             </div>
         </im-demo-row>
 
+        <im-demo-row title="测试内容绑定">
+            <im-button label="show dom" @click="test1(3)" ref="button3"/>
+            <im-input v-model="text"/>
+            <div class="box-wrapper">
+                <div class="box" ref="popper3">
+                    <im-icon icon="pad-star"/>
+                    <im-button label="close" @click="!!popper[3] && popper[3].hide()"/>
+                    <im-input v-model="text"/>
+                </div>
+            </div>
+        </im-demo-row>
+
+        <im-demo-row title="设置间距">
+            <im-button label="show dom" @click="test4(4)" ref="button4"/>
+            <div class="box-wrapper">
+                <div class="box" ref="popper4">
+                    <im-icon icon="pad-star"/>
+                </div>
+            </div>
+        </im-demo-row>
+        <im-demo-row title="动画">
+            <im-button label="drop" @click="test5(5)" ref="button5"/>
+            <im-button label="scale" @click="test5(6)" ref="button6"/>
+            <div class="box-wrapper">
+                <div class="box" ref="popper5">
+                    <im-icon icon="pad-star"/>
+                </div>
+            </div>
+            <div class="box-wrapper">
+                <div class="box" ref="popper6">
+                    <im-icon icon="pad-star"/>
+                </div>
+            </div>
+        </im-demo-row>
     </div>
 </template>
 
@@ -64,11 +95,27 @@
                 if (!this.popper[num]) this.popper[num] = await this.$plain.$popper.newPopper({
                     reference: this.$refs[`button${num}`],
                     popper: this.$refs[`popper${num}`],
-                    arrow:this.arrow,
+                    arrow: this.arrow,
                 })
                 this.popper[num].p_direction = this.direction
                 this.popper[num].p_align = this.align
                 this.popper[num].p_arrow = this.arrow
+                this.popper[num].p_show ? this.popper[num].hide() : this.popper[num].show()
+            },
+            async test4(num) {
+                if (!this.popper[num]) this.popper[num] = await this.$plain.$popper.newPopper({
+                    reference: this.$refs[`button${num}`],
+                    popper: this.$refs[`popper${num}`],
+                    offset: 30,
+                })
+                this.popper[num].p_show ? this.popper[num].hide() : this.popper[num].show()
+            },
+            async test5(num) {
+                if (!this.popper[num]) this.popper[num] = await this.$plain.$popper.newPopper({
+                    reference: this.$refs[`button${num}`],
+                    popper: this.$refs[`popper${num}`],
+                    animate: num === 5 ? 'drop' : 'scale',
+                })
                 this.popper[num].p_show ? this.popper[num].hide() : this.popper[num].show()
             },
         }
@@ -84,7 +131,9 @@
             padding: 12px;
             border-radius: 4px;
         }
-
+        .box-wrapper {
+            display: none;
+        }
     }
 
 </style>
