1393d67954d5e4b6b3851fa42d1f2bdb219704df
韦胜健 <martsforever@aliyun.com>
2019-05-14 16:51:45 +0800

    adjust popover and select service

--- portal/home/app-menu.vue
+++ portal/home/app-menu.vue
@@ -129,7 +129,6 @@
                             {title: '$select选择服务', icon: 'pad-message', page: '/service/demo-select-service', complete: true},
                             {title: '$dialog对话框服务', icon: 'pl-window', page: '/service/demo-dialog-service', complete: true},
                             {title: '$notice通知', icon: 'pad-bell', page: '/service/demo-notice-service', complete: true},
-                            {title: '$popper下拉菜单', icon: 'pl-menu', page: '/service/demo-popper-service', complete: false},
                         ],
                     },
                     {
