d308c38e4113a0547d514a04bad547377ba6d098
martsforever <martsforever@aliyun.com>
2019-05-16 23:27:21 +0800

    adjust pagination styles

--- portal/components/nav/demo-nav-page-1.vue
+++ portal/components/nav/demo-nav-page-1.vue
@@ -2,8 +2,8 @@
     <div class="demo-nav-page-1">
         <im-demo-row title="页面一">
             <im-button label="页面二参数：" type="line"/>
-            <im-input placeholder/>
-            <im-button label="打开页面二" @click="nav.push({path:''})"/>
+            <im-input placeholder v-model="msg"/>
+            <im-button label="打开页面二" @click="nav.push({path:'/nav/demo-nav-page-2',param:{msg:msg}})"/>
         </im-demo-row>
     </div>
 </template>
@@ -13,6 +13,11 @@
         name: "demo-nav-page-1",
         props: {
             nav: {},
+        },
+        data() {
+            return {
+                msg: null,
+            }
         },
     }
 </script>
---
+++ portal/components/nav/demo-nav-page-2.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="demo-nav-page-2">
+        <im-demo-row>
+            <im-button label="返回" @click="nav.back()"/>
+        </im-demo-row>
+        <im-demo-row title="第一页参数">
+            <im-button :label="`页面参数:${param.msg}`" type="line"/>
+        </im-demo-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "demo-nav-page-2",
+        props: {
+            nav: {},
+            param: {},
+        },
+    }
+</script>
+
+<style lang="scss">
+
+</style>
