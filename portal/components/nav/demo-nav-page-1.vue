2ef75e04c48954a5bf4073b92c223f959a4463e5
韦胜健 <martsforever@aliyun.com>
2019-05-16 21:06:07 +0800

    adjust demo nav page1

--- portal/components/nav/demo-nav-page-1.vue
+++ portal/components/nav/demo-nav-page-1.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="demo-nav-page-1">
-        demo-nav-page-1
+        <im-demo-row title="页面一">
+            <im-button label="页面二参数：" type="line"/>
+            <im-input placeholder/>
+            <im-button label="打开页面二" @click="nav.push({path:''})"/>
+        </im-demo-row>
     </div>
 </template>
 
 <script>
     export default {
-        name: "demo-nav-page-1"
+        name: "demo-nav-page-1",
+        props: {
+            nav: {},
+        },
     }
 </script>
 
