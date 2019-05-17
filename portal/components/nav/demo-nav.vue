2099e743145c0da87a7e94d9d5a1215a75107393
韦胜健 <martsforever@aliyun.com>
2019-05-17 10:53:36 +0800

    adjust demo nav

--- portal/components/nav/demo-nav.vue
+++ portal/components/nav/demo-nav.vue
@@ -24,7 +24,7 @@
             return {
                 menus: [
                     {id: 'a', title: '页面一（缓存）', path: '/nav/demo-nav-page-1.vue', param: {msg: 'page1'}},
-                    {id: 1, title: '页面一（不缓存）', path: '/nav/test/demo-nav-1.vue', param: {msg: 'page1'}, storage: false},
+                    {id: 1, title: '页面一（不缓存）', path: '/nav/demo-nav-page-1.vue', param: {msg: 'page1'}, storage: false},
                     {id: 2, title: 'Icon图标', path: '/normal/demo-icon', param: {msg: 'icon'}},
                     {id: 3, title: 'Button按钮', path: '/normal/demo-button', param: {msg: 'button'}},
                     {id: 4, title: 'Input输入框', path: '/normal/demo-input', param: {msg: 'input'}},
