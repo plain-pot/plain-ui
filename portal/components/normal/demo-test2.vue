dcadae40a95022b43472948719e0da3518e916d1
martsforever <martsforever@aliyun.com>
2019-05-20 23:05:52 +0800

    test computed

--- portal/components/normal/demo-test2.vue
+++ portal/components/normal/demo-test2.vue
@@ -1,17 +1,35 @@
 <template>
     <div class="demo-test2">
-        demo-test2
+        <im-button label="demo-test2" @click="test"/>
     </div>
 </template>
 
 <script>
 
 
-
     export default {
         name: "demo-test2",
+        data() {
+            return {
+                a: 111,
+                b: 222,
+            }
+        },
+        computed: {
+            ret() {
+                console.log('calc ret')
+                return this.a + '' + this.b
+            },
+        },
         mounted() {
         },
+        methods: {
+            test() {
+                console.log(this.ret)
+                console.log(this.ret)
+                console.log(this.ret)
+            },
+        }
     }
 </script>
 
