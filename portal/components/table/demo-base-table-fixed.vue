f96c35397ee65dc48ee810a9600c219dd7bbccf1
韦胜健 <martsforever@aliyun.com>
2019-05-24 18:21:29 +0800

    complete table fixed

--- portal/components/table/demo-base-table-fixed.vue
+++ portal/components/table/demo-base-table-fixed.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="demo-table-table-fixed">
-        <im-base-table :data="data" id="trainno">
+        <im-base-table :data="simpleData" id="trainno">
             <im-tc-column title="类型" field="type"/>
             <im-tc-column title="车次,fixed=left" field="trainno" fixed="left"/>
             <im-tc-column title="车次,fixed=right" field="trainno" fixed="right"/>
@@ -18,13 +18,45 @@
             <im-tc-column title="高级软卧下票价" field="pricegr2"/>
         </im-base-table>
 
-        <!--
+        <im-base-table :data="simpleData" id="trainno">
+            <im-tc-column title="类型" field="type"/>
+            <im-tc-column title="车次,fixed=left" field="trainno" fixed="left"/>
+            <im-tc-column title="车次,fixed=right" field="trainno"/>
+            <im-tc-column title="用时" field="costtime" fixed="left"/>
+            <im-tc-column title="出发站" field="station"/>
+            <im-tc-column title="到达站" field="endstation"/>
+            <im-tc-column title="出发时间" field="departuretime"/>
+            <im-tc-column title="到达时间" field="arrivaltime"/>
+            <im-tc-column title="顺序" field="sequenceno"/>
+            <im-tc-column title="距离" field="distance"/>
+            <im-tc-column title="是否终点" field="isend"/>
+            <im-tc-column title="商务座票价" field="pricesw"/>
+            <im-tc-column title="特等座票价" field="pricetd"/>
+            <im-tc-column title="高级软卧上票价" field="pricegr1"/>
+            <im-tc-column title="高级软卧下票价" field="pricegr2"/>
+        </im-base-table>
+        <im-base-table :data="simpleData" id="trainno">
+            <im-tc-column title="类型" field="type"/>
+            <im-tc-column title="车次,fixed=left" field="trainno" fixed="right"/>
+            <im-tc-column title="车次,fixed=right" field="trainno"/>
+            <im-tc-column title="用时" field="costtime" fixed="right"/>
+            <im-tc-column title="出发站" field="station"/>
+            <im-tc-column title="到达站" field="endstation"/>
+            <im-tc-column title="出发时间" field="departuretime"/>
+            <im-tc-column title="到达时间" field="arrivaltime"/>
+            <im-tc-column title="顺序" field="sequenceno"/>
+            <im-tc-column title="距离" field="distance"/>
+            <im-tc-column title="是否终点" field="isend"/>
+            <im-tc-column title="商务座票价" field="pricesw"/>
+            <im-tc-column title="特等座票价" field="pricetd"/>
+            <im-tc-column title="高级软卧上票价" field="pricegr1"/>
+            <im-tc-column title="高级软卧下票价" field="pricegr2"/>
+        </im-base-table>
+
         <im-base-table :data="data" id="trainno">
             <im-tc-column title="类型" field="type"/>
             <im-tc-column title="车次,fixed=left" field="trainno" fixed="left"/>
             <im-tc-column title="车次,fixed=right" field="trainno" fixed="right"/>
-            <im-tc-column title="用时" field="costtime" fixed="left"/>
-            <im-tc-column title="出发站" field="station" fixed="right"/>
 
             <im-tc-group title="组1" fixed="left">
                 <im-tc-column title="到达站" field="endstation"/>
@@ -44,18 +76,18 @@
             <im-tc-column title="高级软卧上票价" field="pricegr1"/>
             <im-tc-column title="高级软卧下票价" field="pricegr2"/>
         </im-base-table>
-        -->
     </div>
 </template>
 
 <script>
 
-    import {TableData} from "../../data";
+    import {TableData, SimpleTableData} from "../../data";
 
     export default {
         name: "demo-table-table-fixed",
         data() {
             return {
+                simpleData: SimpleTableData,
                 data: TableData,
             }
         },
@@ -66,5 +98,9 @@
     .demo-table-table-fixed {
         box-sizing: border-box;
         padding: 16px;
+
+        .pl-base-table {
+            margin-bottom: 16px;
+        }
     }
 </style>
