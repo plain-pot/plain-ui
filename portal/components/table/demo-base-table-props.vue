94ce31bcd0d0758d4605efc48feee8819ffeb1d7
韦胜健 <martsforever@aliyun.com>
2019-05-27 09:50:24 +0800

    fixed bug in body item calc body height

--- portal/components/table/demo-base-table-props.vue
+++ portal/components/table/demo-base-table-props.vue
@@ -100,6 +100,26 @@
                 <im-tc-column title="高级软卧下票价" field="pricegr2"/>
             </im-base-table>
         </im-demo-row>
+        <im-demo-row title="空表格">
+            <im-base-table :data="[]" id="trainno">
+                <im-tc-column title="类型" field="type"/>
+                <im-tc-column title="车次,fixed=left" field="trainno" fixed="left"/>
+                <im-tc-column title="车次,fixed=right" field="trainno" fixed="right"/>
+                <im-tc-column title="用时" field="costtime"/>
+                <im-tc-column title="出发站" field="station"/>
+                <im-tc-column title="到达站" field="endstation"/>
+                <im-tc-column title="出发时间" field="departuretime"/>
+                <im-tc-column title="到达时间" field="arrivaltime"/>
+                <im-tc-column title="顺序" field="sequenceno"/>
+                <im-tc-column title="用时" field="costtime"/>
+                <im-tc-column title="距离" field="distance"/>
+                <im-tc-column title="是否终点" field="isend"/>
+                <im-tc-column title="商务座票价" field="pricesw"/>
+                <im-tc-column title="特等座票价" field="pricetd"/>
+                <im-tc-column title="高级软卧上票价" field="pricegr1"/>
+                <im-tc-column title="高级软卧下票价" field="pricegr2"/>
+            </im-base-table>
+        </im-demo-row>
     </div>
 </template>
 
