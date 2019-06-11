4b3de187c8f163763f42ea2f422a38368712a09d
韦胜健 <martsforever@aliyun.com>
2019-06-11 15:49:46 +0800

    adjust demo layout

--- portal/components/normal/demo-layout.vue
+++ portal/components/normal/demo-layout.vue
@@ -108,54 +108,53 @@
                     </im-col>
                 </im-row>
             </im-demo-row-item>
-            <im-demo-row title="当使用flex布局时，可以使用order属性排序">
-                <im-row type="flex" align="middle" gutter="16">
-                    <im-col span="2" order="1">
-                        <div class="ct-cls">col-2</div>
-                    </im-col>
-                    <im-col span="4" order="2">
-                        <div class="ct-cls">col-4</div>
-                    </im-col>
-                    <im-col span="6" order="4">
-                        <div class="ct-cls">col-6</div>
-                    </im-col>
-                    <im-col span="8" order="3">
-                        <div class="ct-cls">col-8</div>
-                    </im-col>
-                    <im-col span="4" order="5">
-                        <div class="ct-cls">col-4</div>
-                    </im-col>
-                </im-row>
-            </im-demo-row>
+        </im-demo-row>
+        <im-demo-row title="当使用flex布局时，可以使用order属性排序">
+            <im-row type="flex" align="middle" gutter="16">
+                <im-col span="2" order="1">
+                    <div class="ct-cls">col-2</div>
+                </im-col>
+                <im-col span="4" order="2">
+                    <div class="ct-cls">col-4</div>
+                </im-col>
+                <im-col span="6" order="4">
+                    <div class="ct-cls">col-6</div>
+                </im-col>
+                <im-col span="8" order="3">
+                    <div class="ct-cls">col-8</div>
+                </im-col>
+                <im-col span="4" order="5">
+                    <div class="ct-cls">col-4</div>
+                </im-col>
+            </im-row>
+        </im-demo-row>
 
-            <im-demo-row title="当使用浮动布局时，可以使用pull以及push属性排序">
-                <im-row gutter="16">
-                    <im-col span="2" push="10" :xs="xs[0]">
-                        <div class="ct-cls">col-2</div>
-                    </im-col>
-                    <im-col span="4" pull="2" :xs="xs[1]">
-                        <div class="ct-cls">col-4</div>
-                    </im-col>
-                    <im-col span="6" pull="2" :xs="xs[2]">
-                        <div class="ct-cls">col-6</div>
-                    </im-col>
-                </im-row>
-            </im-demo-row>
+        <im-demo-row title="当使用浮动布局时，可以使用pull以及push属性排序">
+            <im-row gutter="16">
+                <im-col span="2" push="10" :xs="xs[0]">
+                    <div class="ct-cls">col-2</div>
+                </im-col>
+                <im-col span="4" pull="2" :xs="xs[1]">
+                    <div class="ct-cls">col-4</div>
+                </im-col>
+                <im-col span="6" pull="2" :xs="xs[2]">
+                    <div class="ct-cls">col-6</div>
+                </im-col>
+            </im-row>
+        </im-demo-row>
 
-            <im-demo-row title="响应式布局，可以用chrome把客户端变成移动端模式，当为普通模式时，分别占2,4,6格，当为移动端时，分别占4,8,12格">
-                <im-row gutter="16">
-                    <im-col span="2" :xs="xs[0]">
-                        <div class="ct-cls">col-2</div>
-                    </im-col>
-                    <im-col span="4" :xs="xs[1]">
-                        <div class="ct-cls">col-4</div>
-                    </im-col>
-                    <im-col span="6" :xs="xs[2]">
-                        <div class="ct-cls">col-6</div>
-                    </im-col>
-                </im-row>
-            </im-demo-row>
-
+        <im-demo-row title="响应式布局，可以用chrome把客户端变成移动端模式，当为普通模式时，分别占2,4,6格，当为移动端时，分别占4,8,12格">
+            <im-row gutter="16">
+                <im-col span="2" :xs="xs[0]">
+                    <div class="ct-cls">col-2</div>
+                </im-col>
+                <im-col span="4" :xs="xs[1]">
+                    <div class="ct-cls">col-4</div>
+                </im-col>
+                <im-col span="6" :xs="xs[2]">
+                    <div class="ct-cls">col-6</div>
+                </im-col>
+            </im-row>
         </im-demo-row>
     </div>
 </template>
