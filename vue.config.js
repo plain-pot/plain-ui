2c5251fb885f1e5035edc57c4ae7ea90bd16c5b7
韦胜健 <martsforever@aliyun.com>
2019-06-19 14:16:09 +0800

    default not use analysis plugin

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 const arg = $utils.decodeArgv()
 /*添加插件*/
 const plugins = []
-!!arg.analysis && plugins.push(new BundleAnalyzerPlugin({analyzerPort: '9999'}))                        //如果命令行参数中存在analysis，则启用webpack-bundle-analysis插件分析打包数据
+// !!arg.analysis && plugins.push(new BundleAnalyzerPlugin({analyzerPort: '9999'}))                        //如果命令行参数中存在analysis，则启用webpack-bundle-analysis插件分析打包数据
 const isProduction = !!arg.production
 
 const option = {
