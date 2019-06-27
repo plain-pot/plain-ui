a94ddb61f03d9aecd805523cce1876c02a0fb588
韦胜健 <martsforever@aliyun.com>
2019-06-27 21:04:05 +0800

    准备实现按钮动态控制隐藏以及动态禁用

--- portal/components/plain-table/components/plain-table-buttons.js
+++ portal/components/plain-table/components/plain-table-buttons.js
@@ -9,16 +9,19 @@ export const StandardButtons = {
     insertButton: {
         type: 'insert',                         //按钮类型，受安全性控制
         order: 100,                             //排序
-        disabled: false,                        //是否禁用
         display: true,                          //是否显示
-        label: '新建',                           //文本
+        label: '新建',                          //文本
         icon: 'pad-plus-circle',                //图标
         needRow: false,                         //是否需要选择一行数据
         confirm: false,                         //是否需要确认对话框
         inner: false,                           //在内部还是外部
         confirmMessage: null,                   //确认对话框信息
         key: 'ctrl+n',
-
+        disabled() {
+            if (!this.option) return true
+            if (!!this.option.parentOption && (!this.option.parentOption.selectDataRow || !!this.option.parentOption.loading)) return true
+            return false
+        },
         handler() {
             this.newInsert()
         },
