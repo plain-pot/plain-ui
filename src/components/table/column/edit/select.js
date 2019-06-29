469d16b37505ad54e33e98652a57e708651f8758
martsforever <martsforever@aliyun.com>
2019-06-29 22:23:09 +0800

    select column add before and after hook

--- src/components/table/column/edit/select.js
+++ src/components/table/column/edit/select.js
@@ -28,10 +28,20 @@ export default {
                         ...that.publicProps,
                         value: editRow[col.field],
                         required,
+                        before: (val) => that.pl_before({row, editRow, col, colIndex, require, prop, required}, val),
+                        after: (val) => that.pl_after({row, editRow, col, colIndex, require, prop, required}, val),
                     },
                     on: {input: (val) => this.$set(editRow, col.field, val),}
                 }}/>)
             },
         }
+    },
+    methods: {
+        async pl_before(dataRow, val) {
+            if (!!this.prop.before) await this.prop.before(dataRow, val)
+        },
+        async pl_after(dataRow, val) {
+            if (!!this.prop.before) await this.prop.after(dataRow, val)
+        },
     },
 }
