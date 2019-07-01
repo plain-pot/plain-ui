5ce646cf932cf20889671397ed5daa242adbb590
韦胜健 <martsforever@aliyun.com>
2019-07-01 16:37:11 +0800

    demo field add object

--- portal/pages/plain-table/demo-plain-table-field.vue
+++ portal/pages/plain-table/demo-plain-table-field.vue
@@ -9,7 +9,7 @@
             <pl-tc-select field="sel" title="下拉选择框" :prop="{data:selectData,labelKey:'name',valueKey:'val'}"/>
             <!--            <pl-tc-ov field="ov" title="选项值选择框"/>-->
             <pl-tc-color field="color" title="颜色选择框"/>
-            <!--            <pl-tc-obj field="obj" title="对象选择框"/>-->
+            <pl-tc-object field="objName" title="对象选择框" :prop="{option:tableAddressOption,map:{obj:'id',objName:'name'}}"/>
             <!--            <pl-tc-address field="address" title="地址选择框"/>-->
             <pl-tc-radio field="radio" title="单选复选框"/>
             <pl-tc-toggle field="toggle" title="开关选择框"/>
@@ -53,7 +53,27 @@
                     {name: '北京', val: 'beijing'},
                     {name: '上海', val: 'shanghai'},
                     {name: '广州', val: 'guangzhou'},
-                ]
+                ],
+                tableAddressOption: new PlainOption({
+                    context: this,
+                    module: 'address',
+                    filters: [
+                        {field: 'deep', value: '0'}
+                    ],
+                    renderFunc(h) {
+                        return (
+                            <div>
+                                <pl-tc-input field="name" title="地址名称" required width="100px" tooltip/>
+                                <pl-tc-input field="code" title="地址代码" width="100px" required/>
+                                <pl-tc-column field="parentName" title="父级地址名称" width="100px" tooltip/>
+                                <pl-tc-column field="parentCode" title="父级地址代码" width="100px"/>
+                                <pl-tc-input field="longitude" title="经度" width="100px"/>
+                                <pl-tc-input field="latitude" title="纬度" width="100px"/>
+                                <pl-tc-input field="deep" title="地址级别" width="100px"/>
+                            </div>
+                        )
+                    }
+                }),
             }
         },
     }
