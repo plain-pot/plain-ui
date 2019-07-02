82be71e5820fa0140d8d32eba47c3122d2e7073d
韦胜健 <martsforever@aliyun.com>
2019-07-02 12:14:14 +0800

    td的index变为rowIndex+colIndex会导致继续新建速度大大变慢，这里为了优化体验，先把这个撤回去

--- src/components/table/pl-base-table-row.vue
+++ src/components/table/pl-base-table-row.vue
@@ -1,6 +1,6 @@
 <template>
     <tr class="pl-base-table-row" :class="classes">
-        <td v-for="(col,colIndex) in bodyColumns" :key="rowData.index+'-'+col.id">
+        <td v-for="(col,colIndex) in bodyColumns" :key="col.id">
             <pl-base-table-cell
                     :is-fixed="fixed === col.fixed"
                     :height="bodyRowHeight"
