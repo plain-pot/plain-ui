09fcf019e575ec9105661569ac8af8557ff2b29b
martsforever <martsforever@aliyun.com>
2019-07-06 10:44:11 +0800

    adjust styles

--- src/index.js
+++ src/index.js
@@ -70,7 +70,7 @@ const Plain = {
         !!iconfont && this.$utils.addScript(iconfont)
 
         this.changeTheme(theme)
-        const components = getComponents(prefix, customColumns)
+        const components = getComponents(prefix, customColumns || {})
         Object.keys(components).forEach(key => Vue.component(key, components[key]))
         const directives = getDirectives(prefix)
         Object.keys(directives).forEach(key => Vue.directive(key, directives[key]))
