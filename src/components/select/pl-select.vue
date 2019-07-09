acf3ae8b24b0cd995961129aeb56a9aa5e22e350
韦胜健 <martsforever@aliyun.com>
2019-07-09 14:37:48 +0800

    adjust pl-select

--- src/components/select/pl-select.vue
+++ src/components/select/pl-select.vue
@@ -1,19 +1,19 @@
 <template>
     <pl-input
             v-if="!multiple"
+            ref="input"
             class="pl-select"
             :class="{'pl-select-open':p_show}"
             :value="p_showValue"
-            ref="input"
             inputReadonly
             :readonly="readonly"
             :disabled="disabled"
             :required="required"
             :placeholder="placeholder"
-            :open="pl_open"
             icon="pl-triangle-down-fill"
             v-bind="Object.assign({},input,simpleBinding)"
 
+            @click="pl_click"
             @clear="pl_clear"
             @up.prevent="!!p_select && p_select.prev()"
             @down.prevent="!!p_select && p_select.next()"
@@ -21,7 +21,6 @@
             @tab="!!p_select && p_select.hide()"
             @esc="!!p_select && p_select.hide()"
             @space="pl_space"
-            @blur="pl_blur"
     />
     <pl-tag-input
             v-else
@@ -34,10 +33,11 @@
             :required="required"
             :placeholder="placeholder"
             v-bind="input"
-            @click="pl_enter"
             icon="pl-triangle-down-fill"
-            @clear="pl_clear"
             :onRemove="pl_onRemove"
+
+            @click="pl_click"
+            @clear="pl_clear"
             @keyup.up.prevent="!!p_select && p_select.prev()"
             @keydown.down.prevent="!!p_select && p_select.next()"
             @keyup.esc.prevent="!!p_select && p_select.hide()"
@@ -68,6 +68,50 @@
             after: {type: Function},
 
             placeholder: {default: '请选择...'},
+        },
+        data() {
+            return {
+                p_value: [],
+                p_select: null,
+                p_show: false,
+                p_watchValue: false,
+
+                p_option: null,
+            }
+        },
+        mounted() {
+            this.p_option = {
+                data: null,
+                reference: this.$refs.input,
+                autoFocus: false,
+                labelKey: this.labelKey,
+                render: this.pl_render,
+                autoClose: !this.multiple,
+                watchData: this.p_value,
+                popper: {
+                    ...(this.popper || {}),
+                    onShow: () => this.p_show = true,
+                    onHide: () => this.p_show = false,
+                },
+                onClose: () => this.p_select = null,
+                onOpen: () => this.pl_resetOptionData(),
+                onConfirm: async (e) => {
+                    const oldValue = this.p_value
+                    const value = this.pl_getValue(e)
+                    if (!this.multiple) {
+                        this.p_value = value
+                    } else {
+                        const index = this.p_value.indexOf(value)
+                        if (index > -1) {
+                            this.p_value.splice(index, 1)
+                        } else {
+                            this.p_value.push(value)
+                        }
+                    }
+                    this.$emit('input', this.p_value)
+                    !!this.after && await this.after(this.p_value, oldValue)
+                },
+            }
         },
         watch: {
             value: {
@@ -107,84 +151,22 @@
                 if (!this.multiple && this.p_value != null && showValues.length === 0) showValues.push(this.p_value)
                 return !this.multiple ? showValue : showValues
             },
+            optionData() {
+                return this.data
+            },
         },
-        data() {
-            return {
-                p_value: [],
-                p_select: null,
-                p_show: false,
-                p_watchValue: false,
-            }
-        },
+
         methods: {
-            async pl_open() {
+            /*---------------------------------------处理事件-------------------------------------------*/
+            async pl_click() {
+                if (!!this.$refs.input.p_readonly || !!this.$refs.input.p_disabled) return
                 if (!!this.before) await this.before(this.p_value)
                 if (!this.p_select) this.p_select = await this.$plain.$select.getSelect()
-                !this.p_select.p_show ?
-                    this.p_select.select({
-                        reference: this.$refs.input,
-                        autoFocus: false,
-                        data: this.data,
-                        labelKey: this.labelKey,
-                        render: this.pl_render,
-                        autoClose: !this.multiple,
-                        watchData: this.p_value,
-                        popper: {
-                            ...(this.popper || {}),
-                            onShow: () => this.p_show = true,
-                            onHide: () => this.p_show = false,
-                        },
-                        onClose: () => this.p_select = null,
-                        onConfirm: async (e) => {
-                            const oldValue = this.p_value
-                            const value = this.pl_getValue(e)
-                            if (!this.multiple) {
-                                this.p_value = value
-                            } else {
-                                const index = this.p_value.indexOf(value)
-                                if (index > -1) {
-                                    this.p_value.splice(index, 1)
-                                } else {
-                                    this.p_value.push(value)
-                                }
-                            }
-                            this.$emit('input', this.p_value)
-                            !!this.after && await this.after(this.p_value, oldValue)
-                        },
-                    })
-                    :
+                if (!this.p_select.p_show) {
+                    this.p_select.select(this.p_option)
+                } else {
                     this.p_select.hide()
-            },
-            pl_clear() {
-                this.p_value = !this.multiple ? null : []
-                !!this.p_select && (this.p_select.watchData = this.p_value)
-                this.$emit('input', this.p_value)
-            },
-            pl_render(h, {item, index}) {
-                const val = this.pl_getValue(item)
-                const isChecked = !this.multiple ? val === this.p_value : this.p_value.indexOf(val) > -1
-                const cls = [
-                    'pl-select-item',
-                    {
-                        'pl-select-item-checked': isChecked,
-                    }
-                ]
-
-                return (
-                    <div class={cls}>
-                        {!!this.multiple && <pl-icon icon={isChecked ? 'pad-check-square-fill' : 'pl-square-light'} class="pl-select-check-icon"/>}
-                        <div class="pl-select-item-content">
-                            {!!this.$scopedSlots.default ? <pl-scope-slot scope-slot-func={this.$scopedSlots.default} data={{item, index}}/> : this.pl_getShowValue(item)}
-                        </div>
-                    </div>
-                )
-            },
-
-            pl_getValue(item) {
-                return !!this.valueKey ? item[this.valueKey] : item
-            },
-            pl_getShowValue(item) {
-                return !!this.labelKey ? item[this.labelKey] : item
+                }
             },
             pl_enter() {
                 if (!!this.p_select) {
@@ -209,9 +191,42 @@
             pl_onRemove(item, index) {
                 this.p_value.splice(index, 1)
             },
-            pl_blur() {
-
+            pl_clear() {
+                this.p_value = !this.multiple ? null : []
+                !!this.p_select && (this.p_select.watchData = this.p_value)
+                this.$emit('input', this.p_value)
             },
+
+            /*---------------------------------------其他函数-------------------------------------------*/
+            pl_render(h, {item, index}) {
+                const val = this.pl_getValue(item)
+                const isChecked = !this.multiple ? val === this.p_value : this.p_value.indexOf(val) > -1
+                const cls = [
+                    'pl-select-item',
+                    {
+                        'pl-select-item-checked': isChecked,
+                    }
+                ]
+
+                return (
+                    <div class={cls}>
+                        {!!this.multiple && <pl-icon icon={isChecked ? 'pad-check-square-fill' : 'pl-square-light'} class="pl-select-check-icon"/>}
+                        <div class="pl-select-item-content">
+                            {!!this.$scopedSlots.default ? <pl-scope-slot scope-slot-func={this.$scopedSlots.default} data={{item, index}}/> : this.pl_getShowValue(item)}
+                        </div>
+                    </div>
+                )
+            },
+            pl_getValue(item) {
+                return !!this.valueKey ? item[this.valueKey] : item
+            },
+            pl_getShowValue(item) {
+                return !!this.labelKey ? item[this.labelKey] : item
+            },
+            pl_resetOptionData() {
+                this.p_option.data = this.optionData
+            },
+
         }
     }
 </script>
