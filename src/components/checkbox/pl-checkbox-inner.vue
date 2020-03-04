5802f487ace63c4f3178e9d497d60692cc282b20
martsforever <martsforever@aliyun.com>
2020-03-04 11:50:30 +0800

    去掉checkbox 的rect，改为使用背景色以及border

--- src/components/checkbox/pl-checkbox-inner.vue
+++ src/components/checkbox/pl-checkbox-inner.vue
@@ -1,6 +1,5 @@
 <template>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="pl-checkbox-inner" :class="classes">
-        <rect width="100" height="100"/>
         <polyline points="22,50 45,75 75,25" v-if="status === 'check'" class="pl-checkbox-inner-check-polyline"/>
         <polyline points="20,50 80,50" v-if="status === 'minus'" class="pl-checkbox-inner-minus-polyline"/>
     </svg>
