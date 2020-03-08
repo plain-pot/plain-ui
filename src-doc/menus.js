d577f69fca0d7715652feb6e705af570ca18cf7d
martsforever <martsforever@aliyun.com>
2020-03-08 20:36:31 +0800

    adjust menu seq

--- src-doc/menus.js
+++ src-doc/menus.js
@@ -10,6 +10,7 @@ export default [
     },
     {
         name: '表单', children: [
+            {name: 'Form', title: '表单', icon: 'pl-form', page: '/normal/form', complete: false},
             {name: 'Input', title: '输入框', icon: 'pl-edit-square-light', page: '/normal/input', complete: true},
             // {name: 'Textarea', title: '文本域', icon: 'pl-textarea', page: '/normal/textarea', complete: false},
             {name: 'Radio', title: '单选框', icon: 'pl-circle-radio', page: '/normal/radio', complete: true},
@@ -26,7 +27,6 @@ export default [
             {name: 'ColorPicker', title: '颜色选择', icon: 'pl-color', page: '/normal/color-picker', complete: false},
             {name: 'Upload', title: '文件上传', icon: 'pad-file', page: '/normal/upload', complete: false},
             {name: 'Img', title: '图片上传', icon: 'pad-image', page: '/normal/img', complete: false},
-            {name: 'Form', title: '表单', icon: 'pl-form', page: '/normal/form', complete: false},
         ]
     },
     {
