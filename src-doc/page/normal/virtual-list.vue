1a8918c8c6a0aff1303655cb02d8fb21e0d82b02
韦胜健 <martsforever@aliyun.com>
2020-03-10 20:38:12 +0800

    init virtual-list

--- src-doc/page/normal/virtual-list.vue
+++ src-doc/page/normal/virtual-list.vue
@@ -2,8 +2,21 @@
     <div class="demo-virtual-list">
         <demo-row title="基本用法">
             <div class="demo-virtual-list-container">
-                <pl-virtual-list>
-
+                <pl-virtual-list :data="list" :size="40" :remain="10">
+                    <div slot-scope="{item,index}" :style="{backgroundColor:item.color}" class="demo-virtual-list-item">
+                        <div class="seq">
+                            {{item.id}}
+                        </div>
+                        <div class="content">
+                            <div class="label">
+                                <span>{{item.name}}</span>
+                                <span>{{item.date}}</span>
+                            </div>
+                            <div class="star">
+                                {{item.star}}
+                            </div>
+                        </div>
+                    </div>
                 </pl-virtual-list>
             </div>
         </demo-row>
@@ -11,20 +24,77 @@
 </template>
 
 <script>
+
+    import {mock} from 'mockjs'
+
     export default {
         name: "demo-virtual-list",
         props: {},
         data() {
-            return {}
+
+            const list = []
+            const length = 100
+
+            list.push(...(mock({
+                [`array|${length}`]: [
+                    {
+                        "id|+1": 0,
+                        color: '@color',
+                        name: '@first',
+                        date: '@date',
+                        "star|1-10": '★',
+                    }
+                ]
+            })).array)
+
+            return {
+                list,
+            }
         },
         methods: {},
+        mounted() {
+
+        }
     }
 </script>
 
 <style lang="scss">
     .demo-virtual-list-container {
         width: 300px;
-        height: 500px;
+        height: 400px;
         background-color: #f6f6f6;
+
+        .demo-virtual-list-item {
+            display: inline-block;
+            width: 100%;
+            height: 40px;
+
+            .seq {
+                display: inline-block;
+                width: 40px;
+                background-color: rgba(0, 0, 0, 0.5);
+                color: white;
+                height: 100%;
+                text-align: center;
+                line-height: 40px;
+            }
+
+            .content {
+                width: calc(100% - 40px);
+                float: right;
+                height: 100%;
+                padding: 0 12px;
+                .label {
+                    font-size: 12px;
+                    display: flex;
+                    justify-content: space-between;
+                }
+
+                .star {
+                    text-align: right;
+                    color: white;
+                }
+            }
+        }
     }
 </style>
--- src/components/virtual-list/pl-virtual-list.vue
+++ src/components/virtual-list/pl-virtual-list.vue
@@ -1,16 +1,26 @@
 <template>
-    <div class="pl-virtual-list">
-
-        pl-virtual-list
-    </div>
+    <pl-scroll class="pl-virtual-list">
+        <template v-for="(item,index) in renderData">
+            <slot :item="item" :index="index"></slot>
+        </template>
+    </pl-scroll>
 </template>
 
 <script>
     export default {
         name: "pl-virtual-list",
-        props: {},
+        props: {
+            data: {type: Array, require: true},
+            size: {type: Number, require: true},
+            remain: {type: Number},
+        },
         data() {
             return {}
+        },
+        computed: {
+            renderData() {
+                return this.data || []
+            },
         },
         methods: {},
     }
