f1bc29f6d00a622044acf929b52c3648ce7f18cf
韦胜健 <martsforever@aliyun.com>
2020-03-17 11:42:11 +0800

    add form validate

---
+++ src-doc/page/form/form-validate.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="form-validate">
+        <demo-row title="基本用法">
+            <pl-form>
+                <pl-form-item label="客户名称" field="name">
+                    <pl-input v-model="formData.name"/>
+                </pl-form-item>
+                <pl-form-item label="客户员工数量" field="type">
+                    <pl-input v-model="formData.type"/>
+                    <span slot="suffix"><pl-icon icon="el-icon-question"/></span>
+                </pl-form-item>
+                <pl-form-item label="客户加入时间" field="joinTime">
+                    <pl-input v-model="formData.joinTime"/>
+                    <span>&nbsp;至&nbsp;</span>
+                    <pl-input v-model="formData.name"/>
+                </pl-form-item>
+                <pl-form-item label="是否老客户" field="oldFlag">
+                    <pl-radio-group v-model="formData.oldFlag" itemWidth="50%">
+                        <pl-radio label="老客户" val="Y"/>
+                        <pl-radio label="非老客户" val="N"/>
+                    </pl-radio-group>
+                </pl-form-item>
+                <pl-form-item label="客户性质" field="properties">
+                    <pl-checkbox-group v-model="formData.properties" itemWidth="50%">
+                        <pl-checkbox label="大客户" val="large"/>
+                        <pl-checkbox label="潜在客户" val="potential"/>
+                        <pl-checkbox label="长久客户" val="long"/>
+                        <pl-checkbox label="赢单客户" val="order"/>
+                    </pl-checkbox-group>
+                </pl-form-item>
+                <pl-form-item label="客户级别" field="level">
+                    <pl-select :data="levelData" labelKey="levelName" valueKey="code" v-model="formData.level"/>
+                </pl-form-item>
+                <pl-form-item label="备注" field="comments">
+                    <pl-input textarea v-model="formData.comments"/>
+                </pl-form-item>
+                <pl-form-item label=" ">
+                    <pl-button mode="stroke" label="取消"/>
+                    <pl-button label="保存"/>
+                </pl-form-item>
+            </pl-form>
+        </demo-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "form-validate",
+        props: {},
+        data() {
+            return {
+                formData: {},
+                levelData: [
+                    {levelName: '一级', code: '1'},
+                    {levelName: '二级', code: '2'},
+                    {levelName: '三级', code: '3'},
+                ],
+                formDisabledFields: [],
+            }
+        },
+        methods: {},
+    }
+</script>
+
+<style lang="scss">
+</style>
