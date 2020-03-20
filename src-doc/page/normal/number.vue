7559c9fe663dd08eb9f80924a0312b8e4eedc9ea
韦胜健 <martsforever@aliyun.com>
2020-03-20 10:18:31 +0800

    number demo max and min

--- src-doc/page/normal/number.vue
+++ src-doc/page/normal/number.vue
@@ -5,6 +5,20 @@
             <pl-number v-model="val[0]"/>
             {{val[0]}}
         </demo-row>
+        <demo-row title="最大最小值">
+            <demo-line title="max:10">
+                <pl-number v-model="val[1]" max="10"/>
+                {{val[1]}}
+            </demo-line>
+            <demo-line title="min:1">
+                <pl-number v-model="val[2]" min="1"/>
+                {{val[2]}}
+            </demo-line>
+            <demo-line title="max:10,min:1">
+                <pl-number v-model="val[3]" max="10" min="1"/>
+                {{val[3]}}
+            </demo-line>
+        </demo-row>
     </div>
 </template>
 
