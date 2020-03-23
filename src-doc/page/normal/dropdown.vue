570abab0015c973fc24b14618100be566d17f44e
韦胜健 <martsforever@aliyun.com>
2020-03-23 10:24:42 +0800

    dropdown 基本功能

--- src-doc/page/normal/dropdown.vue
+++ src-doc/page/normal/dropdown.vue
@@ -2,7 +2,23 @@
     <div class="demo-dropdown">
         <demo-row title="基本用法">
             <pl-dropdown ref="dropdown">
-                <pl-button @click="onClick">https://</pl-button>
+                <pl-button @click="$refs.dropdown.toggle()">https://</pl-button>
+                <pl-dropdown-menu slot="dropdown">
+                    <pl-dropdown-item label="新建"/>
+                    <pl-dropdown-item label="编辑"/>
+                    <pl-dropdown-item label="删除"/>
+                    <pl-dropdown-item label="导入"/>
+                    <pl-dropdown-item label="导出"/>
+                    <pl-dropdown-item label="筛选"/>
+                    <pl-dropdown-item label="排序"/>
+                    <pl-dropdown-item label="多选"/>
+                    <pl-dropdown-item label="设置"/>
+                </pl-dropdown-menu>
+            </pl-dropdown>
+        </demo-row>
+        <demo-row title="宽高设置">
+            <pl-dropdown ref="sizeDropdown" width="100" height="200px">
+                <pl-button @click="$refs.sizeDropdown.toggle()">https://</pl-button>
                 <pl-dropdown-menu slot="dropdown">
                     <pl-dropdown-item label="新建"/>
                     <pl-dropdown-item label="编辑"/>
@@ -27,9 +43,6 @@
             return {}
         },
         methods: {
-            onClick() {
-                this.$refs.dropdown.toggle()
-            },
         },
     }
 </script>
--- src/components/dropdown/pl-dropdown.vue
+++ src/components/dropdown/pl-dropdown.vue
@@ -1,25 +1,49 @@
 <script>
+    import {EmitMixin, PropsMixinFactory} from "../../utils/mixins";
+
     export default {
         name: "pl-dropdown",
-        props: {},
+        mixins: [
+            EmitMixin,
+            PropsMixinFactory.create({
+                width: PropsMixinFactory.Number,
+                height: PropsMixinFactory.Number,
+            }),
+        ],
+        props: {
+            trigger: {type: String, default: 'click'},                      // click, focus, hover, manual
+            width: {type: [String, Number]},
+            height: {type: [String, Number]},
+
+        },
         data() {
             return {
                 service: null,
                 popperOption: {
                     reference: () => this.$el,
-                    popperProps:{
+                    popperProps: {
                         noContentPadding: true,
                     },
                     render: (h) => {
-                        if (!!this.$slots.dropdown) {
+
+                        const wrapper = (content) => {
                             return (
-                                <div>
-                                    {this.$slots.dropdown}
+                                <div style={this.wrapperStyle}>
+                                    {!this.p_height ?
+                                        content :
+                                        <pl-scroll>
+                                            {content}
+                                        </pl-scroll>
+                                    }
                                 </div>
                             )
                         }
+
+                        if (!!this.$slots.dropdown) {
+                            return wrapper(this.$slots.dropdown)
+                        }
                         if (!!this.$scopedSlots.default) {
-                            return this.$scopedSlots.default()
+                            return wrapper(this.$scopedSlots.default())
                         }
                     },
                 },
@@ -34,6 +58,12 @@
                 if (!this.service) return false
                 return this.service.isOpen()
             },
+            wrapperStyle() {
+                return {
+                    ...(!this.p_width ? {} : {width: this.$plain.utils.suffixPx(this.p_width)}),
+                    ...(!this.p_height ? {} : {height: this.$plain.utils.suffixPx(this.p_height)}),
+                }
+            },
         },
         render() {
             return this.$slots.default[0]
@@ -44,10 +74,12 @@
                 if (!this.service) {
                     this.service = await this.$popper(this.popperOption)
                 }
+                console.log('show')
                 this.service.show()
             },
             hide() {
                 if (!this.isShow) return
+                console.log('hide')
                 this.service.hide()
             },
             toggle() {
