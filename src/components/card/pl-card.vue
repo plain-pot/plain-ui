b34ceecdd496a96f950d67e7d34e75a079093460
韦胜健 <martsforever@aliyun.com>
2020-03-25 16:24:23 +0800

    complete card

--- src/components/card/pl-card.vue
+++ src/components/card/pl-card.vue
@@ -5,8 +5,16 @@
 </template>
 
 <script>
+    import {PropsMixinFactory} from "../../utils/mixins";
+
     export default {
         name: "pl-card",
+        mixins: [
+            PropsMixinFactory.create({
+                width: PropsMixinFactory.Number,
+                height: PropsMixinFactory.Number,
+            })
+        ],
         props: {
             width: {default: '300px'},
             height: {default: '300px'},
@@ -15,8 +23,8 @@
         computed: {
             styles() {
                 const styles = {}
-                this.width !== null && (styles.width = this.$plain.utils.unit(this.width))
-                this.height !== null && (styles.height = this.$plain.utils.unit(this.height))
+                this.p_width !== null && (styles.width = this.$plain.utils.suffixPx(this.p_width))
+                this.p_height !== null && (styles.height = this.$plain.utils.suffixPx(this.p_height))
                 return styles
             },
         },
