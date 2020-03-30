632628c2705720efdedd4046b35aaf3f700fd488
韦胜健 <martsforever@aliyun.com>
2020-03-30 17:13:46 +0800

    adjust tree props

--- src/components/tree/pl-tree.vue
+++ src/components/tree/pl-tree.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="pl-tree">
-        pl-tree
+        <pl-tree-node v-for="(item,index) in formatData" :key="item.key || index" :data="item"/>
     </div>
 </template>
 
 <script>
     import {EmitMixin, StyleMixin} from "../../utils/mixins";
+    import PlTreeNode from "./pl-tree-node";
 
     export default {
         name: "pl-tree",
+        components: {PlTreeNode},
         mixins: [
             EmitMixin,
             StyleMixin,
@@ -36,25 +38,26 @@
             // 普通属性
             emptyText: {type: String},                                  // 没有子节点的时候展示的文本
             renderContent: {type: Function},                            // 树节点内容渲染函数
-            currentKey: {type: String},                                 // 当前选中节点的key
             filterNodeMethod: {type: Function},                         // 对树节点进行筛选的方法，返回true表示可以显示，返回false表示隐藏
-            intent: {type: Number},                                     // 相邻级节点水平缩进距离，默认16，单位px
-            expandIcon: {type: String},                                 // 树展开图标
-
-            // 行为属性
-            renderAfterExpand: {type: Boolean, default: true},          // 是否在第一次展开节点之后才渲染内容
-            according: {type: Boolean},                                 // 是否每次只展开一个同级的树节点
             highlightCurrent: {type: Boolean},                          // 是否高亮当前选中节点
-            showCheckbox: {type: Boolean},                              // 是否展示勾选框
-            checkStrictly: {type: Boolean},                             // 在显示复选框的情况下，是否严格遵循父子互不关联的做法，默认为false
-            expandOnClickNode: {type: Boolean},                         // 是否点击树节点的时候展开子节点
-            checkOnClickNode: {type: Boolean},                          // 是否点击树节点的时候选中节点
-            autoExpandParent: {type: Boolean},                          // 是否展开节点的时候，自动展开父节点
+            currentKey: {type: String},                                 // 当前选中节点的key
 
-            // 默认值属性
-            defaultExpandAll: {type: Boolean},                          // 是否默认展开所有节点
+            // 展开相关属性
             defaultExpandKeys: {type: Array},                           // 默认展开的节点key数组
+            autoExpandParent: {type: Boolean},                          // 是否展开节点的时候，自动展开父节点
+            defaultExpandAll: {type: Boolean},                          // 是否默认展开所有节点
+            expandOnClickNode: {type: Boolean},                         // 是否点击树节点的时候展开子节点
+            according: {type: Boolean},                                 // 是否每次只展开一个同级的树节点
+            renderAfterExpand: {type: Boolean, default: true},          // 是否在第一次展开节点之后才渲染内容
+            expandIcon: {type: String},                                 // 树展开图标
+            intent: {type: Number},                                     // 相邻级节点水平缩进距离，默认16，单位px
+
+            // 勾选相关属性
+            showCheckbox: {type: Boolean},                              // 是否展示勾选框
             defaultCheckKeys: {type: Array},                            // 默认选中的节点key的数组
+            checkOnClickNode: {type: Boolean},                          // 是否点击树节点的时候选中节点
+            checkStrictly: {type: Boolean},                             // 在显示复选框的情况下，是否严格遵循父子互不关联的做法，默认为false
+            isCheckable: {type: Function},                              // 当即将选中树节点时，判断是否可以选中该树节点
 
             // 拖拽属性
             draggable: {type: Boolean},                                 // 是否可拖拽
