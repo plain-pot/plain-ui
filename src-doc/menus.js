4c2725a187f958d75715e7f0152fcdfd048b20df
韦胜健 <martsforever@aliyun.com>
2020-04-01 14:28:48 +0800

    add test-drag

--- src-doc/menus.js
+++ src-doc/menus.js
@@ -104,7 +104,8 @@ export default [
     {name: '指令'},
     {
         name: '其他', children: [
-            {name: 'Test', title: '测试手动渲染实例', icon: 'pl-table-solid', page: '/normal/test', complete: false},
+            {name: 'Test', title: '测试手动渲染实例', icon: 'pl-table-solid', page: '/normal/test', complete: true},
+            {name: 'Drag', title: '测试拖拽节点', icon: 'pl-table-solid', page: '/test/test-drag', complete: true},
         ]
     },
 ]
---
+++ src-doc/page/test/test-drag.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="test-drag">
+        <button draggable="true" @dragstart="draggable.start" @drag="draggable.drag" @dranend="draggable.end">test-drag</button>
+        <div style="height: 100px;width: 100px;background-color: #12b4a5" @drop="draggable.drop">
+
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "test-drag",
+        props: {},
+        data() {
+            return {
+                draggable: {
+                    start: (e) => {
+                        console.log('start')
+                    },
+                    drag: e => {
+                        console.log('drag')
+                    },
+                    end: e => {
+                        console.log('end')
+                    },
+                    drop: e => {
+                        console.log('drop')
+                    },
+                },
+            }
+        },
+        methods: {},
+    }
+</script>
+
+<style lang="scss">
+</style>
