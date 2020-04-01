21fcdaf91ce4d17188529d7fac317a1cfd07435e
韦胜健 <martsforever@aliyun.com>
2020-04-01 09:35:50 +0800

    取消expandKeys以及checkKeys

--- src/components/tree/pl-tree.vue
+++ src/components/tree/pl-tree.vue
@@ -38,9 +38,8 @@
 
             // 展开相关属性
             emptyText: {type: String, default: '暂无数据'},              // 没有子节点的时候展示的文本
-            expandKeys: {type: Array},                                  // 默认展开的节点key数组
             defaultExpandAll: {type: Boolean},                          // 是否默认展开所有节点
-            according: {type: Boolean},                                 // 是否每次只展开一个同级的树节点
+            //   according: {type: Boolean},                                 // 是否每次只展开一个同级的树节点
             expandIcon: {type: String},                                 // 树展开图标
             intent: {type: Number, default: 14},                        // 相邻级节点水平缩进距离，默认16，单位px
             lazy: {type: Boolean},                                      // 是否懒加载子节点数据
@@ -52,17 +51,16 @@
             autoExpandParent: {type: Boolean, default: true},           // 是否展开节点的时候，自动展开父节点
 
             // 勾选相关属性
-            checkKeys: {type: Array},                                   // 默认选中的节点key的数组
-            checkableKeys: {type: String},                              // 树节点复选框是否可选对应字段，为false值会被禁用
+            //   checkableKeys: {type: String},                              // 树节点复选框是否可选对应字段，为false值会被禁用
             showCheckbox: {type: Boolean},                              // 是否展示勾选框
-            checkOnClickNode: {type: Boolean},                          // 是否点击树节点的时候选中节点
-            checkStrictly: {type: Boolean},                             // 在显示复选框的情况下，是否严格遵循父子互不关联的做法，默认为false
-            isCheckable: {type: Function},                              // 当即将选中树节点时，判断是否可以选中该树节点
+            //   checkOnClickNode: {type: Boolean},                          // 是否点击树节点的时候选中节点
+            //   checkStrictly: {type: Boolean},                             // 在显示复选框的情况下，是否严格遵循父子互不关联的做法，默认为false
+            //   isCheckable: {type: Function},                              // 当即将选中树节点时，判断是否可以选中该树节点
 
             // 拖拽属性
-            draggable: {type: Boolean},                                 // 是否可拖拽
-            isDraggable: {type: Function},                              // 判断节点是否可以拖拽
-            isDroppable: {type: Function},                              // 判断目标节点能够被放置
+            //   draggable: {type: Boolean},                                 // 是否可拖拽
+            //   isDraggable: {type: Function},                              // 判断节点是否可以拖拽
+            //   isDroppable: {type: Function},                              // 判断目标节点能够被放置
         },
         emitters: {
             emitNodeClick: Function,
@@ -84,32 +82,19 @@
             emitDrop: Function,
         },
         watch: {
+            /**
+             * 单向绑定currentKey
+             * @author  韦胜健
+             * @date    2020/4/1 9:30
+             */
             currentKey(val: string): void {
                 this.p_currentKey = val
             },
             /**
-             * expandKeys 是一个数组，比如 [1,2,3]，当变化的时候设置对应数节点展开或者收起
+             * data发生变化之后，清理 mark中无效的数据
              * @author  韦胜健
-             * @date    2020/3/30 20:08
+             * @date    2020/4/1 9:30
              */
-            expandKeys: {
-                immediate: true,
-                handler(val: string[]) {
-                    if (!val) val = []
-                    if (JSON.stringify(val) !== JSON.stringify(this.emitExpandKeys)) {
-                        Object.values(this.mark).forEach((mark: TreeMark) => mark.expanded = val.indexOf(mark.key) > -1)
-                    }
-                },
-            },
-            checkKeys: {
-                immediate: true,
-                handler(val: string[]) {
-                    if (!val) val = []
-                    if (JSON.stringify(val) !== JSON.stringify(this.emitCheckKeys)) {
-                        Object.values(this.mark).forEach((mark: TreeMark) => mark.checked = val.indexOf(mark.key) > -1)
-                    }
-                },
-            },
             data: {
                 deep: true,
                 handler(val) {
