7a12c3a3412cbf029bb6be541a0728663b8219c5
韦胜健 <martsforever@aliyun.com>
2020-04-02 21:26:22 +0800

    add test canvas

--- src-doc/page/test/test-canvas.vue
+++ src-doc/page/test/test-canvas.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="test-canvas">
         <demo-row>
-            <canvas/>
+            <canvas ref="canvas" class="canvas" width="300" height="200"/>
         </demo-row>
     </div>
 </template>
@@ -14,8 +14,28 @@
             return {}
         },
         methods: {},
+        mounted() {
+            const canvas = this.$refs.canvas
+            const ctx = canvas.getContext('2d');
+
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(200,0);
+            ctx.lineTo(300, 100);
+            ctx.lineTo(200, 200);
+            ctx.lineTo(0, 200);
+            ctx.lineTo(100, 100);
+            ctx.fill();
+        }
     }
 </script>
 
 <style lang="scss">
+    .test-canvas {
+        .canvas {
+            /*width: 300px;
+            height: 200px;*/
+            /*background-color: #ddd;*/
+        }
+    }
 </style>
