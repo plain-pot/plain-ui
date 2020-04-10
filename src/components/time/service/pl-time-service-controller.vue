a349d1a816ede5e297ca2f96d805a72a0d72fd91
韦胜健 <martsforever@aliyun.com>
2020-04-10 10:46:40 +0800

    add time service

---
+++ src/components/time/service/pl-time-service-controller.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="pl-time-service-controller">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "pl-time-service-controller",
+        props: {},
+        data() {
+            return {}
+        },
+        methods: {},
+    }
+</script>
+
+<style lang="scss">
+</style>
