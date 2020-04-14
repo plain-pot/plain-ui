37bb8d844299809eb45d54527a8e0faca7a9f558
韦胜健 <martsforever@aliyun.com>
2020-04-14 14:38:03 +0800

    adjust panel item style

--- src/components/date/pl-date.vue
+++ src/components/date/pl-date.vue
@@ -85,12 +85,12 @@
 
                     &.pl-date-base-panel-item {
                         cursor: pointer;
-                        padding: 3px;
                         box-sizing: border-box;
+
                         & > div {
                             width: 100%;
                             height: 100%;
-                            padding: 3px;
+                            padding: 4px;
                             box-sizing: border-box;
                             border-radius: 100px;
 
@@ -116,9 +116,13 @@
                         }
 
                         &.pl-date-base-panel-item-active {
-                            span {
-                                background-color: $colorPrimary;
-                                color: white;
+                            & > div {
+                                background-color: rgba($colorPrimary, 0.1);
+
+                                & > span {
+                                    background-color: $colorPrimary;
+                                    color: white;
+                                }
                             }
                         }
 
@@ -128,7 +132,6 @@
                         }
 
                         &.pl-date-base-panel-item-hover-start {
-                            padding-right: 0;
 
                             & > div {
                                 background-color: rgba($colorPrimary, 0.1);
@@ -137,8 +140,6 @@
                         }
 
                         &.pl-date-base-panel-item-hover {
-                            padding-left: 0;
-                            padding-right: 0;
 
                             & > div {
                                 background-color: rgba($colorPrimary, 0.1);
@@ -147,7 +148,6 @@
                         }
 
                         &.pl-date-base-panel-item-hover-end {
-                            padding-left: 0;
 
                             & > div {
                                 background-color: rgba($colorPrimary, 0.1);
