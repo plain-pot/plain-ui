d598c7df900b9d5f12b15ca3a452417398d45423
韦胜健 <martsforever@aliyun.com>
2020-04-16 17:42:46 +0800

    梳理结构

--- src/components/date/subs/pl-date-panel-week.tsx
+++ src/components/date/subs/pl-date-panel-week.tsx
@@ -83,11 +83,39 @@ export default {
                     },
                 },
                 on: {
-                    'mouseenter-item': ({ipd}: DateBasePanelItemData) => {
-                        this.hoverPd = ipd
-                    },
+                    'mouseenter-item': this.onMouseenterItem,
                     'click-item': this.onClickItem
                 },
+            }
+        },
+        WeekGetData() {
+            const {value} = this.formatData as { [key: string]: PlainDate }
+            const [start, end] = this.valueRange as [PlainDate, PlainDate]
+            const {hoverRange} = this
+            const [hoverStart, hoverEnd] = (hoverRange || []) as [PlainDate, PlainDate]
+
+            return {
+                value: {
+                    pd: value,
+                    range: value.isNull ? null : this.getWeekPdByPlainDate(value),
+                },
+                start: {
+                    pd: start,
+                    range: start.isNull ? null : this.getWeekPdByPlainDate(start),
+                },
+                end: {
+                    pd: end,
+                    range: end.isNull ? null : this.getWeekPdByPlainDate(end),
+                },
+                hoverStart: {
+                    pd: hoverStart,
+                    range: !!hoverRange ? this.getWeekPdByPlainDate(hoverStart) : null,
+                },
+                hoverEnd: {
+                    pd: hoverEnd,
+                    range: !!hoverRange ? this.getWeekPdByPlainDate(hoverEnd) : null
+                },
+                hoverRange,
             }
         },
     },
@@ -96,8 +124,12 @@ export default {
         getWeekPdByPlainDate(pd: PlainDate): { start: PlainDate, end: PlainDate } {
             let day = pd.day
             let firstWeekDay = this.firstWeekDay
+
+            const weekDayDuration = day - firstWeekDay
+            let offsetDay = weekDayDuration === 0 ? 0 : weekDayDuration > 0 ? weekDayDuration : 7 + weekDayDuration
+
             let startPd = pd.copy()
-            startPd.setMonthDate(startPd.month, startPd.date - (day - firstWeekDay))
+            startPd.setMonthDate(startPd.month, startPd.date - offsetDay)
             let endPd = startPd.copy()
             endPd.setMonthDate(endPd.month, endPd.date + 6)
             return {
@@ -116,18 +148,33 @@ export default {
 
         getActive(ipd: PlainDate | number, type: DateView) {
             if (type === DateView.date) {
+                const {value, start, end} = this.WeekGetData as { [key: string]: { pd: PlainDate, range: { start: PlainDate, end: PlainDate } | null } }
+
                 ipd = ipd as PlainDate
                 if (!this.range) {
-                    const {value} = this.formatData as { [key: string]: PlainDate }
-                    if (!value.isNull) {
-                        const {start, end} = this.getWeekPdByPlainDate(value)
-                        return (ipd.greaterThan(start, PlainDate.CompareMode.date) === 0) || (ipd.lessThan(end, PlainDate.CompareMode.date) === 0)
+                    if (!value.pd.isNull) {
+                        const {start: startPd, end: endPd} = value.range
+
+                        return !startPd.isNull && ((ipd.greaterThan(startPd, PlainDate.CompareMode.date) === 0) || (ipd.lessThan(endPd, PlainDate.CompareMode.date) === 0))
+                    }
+                } else {
+                    if (!start.pd.isNull) {
+                        const {start: startPd, end: endPd} = start.range
+                        if (startPd.greaterThan(ipd, PlainDate.CompareMode.date) === 0 || endPd.lessThan(ipd, PlainDate.CompareMode.date) === 0) {
+                            return true
+                        }
+                    }
+                    if (!end.pd.isNull) {
+                        const {start: startPd, end: endPd} = end.range
+                        if (startPd.greaterThan(ipd, PlainDate.CompareMode.date) === 0 || endPd.lessThan(ipd, PlainDate.CompareMode.date) === 0) {
+                            return true
+                        }
                     }
                 }
             }
         },
         getHoverStart(ipd: PlainDate | number, type: DateView) {
-
+            return false
         },
         getHover(ipd: PlainDate | number, type: DateView) {
             if (type === DateView.date) {
@@ -137,24 +184,74 @@ export default {
                         return true
                     }
                 }
-                const {value, start, end} = this.formatData as { [key: string]: PlainDate }
+
+                const {value, start, end, hoverStart, hoverEnd, hoverRange} = this.WeekGetData as { [key: string]: { pd: PlainDate, range: { start: PlainDate, end: PlainDate } | null } }
+
                 if (!this.range) {
-                    if (!value.isNull) {
-                        return this.isHover(value, ipd)
+                    if (!value.pd.isNull) {
+                        const {start: startPd, end: endPd} = value.range
+                        return startPd.lessThan(ipd, PlainDate.CompareMode.date) >= 0 && endPd.greaterThan(ipd, PlainDate.CompareMode.date) >= 0
                     }
                 } else {
-
+                    if (!!hoverRange) {
+                        let startPd = hoverStart.range.start
+                        let endPd = hoverEnd.range.end
+                        return startPd.lessThan(ipd, PlainDate.CompareMode.date) >= 0 && endPd.greaterThan(ipd, PlainDate.CompareMode.date) >= 0
+                    } else {
+                        if (!!start.range && !!end.range) {
+                            let startPd = start.range.start
+                            let endPd = end.range.end
+                            return startPd.lessThan(ipd, PlainDate.CompareMode.date) >= 0 && endPd.greaterThan(ipd, PlainDate.CompareMode.date) >= 0
+                        }
+                    }
                 }
             }
         },
         getHoverEnd(ipd: PlainDate | number, type: DateView) {
-
+            return false
         },
         /*---------------------------------------handler-------------------------------------------*/
         onClickItem({ipd}: DateBasePanelItemData) {
             const {start} = this.getWeekPdByPlainDate(ipd)
-            this.p_value = start.valueString
-            this.emitInput(this.p_value)
+
+            if (!this.range) {
+                this.p_value = start.valueString
+                this.emitInput(this.p_value)
+            } else {
+                if (!this.hoverRange) {
+                    const {start: itemStart} = this.getWeekPdByPlainDate(ipd)
+
+                    this.hoverRange = [itemStart, itemStart]
+                    this.valueRange = [itemStart, itemStart]
+                } else {
+                    const [startPd, endPd] = this.hoverRange as [PlainDate, PlainDate]
+
+                    this.p_start = startPd.valueString
+                    this.p_end = endPd.valueString
+
+                    this.hoverRange = null
+                    this.valueRange = [startPd, endPd]
+
+                    this.emitUpdateStart(this.p_start)
+                    this.emitInput(this.p_start, 'start')
+                    this.emitUpdateEnd(this.p_end)
+                    this.emitInput(this.p_end, 'end')
+                }
+            }
+        },
+        onMouseenterItem({ipd}: DateBasePanelItemData) {
+            const {start: itemStartPd} = this.getWeekPdByPlainDate(ipd)
+            console.log(itemStartPd.valueString)
+
+            if (!this.range) {
+                this.hoverPd = itemStartPd
+            } else {
+                if (!!this.hoverRange) {
+                    const {start: {range}} = this.WeekGetData
+                    const midPd = range[0]
+                    this.hoverRange = midPd.greaterThan(itemStartPd, PlainDate.CompareMode.yearmonth) > 0 ? [itemStartPd, midPd] : [midPd, itemStartPd]
+                }
+            }
         },
     },
 }
