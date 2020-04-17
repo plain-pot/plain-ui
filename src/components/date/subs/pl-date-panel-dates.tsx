284bfb6c9ea0dde154628b982198b8b07f7e6f4a
韦胜健 <martsforever@aliyun.com>
2020-04-17 17:12:02 +0800

    完成 pl-date-panel-dates

--- src/components/date/subs/pl-date-panel-dates.tsx
+++ src/components/date/subs/pl-date-panel-dates.tsx
@@ -8,6 +8,9 @@ export default {
         EmitMixin,
         DatePublicMixin,
     ],
+    emitters: {
+        emitInput: Function,
+    },
     data() {
         const {value: p_value} = this
         return {
@@ -16,7 +19,7 @@ export default {
     },
     render(h) {
         return (
-            <pl-date-base-panel-date/>
+            <pl-date-base-panel-date {...this.datePanelBinding}/>
         )
     },
     computed: {
@@ -35,20 +38,44 @@ export default {
                 min,
             }
         },
-    },
-    methods: {
-        getDisabled() {
-        },
-        getActive(ipd: PlainDate, type: DateView) {
-            if (type === DateView.date) {
-
+        datePanelBinding() {
+            return {
+                props: {
+                    max: this.max,
+                    min: this.min,
+                },
+                on: {
+                    'click-item': this.onClickItem
+                },
             }
         },
-        getHoverStart() {
+    },
+    methods: {
+        getChildActive(ipd: PlainDate, type: DateView) {
+            const {value} = this.formatData
+
+            if (type === DateView.year) {
+                return !!this.$plain.utils.findOne(value, item => item.Y === ipd)
+            } else if (type === DateView.month) {
+                return !!this.$plain.utils.findOne(value, item => item.YM === ipd.YM)
+            } else if (type === DateView.date) {
+                return !!this.$plain.utils.findOne(value, item => item.YMD === ipd.YMD)
+            }
         },
-        getHover() {
-        },
-        getHoverEnd() {
+        /*---------------------------------------handler-------------------------------------------*/
+        onClickItem(ipd: PlainDate) {
+
+            const {value} = this.formatData
+
+            const ret = this.$plain.utils.findOne(value, item => item.YMD === ipd.YMD, true)
+            if (!!ret) {
+                value.splice(ret.index, 1)
+            } else {
+                value.push(ipd)
+            }
+
+            this.p_value = value.map(item => item.valueString)
+            this.emitInput(this.p_value)
         },
     },
 }
