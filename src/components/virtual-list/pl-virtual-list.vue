1fe55f806f386422ca68fa51818f0c78272427fa
韦胜健 <martsforever@aliyun.com>
2020-04-24 16:11:24 +0800

    virtual list 新增属性禁用虚拟滚动

--- src/components/virtual-list/pl-virtual-list.vue
+++ src/components/virtual-list/pl-virtual-list.vue
@@ -16,6 +16,7 @@
             remain: {type: Number, require: true},                      // 一屏渲染的行数，总共渲染三屏，一屏渲染个数越多，滚动效果越好，但是浏览器卡顿的效果可能更明显；如果不传remain，则根据size以及 pl-virtual-list 跟节点的高度自动计算行数
             dynamicSize: {type: Boolean},                               // 标识列表中的每一行高度不是固定的，但是还是需要提供 size 属性，而且size属性不能与每一行的高度差距太多；
             renderContent: {type: Function},                            // 渲染content节点的渲染函数
+            disabled: {type: Boolean},
 
             contentIs: {},
             contentProps: {},
@@ -24,6 +25,8 @@
             remain: {
                 immediate: true,
                 handler(val) {
+                    if (this.isDisabled) return
+
                     if (!!val) this.p_remain = val
                     else {
                         // 自动计算 remain
@@ -38,6 +41,8 @@
             data: {
                 immediate: true,
                 handler(val) {
+                    if (this.isDisabled) return
+
                     if (!this.dynamicSize) return
                     val = val || []
                     this.dataInfo = val.map((item, index) => ({
@@ -76,6 +81,8 @@
             )
         },
         async updated() {
+            if (this.isDisabled) return
+
             if (!this.dynamicSize) return
             // 页面渲染完成之后，需要根据当前展示的数据，更新缓存的内容
             await this.$plain.nextTick()
@@ -105,6 +112,9 @@
             }
         },
         computed: {
+            isDisabled() {
+                return this.disabled
+            },
             targetStart() {
                 return this.start - Math.min(this.start, this.p_remain)
             },
@@ -112,6 +122,9 @@
                 return this.end + Math.min(this.data.length - this.end, this.p_remain)
             },
             targetData() {
+                if (this.isDisabled) {
+                    return this.data.map((item, index) => ({item, index}))
+                }
                 if (!this.p_remain) return []
                 return (this.data || []).map((item, index) => ({item, index})).slice(this.targetStart, this.targetEnd)
             },
@@ -129,21 +142,24 @@
         methods: {
             /*---------------------------------------listener-------------------------------------------*/
             onScroll(e) {
-                const scrollTop = e.target.scrollTop
 
-                if (!this.dynamicSize) {
-                    // 固定高度
-                    this.start = Math.floor(scrollTop / this.size)
-                    this.offset = (this.targetStart) * this.size
-                } else {
-                    // 动态高度
-                    this.start = this.getStartByDynamic(scrollTop)
-                    this.offset = this.dataInfo[this.targetStart].top
+                if (!this.isDisabled) {
+                    const scrollTop = e.target.scrollTop
+
+                    if (!this.dynamicSize) {
+                        // 固定高度
+                        this.start = Math.floor(scrollTop / this.size)
+                        this.offset = (this.targetStart) * this.size
+                    } else {
+                        // 动态高度
+                        this.start = this.getStartByDynamic(scrollTop)
+                        this.offset = this.dataInfo[this.targetStart].top
+                    }
+
+                    this.end = this.start + this.p_remain
                 }
 
-                this.end = this.start + this.p_remain
-
-                this.$emit('scroll')
+                this.$emit('scroll', e)
             },
 
             /*---------------------------------------utils-------------------------------------------*/
