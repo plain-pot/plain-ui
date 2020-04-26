ba0f905baf4f2b4e8c6339ee8d4922b6244c0a64
韦胜健 <martsforever@aliyun.com>
2020-04-26 10:33:24 +0800

    准备实现列头拖拽排序的功能

--- src/components/table/table/plt-head-cell.tsx
+++ src/components/table/table/plt-head-cell.tsx
@@ -1,59 +1,61 @@
 import {getCellClass, TableComponentMixin} from "./table-utils";
 import {Plc} from "../plc/plc-utils";
 
-const DragMixin = {
+const ResizeMixin = {
     data() {
-        const dragHandler = {
-            mousedown: (e: any) => {
-                window.addEventListener('mousemove', dragHandler.mousemove)
-                window.addEventListener('mouseup', dragHandler.mouseup)
-                this.$plain.enableSelectNone()
-                this.startX = e.clientX
-
-                this.indicatorEl = document.createElement('div')
-
-                const cell = e.currentTarget
-                const table = this.plTable.$el
-
-                const {left, width} = cell.getBoundingClientRect()
-                const {top, height} = table.getBoundingClientRect()
-
-                Object.assign(this.indicatorEl.style, {
-                    display: 'inline-block',
-                    zIndex: 9999,
-                    position: 'fixed',
-
-                    width: `${width}px`,
-                    backgroundColor: '#ddd',
-                    height: `${height}px`,
-                    top: `${top}px`,
-                    left: `${left}px`,
-                })
-
-                document.body.appendChild(this.indicatorEl)
-            },
-            mousemove: (e: any) => {
-                this.indicatorEl.style.left = `${e.clientX}px`
-            },
-            mouseup: (e: any) => {
-                window.removeEventListener('mousemove', dragHandler.mousemove)
-                window.removeEventListener('mouseup', dragHandler.mouseup)
-                this.$plain.disabledSelectNone()
-                document.body.removeChild(this.indicatorEl)
-                this.endX = e.clientX
-
-                let durX = this.endX - this.startX
-
-                this.plc.setDurWidth(durX)
-                this.plTable.refreshPlcWidth()
-            },
-        }
-
-        return {
-            dragHandler,
+        const resize = {
             indicatorEl: null,
             startX: null,
             endX: null,
+            handler: {
+                mousedown: (e: any) => {
+                    window.addEventListener('mousemove', resize.handler.mousemove)
+                    window.addEventListener('mouseup', resize.handler.mouseup)
+                    this.$plain.enableSelectNone()
+                    resize.startX = e.clientX
+
+                    resize.indicatorEl = document.createElement('div')
+
+                    const cell = e.currentTarget
+                    const table = this.plTable.$el
+
+                    const {left, width} = cell.getBoundingClientRect()
+                    const {top, height} = table.getBoundingClientRect()
+
+                    Object.assign(resize.indicatorEl.style, {
+                        display: 'inline-block',
+                        zIndex: 9999,
+                        position: 'fixed',
+
+                        width: `${width}px`,
+                        backgroundColor: '#ddd',
+                        height: `${height}px`,
+                        top: `${top}px`,
+                        left: `${left}px`,
+                    })
+
+                    document.body.appendChild(resize.indicatorEl)
+                },
+                mousemove: (e: any) => {
+                    resize.indicatorEl.style.left = `${e.clientX}px`
+                },
+                mouseup: (e: any) => {
+                    window.removeEventListener('mousemove', resize.handler.mousemove)
+                    window.removeEventListener('mouseup', resize.handler.mouseup)
+                    this.$plain.disabledSelectNone()
+                    document.body.removeChild(resize.indicatorEl)
+                    resize.endX = e.clientX
+
+                    let durX = resize.endX - resize.startX
+
+                    this.plc.setDurWidth(durX)
+                    this.plTable.refreshPlcWidth()
+                },
+            },
+        }
+        return {
+            resize,
+
         }
     },
 }
@@ -62,20 +64,33 @@ export default {
     name: 'plt-head-cell',
     inject: {
         plTable: {default: null},
+        pltHead: {default: null},
     },
     mixins: [
         TableComponentMixin,
-        DragMixin,
+        ResizeMixin,
     ],
     props: {
         plc: {type: Object},
     },
     render(h) {
         const plc = this.plc as Plc
+        const {colspan, rowspan, title} = plc
+
+        const binding = {
+            class: this.classes,
+            style: this.styles,
+            attrs: {
+                colspan,
+                rowspan,
+                // draggable: this.pltHead.headPlcList.length > 1,
+            },
+        }
+
         return (
-            <th class={this.classes} colspan={plc.colspan} rowspan={plc.rowspan} style={this.styles}>
-                {plc.title}
-                <span class="plt-head-cell-indicator" onMousedown={this.dragHandler.mousedown}/>
+            <th {...binding}>
+                {title}
+                <span class="plt-head-cell-indicator" onMousedown={this.resize.handler.mousedown}/>
             </th>
         )
     },
