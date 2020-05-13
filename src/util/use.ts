98a57362c5d1915d02dc6f8826867e8dcb4d5dc4
= <=>
2020-05-13 09:49:10 +0800

    好像实现了useProps

--- src/util/use.ts
+++ src/util/use.ts
@@ -4,19 +4,33 @@ export enum FormatPropsType {
     number = 'number',
 }
 
-export function useProps<T, O>(props: T, option: O extends { [key in keyof T]?: FormatPropsType }): any {
+class People {
+    sayHello() {}
+}
+
+// @ts-ignore
+export function useProps<T extends object, O = { [key in keyof T]?: FormatPropsType }, K = keyof O>(props: T, option: O): { [key in keyof O]: T[key] } {
     // @ts-ignore
     return undefined
 }
-
 
 const props = {
     name: 'hello world',
     age: 20,
     gender: true,
+    people: new People(),
 }
 
 const newProps = useProps(props, {
-
+    name: FormatPropsType.number,
+    gender: FormatPropsType.function,
+    people: FormatPropsType.promise,
 })
+
+newProps.people.sayHello()
+
+
+// newProps.name 是字符串
+// newProps.gender 是布尔值
+// newProps 没有age属性
 
