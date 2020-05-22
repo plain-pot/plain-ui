922c7d1097882c2d11353c268ce97bafa8baec7f
martsforever <martsforever@aliyun.com>
2020-05-22 11:30:29 +0800

    add color picker

---
+++ src/packages/color-picker/color-picker.tsx
@@ -0,0 +1,15 @@
+import {defineComponent} from "@vue/composition-api";
+
+export default defineComponent({
+    name: 'pl-color-picker',
+    props: {
+        
+    },
+    setup(props, context) {
+        return () => (
+            <div>
+                
+            </div>
+        )
+    },
+})
---
+++ src/packages/color-picker/index.ts
@@ -0,0 +1,7 @@
+import './color-picker.scss'
+import color-picker from './color-picker'
+import {installPlugin} from "@/util/install";
+
+export default installPlugin([
+    color-picker,
+])
