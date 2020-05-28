384ac746403b508a90a1f34b2b8045b60150ac85
韦胜健 <martsforever@aliyun.com>
2020-05-28 18:25:40 +0800

    实现了箭头

--- src-doc/page/test/test-svg.vue
+++ src-doc/page/test/test-svg.vue
@@ -1,7 +1,11 @@
 <template>
     <div class="test-svg">
         <demo-row>
-
+            <div class="wrapper">
+                wrapper
+                <div class="arrow-left"></div>
+                <div class="arrow-right"></div>
+            </div>
         </demo-row>
 
     </div>
@@ -21,24 +25,35 @@
 <style lang="scss">
     .test-svg {
         .wrapper {
-            height: 60px;
             width: 300px;
-            background-color: #f6f6f6;
+            height: 80px;
+            background-color: #12b4a5;
+            color: white;
+            display: inline-flex;
+            align-items: center;
+            padding: 0 16px;
+            box-sizing: border-box;
             position: relative;
+            box-shadow: 0 0 16px 0 #12b4a5;
 
-            .wrapper-top {
-                height: 50%;
+            .arrow-left, .arrow-right {
+                width: 0;
+                height: 0;
+                border: 40px solid transparent;
                 position: absolute;
-                top: 0;
-                left: 0;
-                right: 0;
-                background-color: #12b4a5;
             }
 
-            .wrapper-bottom {
-
+            .arrow-left {
+                border-right-color: #12b4a5;
+                border-left-width: 0;
+                right: 100%;
             }
 
+            .arrow-right {
+                border-left-color: #12b4a5;
+                border-right-width: 0;
+                left: 100%;
+            }
         }
     }
 </style>
