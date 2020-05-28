b9cf8a118c739474aa256558c44a6f4d8b6461a9
韦胜健 <martsforever@aliyun.com>
2020-05-28 18:01:21 +0800

    complete step

--- src-doc/menus.js
+++ src-doc/menus.js
@@ -52,7 +52,7 @@ export default [
             {name: 'Virtual Tree', title: '虚拟树', icon: 'pl-tree', page: '/normal/virtual-tree', complete: false},
             {name: 'Pagination', title: '分页', icon: 'pl-book2', page: '/normal/pagination', complete: false},
             {name: 'Badge', title: '标记', icon: 'pl-badge', page: '/normal/badge', complete: true},
-            {name: 'Step', title: '步骤条', icon: 'pl-step', page: '/normal/step', complete: false},
+            {name: 'Step', title: '步骤条', icon: 'pl-step', page: '/normal/step', complete: true},
             {name: 'Tab', title: '页签', icon: 'pl-tabs', page: '/normal/tab', complete: false},
             // {name: 'Transform', title: '穿梭框', icon: 'pl-exchange', page: '/normal/transform', complete: false},
         ]
