cbc0fb881cf078c5e832ef38969e152c33575e0d
韦胜健 <martsforever@aliyun.com>
2020-05-28 15:22:56 +0800

    complete carousel

--- src-doc/menus.js
+++ src-doc/menus.js
@@ -73,7 +73,7 @@ export default [
             {name: 'Popover', title: '弹出框', icon: 'pl-popper', page: '/normal/popover', complete: true},
             {name: 'Popper', title: '悬浮框', icon: 'pl-popper', page: '/normal/popper', complete: true},
             {name: 'Card', title: '卡片', icon: 'pl-card', page: '/normal/card', complete: true},
-            {name: 'Carousel', title: '轮播', icon: 'pl-carousel', page: '/normal/carousel', complete: false},
+            {name: 'Carousel', title: '轮播', icon: 'pl-carousel', page: '/normal/carousel', complete: true},
             {name: 'Collapse', title: '折叠面板', icon: 'pl-collapse', page: '/normal/collapse', complete: true},
             {name: 'Alert', title: '提示信息', icon: 'pl-collapse', page: '/normal/alert', complete: true},
             {name: 'Portal', title: '迁移DOM元素', icon: 'icon-scroll', page: '/normal/portal', complete: true},
