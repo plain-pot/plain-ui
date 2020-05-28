5b59653d4d95729c211d37fcb8ebcb794bb1d142
韦胜健 <martsforever@aliyun.com>
2020-05-28 12:13:30 +0800

    complete collapse

--- src-doc/menus.js
+++ src-doc/menus.js
@@ -74,7 +74,7 @@ export default [
             {name: 'Popper', title: '悬浮框', icon: 'pl-popper', page: '/normal/popper', complete: true},
             {name: 'Card', title: '卡片', icon: 'pl-card', page: '/normal/card', complete: true},
             {name: 'Carousel', title: '轮播', icon: 'pl-carousel', page: '/normal/carousel', complete: false},
-            {name: 'Collapse', title: '折叠面板', icon: 'pl-collapse', page: '/normal/collapse', complete: false},
+            {name: 'Collapse', title: '折叠面板', icon: 'pl-collapse', page: '/normal/collapse', complete: true},
             {name: 'Alert', title: '提示信息', icon: 'pl-collapse', page: '/normal/alert', complete: false},
             {name: 'Portal', title: '迁移DOM元素', icon: 'icon-scroll', page: '/normal/portal', complete: true},
         ]
