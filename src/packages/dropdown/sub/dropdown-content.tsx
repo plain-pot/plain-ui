cca5e96bcce9a932173c9c9f78cc0dad15a609d5
韦胜健 <martsforever@aliyun.com>
2020-05-29 15:08:53 +0800

    dropdown 默认没有高度

--- src/packages/dropdown/sub/dropdown-content.tsx
+++ src/packages/dropdown/sub/dropdown-content.tsx
@@ -7,9 +7,9 @@ export const DROPDOWN_CONTENT_PROVIDER = '@@DROPDOWN_CONTENT_PROVIDER'
 export default defineComponent({
     name: 'pl-dropdown-content',
     props: {
-        width: {type: [String, Number], default: 120},                                // popper 宽度
-        height: {type: [String, Number], default: 200},                               // popper高度
-        content: {type: Function},                                                    // 内容渲染函数
+        width: {type: [String, Number], default: 120},                                  // popper 宽度
+        height: {type: [String, Number]},                                               // popper高度
+        content: {type: Function},                                                      // 内容渲染函数
     },
     setup(props) {
 
@@ -40,7 +40,7 @@ export default defineComponent({
         return () => {
             return (
                 <div class="pl-dropdown-content" style={styles.value} onMouseenter={emit.enterPopper} onMouseleave={emit.leavePopper}>
-                    {props.width == null && props.height == null ? (
+                    {props.height == null ? (
                         !!props.content && props.content(ctx.$createElement)
                     ) : (
                         <pl-scroll>
