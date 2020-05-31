f2fe20d85601cfd41ce75eb2025f07b0ce40e75f
韦胜健 <martsforever@aliyun.com>
2020-05-31 23:36:29 +0800

    tabheader 基本结构

--- src-doc/page/normal/tab-header.vue
+++ src-doc/page/normal/tab-header.vue
@@ -11,18 +11,16 @@
         <demo-row title="位置">
             <demo-line title="position=top">
                 <pl-tab-header position="top">
-                    <pl-tab-header-item active>订单信息</pl-tab-header-item>
-                    <pl-tab-header-item>物理信息</pl-tab-header-item>
-                    <pl-tab-header-item>商品信息</pl-tab-header-item>
-                    <pl-tab-header-item>其他</pl-tab-header-item>
+                    <pl-tab-header-item v-for="item in ['订单信息','物理信息','商品信息','其他']" :key="item" :active="val[0] === item" @click="val[0] = item">
+                        {{item}}
+                    </pl-tab-header-item>
                 </pl-tab-header>
             </demo-line>
             <demo-line title="position-bottom">
                 <pl-tab-header position="bottom">
-                    <pl-tab-header-item active>订单信息</pl-tab-header-item>
-                    <pl-tab-header-item>物理信息</pl-tab-header-item>
-                    <pl-tab-header-item>商品信息</pl-tab-header-item>
-                    <pl-tab-header-item>其他</pl-tab-header-item>
+                    <pl-tab-header-item v-for="item in ['订单信息','物理信息','商品信息','其他']" :key="item" :active="val[0] === item" @click="val[0] = item">
+                        {{item}}
+                    </pl-tab-header-item>
                 </pl-tab-header>
             </demo-line>
         </demo-row>
