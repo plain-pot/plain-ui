32116aa17a189834eb73524e050ff3c67d4389f2
韦胜健 <martsforever@aliyun.com>
2020-06-03 21:44:44 +0800

    add time

---
+++ src/packages/time/index.ts
@@ -0,0 +1,7 @@
+import './time.scss'
+import time from './time'
+import {installPlugin} from "@/util/install";
+
+export default installPlugin([
+    time,
+])
---
+++ src/packages/time/time.tsx
@@ -0,0 +1,15 @@
+import {defineComponent} from "@vue/composition-api";
+
+export default defineComponent({
+    name: 'pl-time',
+    props: {
+        
+    },
+    setup(props) {
+        return () => (
+            <div>
+                
+            </div>
+        )
+    },
+})
