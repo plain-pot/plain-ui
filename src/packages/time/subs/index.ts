d8b8908575042238b93e21eff598b662b066bd8f
韦胜健 <martsforever@aliyun.com>
2020-06-04 11:33:20 +0800

    complete time base panel

--- src/packages/time/subs/index.ts
+++ src/packages/time/subs/index.ts
@@ -1,6 +1,8 @@
 import {installPlugin} from "@/util/install";
 import column from './time-base-column'
+import basePanel from './time-base-panel'
 
 export default installPlugin([
     column,
+    basePanel,
 ])
