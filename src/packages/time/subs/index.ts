9368b758498ecd3d94b64498de795df24a862a4b
韦胜健 <martsforever@aliyun.com>
2020-06-04 11:51:27 +0800

    基本完成range panel

--- src/packages/time/subs/index.ts
+++ src/packages/time/subs/index.ts
@@ -1,8 +1,10 @@
 import {installPlugin} from "@/util/install";
 import column from './time-base-column'
 import basePanel from './time-base-panel'
+import rangePanel from './time-range-panel'
 
 export default installPlugin([
     column,
     basePanel,
+    rangePanel,
 ])
