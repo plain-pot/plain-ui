c51928d6695c696275cfafedf5cdb0e46c71dea4
韦胜健 <martsforever@aliyun.com>
2020-06-10 09:34:10 +0800

    adjust useRefer: 避免在refer的时候，覆盖组件本身的属性

--- src/use/useRefer.ts
+++ src/use/useRefer.ts
@@ -1,7 +1,14 @@
 import {getCurrentInstance} from "@vue/composition-api";
 
-export function useRefer(ref: any) {
-    const ctx = getCurrentInstance()
-    Object.assign(ctx, ref)
+export function useRefer(refer: any) {
+    const ctx = getCurrentInstance() as any
+    Object.keys(refer).forEach(key => {
+        if (ctx[key] !== undefined) {
+            console.log({key, ctx, refer})
+            throw new Error(`useRefer error: key [${key}] is exist on context!`)
+        } else {
+            ctx[key] = refer[key]
+        }
+    })
     return ctx
 }
