21542be8edeb0e0da9a86ff45fd196c10898237c
韦胜健 <martsforever@aliyun.com>
2020-06-11 17:41:54 +0800

    mock data

--- src-doc/page/data/mock.js
+++ src-doc/page/data/mock.js
@@ -1,23 +1,36 @@
 const fs = require('fs')
 const path = require('path')
-
 const mock = require('mockjs').mock
 
-const length = 2000
+function generate() {
+    [
+        {file: 'data.json', num: 2000},
+        {file: 'data-1.json', num: 50},
+        {file: 'data-2.json', num: 200},
+    ].forEach((item) => {
+        fs.writeFileSync(path.resolve(__dirname, `./${item.file}`), JSON.stringify(
+            (mock({
+                [`array|${item.num}`]: [
+                    {
+                        "id|+1": 0,
+                        color: '@color',
+                        name: '@first',
+                        date: '@date',
+                        "star|1-10": '★',
+                        "size|40-80": 60,
+                        addr: '@county(true)',
+                        url: '@url',
+                        domain: '@domain',
+                        protocol: '@protocol',
+                        email: '@email',
+                        ip: '@ip',
+                    }
+                ]
+            })).array,
+            null,
+            2,
+        ))
+    })
+}
 
-fs.writeFileSync(path.resolve(__dirname, './data.json'), JSON.stringify(
-    (mock({
-        [`array|${length}`]: [
-            {
-                "id|+1": 0,
-                color: '@color',
-                name: '@first',
-                date: '@date',
-                "star|1-10": '★',
-                "size|40-80": 60
-            }
-        ]
-    })).array,
-    null,
-    2,
-))
+generate()
