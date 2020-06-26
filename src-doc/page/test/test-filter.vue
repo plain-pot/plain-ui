99a648b38ee08a5b75ab6dca71cf82f9f61a3f67
韦胜健 <martsforever@aliyun.com>
2020-06-26 09:40:59 +0800

    好像实现了异步过滤器的功能

--- src-doc/page/test/test-filter.vue
+++ src-doc/page/test/test-filter.vue
@@ -1,12 +1,66 @@
 <template>
     <div>
-        test-filter
+        <demo-row>
+            <demo-line>
+                <pl-button @click="changeValue">change value</pl-button>
+                <pl-tag>
+                    {{formatValue | plf}}
+                </pl-tag>
+                <pl-tag>
+                    {{num}}
+                </pl-tag>
+            </demo-line>
+        </demo-row>
     </div>
 </template>
 
 <script>
+
     export default {
-        name: "test-filter"
+        name: "test-filter",
+        filters: {
+            plf: (() => {
+                let counter = 0
+                let map = new WeakMap()
+                let data = window.Vue.observable({})
+
+                return (val) => {
+
+                    console.log('filter', counter)
+
+                    if (!!val && !!val.then && typeof val.then === "function") {
+                        let count = map.get(val)
+                        if (count != null) {
+                            return data[count]
+                        } else {
+                            count = ++counter
+                            map.set(val, count)
+                            window.Vue.prototype.$set(data, count, '加载中...')
+                        }
+                        val.then((asyncVal) => {
+                            data[count] = asyncVal
+                        })
+                        return data[count]
+                    }
+
+                    return `plf-${val}`
+                }
+            })(),
+        },
+        data() {
+            return {
+                num: 123,
+                formatValue: 123
+            }
+        },
+        methods: {
+            changeValue() {
+                this.formatValue = new Promise(async (resolve) => {
+                    await this.$plain.utils.delay(1000)
+                    resolve(++this.num)
+                })
+            },
+        }
     }
 </script>
 
