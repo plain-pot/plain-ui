af47984d33eb62bc5a6a52941bdfce5e87d4484b
韦胜健 <martsforever@aliyun.com>
2020-08-06 22:58:15 +0800

    - plc渲染内容的上下文是body-cell，以便能够获取到正确的provide； - 渲染内容可以通过 plc.ctx获取列组件实例，并且使用里面的变量以及方法；通过这种方式，实现 渲染内容之间的组件通信

--- src/packages/table/plc-components/plc-input.tsx
+++ src/packages/table/plc-components/plc-input.tsx
@@ -1,17 +1,52 @@
 import {definePlc} from "@/packages/table/plc-components/register";
 import {set} from "@vue/composition-api";
+import {TableRenderData} from "@/packages/table/plc/plc";
 
 export default definePlc({
     name: 'plc-input',
     props: {
         edit: {
             type: Function,
-            default: function ({plc, row}) {
-                console.log(this)
+            default: function ({plc, row}: TableRenderData) {
+
+                /*return (
+                    <pl-input
+                        value={row[plc.props.field!]}
+                        onInput={val => set(row, plc.props.field, val)}
+                        {...{
+                            on: {
+                                // @ts-ignore
+                                'click-input': () => plc.ctx!.onClickItem(this, plc)
+                            }
+                        }}
+                    />
+                )*/
+
                 return (
-                    <pl-input value={row[plc.props.field]} onInput={val => set(row, plc.props.field, val)}/>
+                    <pl-input value={row[plc.props.field!]} onInput={val => set(row, plc.props.field, val)}/>
                 )
             }
+        },
+    },
+    data() {
+        return {
+            selected: [
+                '123',
+                '4456'
+            ],
+        }
+    },
+    methods: {
+        addSelected() {
+            // @ts-ignore
+            this.selected.push(this.selected.length)
+        },
+        onClickItem(bodyCell, plc) {
+            // @ts-ignore
+            console.log(this.selected)
+            console.log({
+                bodyCell, plc
+            })
         },
     },
 })
