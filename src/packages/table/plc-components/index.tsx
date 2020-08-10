9fc9aebc36741008ca9945a12188cb6b0b4e0f5f
韦胜健 <martsforever@aliyun.com>
2020-08-10 19:43:43 +0800

    区分标准plc组件与扩展plc组件

--- src/packages/table/plc-components/index.tsx
+++ src/packages/table/plc-components/index.tsx
@@ -1,10 +1,10 @@
-import index from './plc-index'
-import toggle from './plc-toggle'
-import input from './plc-input'
-import number from './plc-number'
-import date from './plc-date'
-import color from './plc-color-picker'
-import rate from './plc-rate'
+import index from './standard/plc-index'
+import toggle from './external/plc-toggle'
+import input from './external/plc-input'
+import number from './external/plc-number'
+import date from './external/plc-date'
+import color from './external/plc-color-picker'
+import rate from './external/plc-rate'
 
 
 import {installPlugin} from "@/util/install";
