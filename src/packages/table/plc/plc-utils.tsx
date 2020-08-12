26a8e3d11a253850dda0f84e85bbcde40b2a4560
韦胜健 <martsforever@aliyun.com>
2020-08-12 10:09:54 +0800

    清理 plc 中不需要的属性

--- src/packages/table/plc/plc-utils.tsx
+++ src/packages/table/plc/plc-utils.tsx
@@ -35,25 +35,11 @@ export const PlcProps = {
     // search: {type: Boolean, default: true},                              // 可查询
     // searchType: {type: String, default: 'input'},                        // 查询类型
     // searchField: {type: String},                                         // 查询字段
-    //
     // sort: {type: Boolean, default: true},                                // 可排序
     // sortField: {type: String},                                           // 排序字段
-    //
-    // editableFunc: {type: Function},                                      // 是否可编辑判断函数
-    //
+
     // formatter: {type: Function},                                         // 文本格式化函数，支持异步格式化
-    // tooltip: {type: Boolean},                                            // 是否tooltip显示文本
-    // link: {type: Boolean},                                               // 是否以超链接的形式展示文本，并且点击的时候回派发事件
-    // showInDialog: {type: Boolean},                                       // 非编辑状态下是否点击后再dialog中显示
-    //
-    // required: {type: Boolean},                                           // 是否必输
-    // rules: {type: Array},                                                // 校验规则
-    //
-    // /*---------------------------------------不可配置信息-------------------------------------------*/
-    // scopedSlots: {type: Object},                                         // 作用域插槽
-    // renderNormal: {type: Function},                                      // 渲染函数:非编辑状态
-    // renderEdit: {type: Function},                                        // 渲染函数：编辑状态
-    // renderHead: {type: Function},                                        // 渲染函数：列头
+    // linkText: {type: Boolean},                                           // 是否以超链接的形式展示文本，并且点击的时候回派发事件
 }
 
 export const PLC_COLLECTOR = '@@PLC_COLLECTOR'
