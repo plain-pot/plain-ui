9c9b503c7419e7a79b6ffdd5d6e8eca0440af389
韦胜健 <martsforever@aliyun.com>
2020-08-12 11:22:57 +0800

    得到了 rowCenterEl这个dom对象

--- src/packages/table/plc-components/standard/plc-expand.tsx
+++ src/packages/table/plc-components/standard/plc-expand.tsx
@@ -1,5 +1,9 @@
 import {definePlc} from "@/packages/table/plc-components/register";
 import {PlcType, TableRenderData} from "@/packages/table/plc/plc";
+import {$plain} from "@/packages/base";
+import {inject} from "@vue/composition-api";
+import {TABLE_PROVIDER} from "@/packages/table/table-utils";
+import {PlainTable} from "@/packages/table/table/table";
 
 export default definePlc({
     name: 'plc-expand',
@@ -26,15 +30,45 @@ export default definePlc({
         },
         default: {
             type: Function,
-            default: function ({row, plc}: TableRenderData) {
-                const plcInstance = plc.ctx as any
+            default: function (tableRenderData: TableRenderData) {
+                const plcInstance = tableRenderData.plc.ctx as any
                 return (
-                    <pl-button icon="el-icon-arrow-down" mode="text"/>
+                    <pl-button icon="el-icon-arrow-down" mode="text" onClick={e => plcInstance.expandPlc.toggle(tableRenderData, e)}/>
                 )
             }
         },
     },
     setup() {
-        return {}
+
+        const table = inject(TABLE_PROVIDER) as PlainTable
+
+        const expandPlc = {
+            toggle: (renderData: TableRenderData, e: MouseEvent) => {
+                expandPlc.expand(renderData, e)
+            },
+            expand: (renderData: TableRenderData, e: MouseEvent) => {
+                let target = e.target as HTMLElement
+                while (!$plain.utils.hasClass(target, 'plt-row')) {
+                    target = target.parentNode as HTMLElement
+                }
+                const vid = target.getAttribute('vid')
+                const bodyCenterEl = table.refs.$el.querySelector('.plt-body-item.pl-table-item-fixed-center')
+                if (!bodyCenterEl) {
+                    console.error('内部错误，无法找到center table body！');
+                    return
+                }
+                const rowCenterEl = bodyCenterEl.querySelector(`[vid="${vid}"]`)
+                if (!rowCenterEl) {
+                    console.error('内部错误，无法找到center row center！');
+                    return;
+                }
+
+                console.log(rowCenterEl)
+            }
+        }
+
+        return {
+            expandPlc,
+        }
     },
 })
