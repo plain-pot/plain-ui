ed778592851331446cfc92416d017c027b47a4fd
韦胜健 <martsforever@aliyun.com>
2020-08-13 14:27:43 +0800

    好像固定在右侧以及底部有点问题

--- src/packages/scroll/scroll-sticky.tsx
+++ src/packages/scroll/scroll-sticky.tsx
@@ -2,6 +2,8 @@ import {computed, defineComponent, onBeforeUnmount, onMounted} from "@vue/compos
 import {useSlots} from "@/use/useSlots";
 import {injectScroll} from "@/packages/scroll/scroll";
 import {useRefs} from "@/use/useRefs";
+import {StyleType} from "@/types/utils";
+import {$plain} from "@/packages/base";
 
 export default defineComponent({
     name: 'pl-scroll-sticky',
@@ -15,8 +17,7 @@ export default defineComponent({
     setup(props) {
 
         const {slots} = useSlots()
-        const refs = useRefs()
-        const scroll = injectScroll()
+
 
         let offset = {
             top: 0,
@@ -29,63 +30,81 @@ export default defineComponent({
             height: 0,
         }
 
+        const ie = $plain.utils.ie()
+
         const classes = computed(() => {
             return [
                 'pl-scroll-sticky',
-                'pl-scroll-sticky-compatible',
+                {
+                    'pl-scroll-sticky-compatible': ie
+                }
             ]
         })
 
         const styles = computed(() => {
-            return {
-                zIndex: props.zIndex
+
+            const styles = {} as StyleType
+            styles.zIndex = String(props.zIndex)
+
+            if (!ie) {
+                if (props.top !== null) styles.top = $plain.utils.suffixPx(props.top)
+                if (props.left !== null) styles.left = $plain.utils.suffixPx(props.left)
+                if (props.right !== null) styles.right = $plain.utils.suffixPx(props.right)
+                if (props.bottom !== null) styles.bottom = $plain.utils.suffixPx(props.bottom)
             }
+
+            return styles
         })
 
-        function fresh(data: { scrollTop: number, scrollLeft: number }) {
-            const {scrollTop, scrollLeft} = data
+        if (ie) {
+            const refs = useRefs()
+            const scroll = injectScroll()
 
-            if (props.top != null) {
-                refs.$el.style.top = (Math.max(0, scrollTop - (offset.top - props.top))) + 'px'
+            function fresh(data: { scrollTop: number, scrollLeft: number }) {
+                const {scrollTop, scrollLeft} = data
+
+                if (props.top != null) {
+                    refs.$el.style.top = (Math.max(0, scrollTop - (offset.top - props.top))) + 'px'
+                }
+
+                if (props.bottom != null) {
+                    refs.$el.style.bottom = Math.max(0, offset.top + offset.height - content.height - scrollTop) + 'px'
+                }
+
+                if (props.left != null) {
+                    refs.$el.style.left = (Math.max(0, scrollLeft - (offset.left - props.left))) + 'px'
+                }
+
+                if (props.right != null) {
+                    refs.$el.style.right = Math.max(0, offset.left + offset.width - content.width - scrollLeft) + 'px'
+                }
             }
 
-            if (props.bottom != null) {
-                refs.$el.style.bottom = Math.max(0, offset.top + offset.height - content.height - scrollTop) + 'px'
+            function onScroll(e) {
+                fresh({
+                    scrollTop: e.target.scrollTop,
+                    scrollLeft: e.target.scrollLeft,
+                })
             }
 
-            if (props.left != null) {
-                refs.$el.style.left = (Math.max(0, scrollLeft - (offset.left - props.left))) + 'px'
-            }
+            scroll.on.scroll(onScroll)
 
-            if (props.right != null) {
-                refs.$el.style.right = Math.max(0, offset.left + offset.width - content.width - scrollLeft) + 'px'
-            }
-        }
+            onBeforeUnmount(() => scroll.off.scroll(onScroll));
 
-        function onScroll(e) {
-            fresh({
-                scrollTop: e.target.scrollTop,
-                scrollLeft: e.target.scrollLeft,
+            onMounted(() => {
+                setTimeout(() => {
+                    offset.top = refs.$el.offsetTop
+                    offset.left = refs.$el.offsetLeft
+                    offset.width = refs.$el.offsetWidth
+                    offset.height = refs.$el.offsetHeight
+
+                    content.width = scroll.state.hostWidth
+                    content.height = scroll.state.hostHeight
+
+                    fresh({scrollLeft: 0, scrollTop: 0})
+                }, 0)
             })
         }
-
-        scroll.on.scroll(onScroll)
-
-        onBeforeUnmount(() => scroll.off.scroll(onScroll));
-
-        onMounted(() => {
-            setTimeout(() => {
-                offset.top = refs.$el.offsetTop
-                offset.left = refs.$el.offsetLeft
-                offset.width = refs.$el.offsetWidth
-                offset.height = refs.$el.offsetHeight
-
-                content.width = scroll.state.hostWidth
-                content.height = scroll.state.hostHeight
-
-                fresh({scrollLeft: 0, scrollTop: 0})
-            }, 0)
-        })
 
         return () => {
             return (
