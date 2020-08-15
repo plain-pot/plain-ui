6b44017aee4aec3bc080f50eb8ad3652e7fd3a5d
韦胜健 <martsforever@aliyun.com>
2020-08-15 10:13:29 +0800

    表格校验以及校验样式

--- src/packages/table/table/body/body-cell.tsx
+++ src/packages/table/table/body/body-cell.tsx
@@ -1,15 +1,56 @@
-import {computed, defineComponent, getCurrentInstance} from "@vue/composition-api";
+import {computed, defineComponent, getCurrentInstance, provide} from "@vue/composition-api";
 import {getCellClass} from "@/packages/table/plc/plc-utils";
 import {PlcType} from "@/packages/table/plc/plc";
 import {TableNode} from "@/packages/table/table-bak/TableNode";
 import {PlcRender} from "@/packages/table/table-bak/render";
 import {injectTable} from "@/packages/table/table/table";
 import {getCellStyles} from "@/packages/table/plc/plc-fixed";
+import {FormTrigger, validateField} from "@/packages/form/validate";
+import {EditProvider} from "@/use/useEdit";
+import {useStyle} from "@/use/useStyle";
 
 interface BodyCellPropsType {
     plc: PlcType,
     rowData: TableNode,
     isSummary: boolean
+}
+
+function useFormItemEdit(props: BodyCellPropsType, table: ReturnType<typeof injectTable>): void {
+    const editComputed = computed(() => {
+        return {
+            disabled: null,
+            readonly: null,
+            loading: null,
+            editable: true,
+            onBlur: () => {
+                // console.log('blur', props.plc.props.field)
+            },
+            onChange: () => {
+                // console.log('change', props.plc.props.field)
+                const {rowData, plc: {props: {field}}} = props
+                if (!!field) {
+                    const {data, editRow, isEdit} = rowData
+                    const validateResult = rowData.validateResult || {}
+                    validateField(validateResult, table.validateConfigData.value.allRules, isEdit ? editRow : data, field!, FormTrigger.CHANGE).then(ret => {
+                        rowData.validateResult = validateResult
+                    })
+                }
+            }
+        }
+    })
+    provide(EditProvider, editComputed)
+
+    useStyle({
+        shape: 'none',
+        size: 'mini',
+        adjust: (result) => {
+            const {validateResult} = props.rowData
+            const {status} = result
+            result.status = status != null ? status : (!!props.plc.props.field && !!validateResult && validateResult[props.plc.props.field] != null ? 'error' : (status || null))
+
+            return result
+        }
+    })
 }
 
 export default defineComponent({
@@ -24,6 +65,7 @@ export default defineComponent({
 
         const table = injectTable()
         const ctx = getCurrentInstance()!
+        useFormItemEdit(props, table)
 
         const renderData = computed(() => PlcRender.body({
             ...props,
