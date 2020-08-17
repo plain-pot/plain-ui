b4e3f67644a24650cbae30d99a566608137b7f9f
韦胜健 <martsforever@aliyun.com>
2020-08-17 16:30:09 +0800

    adjust checkbox intermediate styles

--- src/packages/checkbox/pl-checkbox-inner.vue
+++ src/packages/checkbox/pl-checkbox-inner.vue
@@ -1,7 +1,7 @@
 <template>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" class="pl-checkbox-inner" :class="classes">
         <polyline points="22,50 45,75 75,25" v-if="status === 'check'" class="pl-checkbox-inner-check-polyline"/>
-        <polyline points="20,50 80,50" v-if="status === 'minus'" class="pl-checkbox-inner-minus-polyline"/>
+        <rect v-if="status === 'minus'" x="20" y="20" width="60" height="60" class="pl-checkbox-inner-minus-polyline"/>
     </svg>
 </template>
 
