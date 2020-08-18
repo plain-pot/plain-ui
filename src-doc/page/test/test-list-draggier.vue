fa1408e5c670e99340a896e57212f37f8bc60c14
韦胜健 <martsforever@aliyun.com>
2020-08-19 00:05:38 +0800

    先做一版有动画的排序吧，虽然不支持虚拟列表

--- src-doc/page/test/test-list-draggier.vue
+++ src-doc/page/test/test-list-draggier.vue
@@ -1,23 +1,60 @@
 <template>
     <div class="test-list-draggier">
-        test-list-draggier
+        <div class="list">
+            <div class="item" v-for="item in state.data" :key="item.id" @mousedown="handler.onMousedownDraggier">
+                <pl-button
+                        icon="el-icon-rank"
+                        size="normal"
+                        mode="text"
+                        class="plc-draggier-handler"
+                />
+                {{item.name}}
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
     import {useListDraggier} from "../../../src/packages/table/plc-components/standard/draggier/use-list-draggier";
+    import data from '../data/data-1'
+    import {reactive} from "@vue/composition-api";
 
     export default {
         name: "test-list-draggier",
         setup() {
 
-            const {} = useListDraggier()
+            const state = reactive({
+                data,
+            })
 
-            return {}
+            const {handler} = useListDraggier({
+                rowClass: 'item'
+            })
+
+            return {
+                state,
+                handler,
+            }
         },
     }
 </script>
 
 <style lang="scss">
+
+    .test-list-draggier {
+        .list {
+            height: 400px;
+            overflow: auto;
+
+            .item {
+                height: 40px;
+                border-bottom: solid 1px #f2f2f2;
+                box-sizing: border-box;
+                display: flex;
+                align-items: center;
+                background: white;
+            }
+        }
+    }
 
 </style>
--- src/packages/table/plc-components/standard/draggier/use-list-draggier.ts
+++ src/packages/table/plc-components/standard/draggier/use-list-draggier.ts
@@ -1,5 +1,53 @@
-export function useListDraggier() {
+import {$plain} from "@/packages/base";
 
-    return {}
+function isItem(rowEl: HTMLElement, rowClass: string): boolean {
+    return $plain.utils.hasClass(rowEl, rowClass)
+}
+
+export function useListDraggier(
+    {
+        rowClass,
+    }: {
+        rowClass: string
+    }
+) {
+
+    const state = {
+        startY: 0,                                          // 在mousedown draggier的时候，鼠标的clientY初始值；
+        rowEl: null as null | HTMLElement,                  // mousedown 的row的dom对象
+        rowElList: [] as HTMLElement[],                     // row dom对象数组
+    }
+
+    const handler = {
+        onMousedownDraggier: (e: MouseEvent) => {
+            state.startY = e.clientY
+            let rowEl = e.target as HTMLElement
+            while (!!rowEl && !isItem(rowEl, rowClass)) {
+                rowEl = rowEl.parentNode as HTMLElement
+            }
+            if (!rowEl) {
+                throw new Error(`can't find item element!`)
+            }
+            state.rowEl = rowEl
+            rowEl.style.boxShadow = `0 0px 10px #eeeeee`
+            // @ts-ignore
+            state.rowElList = Array.from(rowEl.parentNode.querySelectorAll(`.${rowClass}`) as HTMLElement[])
+
+            document.addEventListener('mousemove', handler.onMousemoveDocument)
+            document.addEventListener('mouseup', handler.onMouseupDocument)
+        },
+        onMousemoveDocument: (e: MouseEvent) => {
+            const durY = e.clientY - state.startY
+            state.rowEl!.style.transform = `translateY(${durY}px)`
+        },
+        onMouseupDocument: () => {
+            document.removeEventListener('mousemove', handler.onMousemoveDocument)
+            document.removeEventListener('mouseup', handler.onMouseupDocument)
+        }
+    }
+
+    return {
+        handler,
+    }
 
 }
