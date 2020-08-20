d817725cf65c7efca2003f4ebac16b9057b8e4c2
韦胜健 <martsforever@aliyun.com>
2020-08-20 22:44:52 +0800

    基本完成了use-list-draggier的功能，但是有个瑕疵，就是拖拽的行没法覆盖他的后续兄弟节点

--- src/packages/table/plc-components/standard/draggier/use-list-draggier.ts
+++ src/packages/table/plc-components/standard/draggier/use-list-draggier.ts
@@ -41,9 +41,11 @@ export function useListDraggier(
     {
         rowClass,
         onChange,
+        dragClass,
     }: {
         rowClass: string,                                                           // 行的class，要确保只有行所在的dom对象有这个class，其子节点是没有这个class的
         onChange: (start: number, end: number) => void | Promise<void>,             // 拖拽导致排序变化动作
+        dragClass?: string,
     }
 ) {
 
@@ -57,6 +59,7 @@ export function useListDraggier(
 
         dragEl: null as null | HTMLElement,         // 拖拽的时候的dragEl的dom对象
         dragHeight: 0,                              // 拖拽的时候的dragEl高度，当在下方移动时，下方需要移动的rowEl都应该往上偏移 dragHeight距离，在上方移动时，上方需要移动的rowEl需要往下偏移 dragHeight距离
+        dragOffsetTop: 0,
 
         scrollParent: null as null | HTMLElement,   // 可以滚动的父元素
         dragStartScrollTop: 0,                      // 拖拽开始的时候，scrollParent 的scrollTop位置
@@ -65,15 +68,16 @@ export function useListDraggier(
         rowList: [] as HTMLElement[],               // dragEl的兄弟节点，包含dragEl
     }
 
-    /*const utils = {
-        /!**
+
+    const utils = {
+        /**
          * 根据startIndex以及endIndex，设置这个index范围内的row的dom对象进行上下平移；
          * 如果startIndex大于endIndex，则范围内的row对象，除了startIndex，其他的应该向上平移；
          * 反之，如果startIndex小于endIndex，则范围内的row对象，除了startIndex，其他的应该向下平移；
          *
          * @author  韦胜健
          * @date    2020/8/19 21:09
-         *!/
+         */
         refresh() {
             const {dragHeight, startIndex, endIndex} = state
             // 是否为向下移动
@@ -91,11 +95,12 @@ export function useListDraggier(
                 el.style.transform = `translateY(${movedown ? '-' : ''}${dragHeight}px)`
             })
         },
-    }*/
-
-    const utils = {
         refreshDragElPosition() {
             state.dragEl!.style.transform = `translateY(${state.moveClientY - state.startClientY + state.dragScrollTop}px)`
+            const top = (state.moveClientY - state.startClientY) + state.dragScrollTop + state.dragOffsetTop
+            state.endIndex = Math.ceil(Math.max(0, top / state.dragHeight - 0.5))
+
+            utils.refresh()
         },
     }
 
@@ -104,55 +109,28 @@ export function useListDraggier(
 
             $plain.disableSelect()
 
-
             state.startClientY = e.clientY
             state.dragEl = getRowEl(e, rowClass)
             state.dragHeight = state.dragEl.offsetHeight
+            state.dragOffsetTop = state.dragEl.offsetTop
             state.rowList = getRowElList(state.dragEl!, rowClass)
             state.startIndex = state.rowList.indexOf(state.dragEl)
             state.scrollParent = getScrollParent(state.dragEl)
             state.dragStartScrollTop = state.scrollParent!.scrollTop
-            state.scrollParent!.addEventListener('scroll', e => {
-                state.dragScrollTop = state.scrollParent!.scrollTop - state.dragStartScrollTop
-                utils.refreshDragElPosition()
-            })
 
+            state.scrollParent!.addEventListener('scroll', handler.parentScroll)
             document.addEventListener('mousemove', handler.mousemove)
             document.addEventListener('mouseup', handler.mouseup)
 
-            state.rowList.forEach((rowEl: any, index) => {
+            !!dragClass && $plain.utils.addClass(state.dragEl!, dragClass);
 
+            state.rowList.forEach((rowEl: any) => {
                 // 如果是当前拖拽的el，则不监听事件，不做任何处理
                 if (rowEl === state.dragEl) {
                     return
                 }
-
                 // 兄弟节点自动进行上下平移
                 rowEl.style.transition = `transform 300ms cubic-bezier(0.23, 1, 0.32, 1)`
-
-                const mouseenter = (e: MouseEvent) => {
-                    // e.movementY > 0 表示从上往下进入rowEl
-                    if (state.startIndex > index) {
-                        /*
-                        *  在startIndex上方拖拽移动的时候，如果是从上方进入row，那么应该是index+1，如果是从下方进入row，那么目标index就是进入的这个row的index；
-                        *  跟refresh中的写法有关；
-                        */
-                        state.endIndex = e.movementY > 0 ? index + 1 : index
-                    } else {
-                        /*
-                        *  在startIndex下方拖拽移动的时候，如果是从上方进入row，那么就是index，否则从下面进入row的时候，就是index-1
-                        *  跟refresh中的写法有关
-                        */
-                        state.endIndex = e.movementY > 0 ? index : index - 1
-                    }
-                    // utils.refresh()
-                }
-
-                rowEl.__draggier = {
-                    mouseenter,
-                }
-
-                rowEl.addEventListener('mouseenter', rowEl.__draggier.mouseenter)
             })
         },
         mousemove: (e: MouseEvent) => {
@@ -161,27 +139,39 @@ export function useListDraggier(
         },
         mouseup: async () => {
 
-            await onChange(state.startIndex, state.endIndex)
             await $plain.nextTick()
             $plain.enableSelect()
 
-            state.dragEl!.style.pointerEvents = ''
             state.dragEl!.style.transform = ''
-
+            !!dragClass && $plain.utils.removeClass(state.dragEl!, dragClass);
             document.removeEventListener('mousemove', handler.mousemove)
             document.removeEventListener('mouseup', handler.mouseup)
+            state.scrollParent!.removeEventListener('scroll', handler.parentScroll)
 
-            state.rowList.forEach((rowEl: any, index) => {
-
+            state.rowList.forEach((rowEl: any) => {
                 if (rowEl === state.dragEl) {
                     return
                 }
-
-                rowEl.removeEventListener('mouseenter', rowEl.__draggier.mouseenter)
-
-                rowEl.style.transition = ``
-                rowEl.style.transform = ``
+                rowEl.style.transition = ''
+                rowEl.style.transform = ''
             })
+
+            state.startClientY = 0
+            state.moveClientY = 0
+            state.dragEl = null
+            state.dragHeight = 0
+            state.dragOffsetTop = 0
+            state.scrollParent = null
+            state.dragStartScrollTop = 0
+            state.dragScrollTop = 0
+            state.rowList = []
+            await onChange(state.startIndex, state.endIndex)
+            state.startIndex = 0
+            state.endIndex = 0
+        },
+        parentScroll: () => {
+            state.dragScrollTop = state.scrollParent!.scrollTop - state.dragStartScrollTop
+            utils.refreshDragElPosition()
         }
     }
 
