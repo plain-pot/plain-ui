950a9685fdee7ffbce94fd783f67228a32d75d16
韦胜健 <martsforever@aliyun.com>
2020-08-27 20:30:26 +0800

    demo col draggable

--- src-doc/page/table/table-col-draggable.vue
+++ src-doc/page/table/table-col-draggable.vue
@@ -1,12 +1,29 @@
 <template>
     <div class="table-col-draggable">
-        table-col-draggable
+        <demo-row title="基本用法">
+            <pl-table :data="data">
+                <plc field="id" title="编号"/>
+                <plc field="size" title="大小"/>
+                <plc field="date" title="日期"/>
+                <plc field="color" title="颜色"/>
+                <plc field="name" title="名称"/>
+                <plc field="star" title="评分"/>
+            </pl-table>
+        </demo-row>
     </div>
 </template>
 
 <script>
+
+    import data from "../data/data-1.json";
+
     export default {
-        name: "table-col-draggable"
+        name: "table-col-draggable",
+        data() {
+            return {
+                data,
+            }
+        },
     }
 </script>
 
