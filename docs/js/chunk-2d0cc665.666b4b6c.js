f3f8852d9bbf0b85fda13f579e15d3d1c1146a09
韦胜健 <martsforever@aliyun.com>
2020-09-03 19:50:44 +0800

    build

--- docs/js/chunk-2d0cc665.666b4b6c.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0cc665"],{"4e86":function(n,t,e){"use strict";e.r(t);var u=function(){var n=this,t=n.$createElement,e=n._self._c||t;return e("div",[n._v("\n    test\n")])},c=[],a={name:"test",data:function(){return{agent:null}},mounted:function(){}},l=a,o=e("2877"),r=Object(o["a"])(l,u,c,!1,null,null,null);t["default"]=r.exports}}]);
-//# sourceMappingURL=chunk-2d0cc665.666b4b6c.js.map
