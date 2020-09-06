eba93f4e388a9b203877f478b1360015f1c94f72
韦胜健 <martsforever@aliyun.com>
2020-09-06 08:39:09 +0800

    表格行拖拽排序列以及树形列使用 useDraggierAutoScroller

--- src-doc/page/table/table-col-draggable.vue
+++ src-doc/page/table/table-col-draggable.vue
@@ -3,6 +3,16 @@
         <demo-row title="基本用法">
             <pl-table :data="data">
                 <plc field="id" title="编号"/>
+                <plc-group title="分组一">
+                    <plc field="size" title="大小"/>
+                    <plc field="date" title="日期"/>
+                </plc-group>
+                <plc field="color" title="颜色"/>
+                <plc-group title="分组二">
+                    <plc field="name" title="名称"/>
+                    <plc field="star" title="评分"/>
+                </plc-group>
+                <plc field="id" title="编号"/>
                 <plc field="size" title="大小"/>
                 <plc field="date" title="日期"/>
                 <plc field="color" title="颜色"/>
