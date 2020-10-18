db7d2b7f3d1928efaa4f3966edaa58b4bdb18038
韦胜健 <martsforever@aliyun.com>
2020-10-18 10:19:13 +0800

    app menu and app content

--- story/app.vue
+++ story/app.vue
@@ -1,21 +1,84 @@
 <template>
-    <div>
-        STORY
-    </div>
+    <article class="app" :style="{paddingLeft:`${config.menuSize}px`,paddingTop:`${config.headSize}px`}">
+        <section class="app-head" :style="{height:config.headSize+'px'}">
+            <span>{{title}}</span>
+        </section>
+        <app-menu :style="{width:`${config.menuSize}px`,top:`${config.headSize}px`}"/>
+        <section class="app-content">
+            this is app-content
+        </section>
+    </article>
 </template>
 
 <script>
     import {defineComponent} from 'vue';
+    import AppMenu from './app/app-menu'
+
+    const config = {
+        menuSize: 300,
+        headSize: 60,
+    }
 
     export default defineComponent({
         name: 'App',
+        components: {AppMenu},
+        data() {
+            return {
+                config,
+                title: 'plain-ui'
+            }
+        },
     });
 </script>
 
 <style lang="scss">
+    html, body {
+        margin: 0;
+        padding: 0;
+    }
+
     #app {
         font-family: Avenir, Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+    }
+
+    .app {
+        .app-head {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 0 40px;
+            box-shadow: $boxshadow;
+            box-sizing: border-box;
+        }
+
+        .app-menu {
+            position: fixed;
+            left: 0;
+            bottom: 0;
+            box-shadow: $boxshadow;
+            padding: 20px 0;
+            box-sizing: border-box;
+        }
+
+        .app-content {
+            box-sizing: border-box;
+            padding: 20px;
+        }
+    }
+
+    @include theme {
+        .app .app-head {
+            color: $colorPrimary;
+            font-size: 24px;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+            font-style: italic;
+            font-weight: bold;
+        }
     }
 </style>
---
+++ story/app/app-menu.vue
@@ -0,0 +1,15 @@
+<template>
+    <section class="app-menu">
+        this is app-menu
+    </section>
+</template>
+
+<script>
+    export default {
+        name: "app-menu"
+    }
+</script>
+
+<style lang="scss">
+
+</style>
