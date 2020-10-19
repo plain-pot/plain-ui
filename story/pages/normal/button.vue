35fec94af905d4c837013033e60759a84440033f
韦胜健 <martsforever@aliyun.com>
2020-10-19 09:12:04 +0800

    可以使用 dart-sass

--- story/pages/normal/button.vue
+++ story/pages/normal/button.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="demo-button">
-
-        demo-button
-
+        <div v-for="item in ['primary','success','warn','error','info']" :key="item" :class="`pl-button pl-button-status-${item}`">
+            {{item}}
+        </div>
     </div>
 </template>
 
@@ -14,4 +14,12 @@
 </script>
 
 <style lang="scss">
+    @include theme {
+        .pl-button {
+            @include statusMixin(button) {
+                background-color: $value;
+                color: white;
+            }
+        }
+    }
 </style>
