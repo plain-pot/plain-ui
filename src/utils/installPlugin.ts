51aa76877f3c6d3994235e33cbcc7602941e1026
韦胜健 <martsforever@aliyun.com>
2020-10-25 13:05:19 +0800

    build: adjust installPlugin

--- src/utils/installPlugin.ts
+++ src/utils/installPlugin.ts
@@ -8,6 +8,7 @@ export function installPlugin(
     plugins?: ComponentPlugin[]
 ) {
     return {
+        ...component,
         install(app: App) {
             app.component(component.name, component);
             !!plugins && plugins.forEach(app.use)
