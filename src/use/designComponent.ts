65143b91860e443a2411a54ab8de16eff43358cc
韦胜健 <martsforever@aliyun.com>
2020-10-28 11:02:29 +0800

    bug: 通过判断 ctx.ctx._ 是否存在，来判断vue是production还是development

--- src/use/designComponent.ts
+++ src/use/designComponent.ts
@@ -2,11 +2,8 @@ import {Component, ComponentPropsOptions, defineComponent, Directive, ExtractPro
 import {ComponentEvent, getComponentEmit, useEvent} from "./useEvent";
 import {createError} from "../utils/createError";
 import {renderNothing} from "../utils/renderNothing";
-import * as Vue from 'vue'
 
 const error = createError('designComponent')
-
-console.log(Vue);
 
 export function designComponent<PropsOptions extends Readonly<ComponentPropsOptions>, Props extends Readonly<ExtractPropTypes<PropsOptions>>, Emits extends { [k: string]: (...args: any[]) => void }, Refer, >(
     options: {
@@ -65,13 +62,9 @@ export function designComponent<PropsOptions extends Readonly<ComponentPropsOpti
 
                 return {
                     get value() {
-                        const flag = process.env.NODE_ENV === 'production'
-                        console.log('flag',flag)
-                        if (flag) {
-                            console.log('is production')
+                        if (!!ctx.ctx._) {
                             return ctx.ctx._.refs[refName]._._refer as Refer || null
                         } else {
-                            console.log('not production')
                             return ctx.refs[refName].$._refer as Refer || null
                         }
                     }
