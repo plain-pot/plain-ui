f6aab6727f7fb9c4701f9aa0a61c1967badbe8d4
韦胜健 <martsforever@aliyun.com>
2020-10-29 15:20:55 +0800

    adjust publicPath

--- build/doc/index.js
+++ build/doc/index.js
@@ -2,7 +2,7 @@ const $utils = require('../build.utils')
 const buildConfig = require('../build.config')
 
 module.exports = {
-    publicPath: './',
+    publicPath: '/plain-ui/',
     devServer: {port: '3334'},
     outputDir: $utils.resolve('docs'),
     pages: {
