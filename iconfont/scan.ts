4a31c85d0970744082325399cff3622ceb89ccc5
韦胜健 <martsforever@aliyun.com>
2020-11-01 12:31:27 +0800

    scan icons

--- iconfont/scan.ts
+++ iconfont/scan.ts
@@ -23,8 +23,10 @@ function scan(config: {
         match = reg.exec(content)
     }
 
-    const icon = icons[0]!
-    fs.writeFileSync(path.join(config.outputDir, `${icon.id}.js`), `export default \`<svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">${icons[0].path}</svg>\``)
+    icons.forEach(icon => {
+        fs.writeFileSync(path.join(config.outputDir, `${icon.id}.js`), `export default \`<svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg">${icons[0].path}</svg>\``)
+    })
+    fs.writeFileSync(path.join(config.outputDir, 'index.js'), `export default ${JSON.stringify(icons.map(i => i.id))}`)
 }
 
 
