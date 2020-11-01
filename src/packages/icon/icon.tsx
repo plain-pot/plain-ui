e374d5c17c677228e8e27dc24620c4a65043d6fb
韦胜健 <martsforever@aliyun.com>
2020-11-01 12:56:47 +0800

    生成的图标有点问题

--- src/packages/icon/icon.tsx
+++ src/packages/icon/icon.tsx
@@ -1,13 +1,30 @@
 import {designComponent} from "../../use/designComponent";
-import {ref, watch, computed} from 'vue';
+import {markRaw, ref, watch} from 'vue';
 import './icon.scss'
-import {StyleProperties} from "../../shims";
+import {VNodeChild} from "../../shims";
+
+interface IconGetter {
+    (icon: string): VNodeChild | Promise<VNodeChild>
+}
 
 const RegistryIcons = {
-    registry: (prefix: string,) => {
-        return null
+    icons: [] as { prefix: string, getter: IconGetter }[],
+    registry(prefix: string, getter: IconGetter) {
+        this.icons.unshift({prefix, getter})
     }
 }
+
+RegistryIcons.registry('el-icon-', async (icon) => {
+    console.log('./icons/' + icon)
+    const module = await import('./icons/' + icon)
+    return (
+        <svg viewBox="0 0 1024 1024"
+             version="1.1"
+             xmlns="http://www.w3.org/2000/svg"
+             innerHTML={module.default}
+        />
+    )
+})
 
 export default designComponent({
     name: 'pl-icon',
@@ -17,11 +34,22 @@ export default designComponent({
     },
     setup({props}) {
 
-        const icon = ref(null as null | string)
+        const icon = ref(null as any)
 
         const utils = {
+            getIconVNode: async (iconName: string) => {
+                for (let i = 0; i < RegistryIcons.icons.length; i++) {
+                    const registryIcon = RegistryIcons.icons[i];
+                    if (iconName.indexOf(registryIcon.prefix) === 0) {
+                        return await registryIcon.getter(iconName)
+                    }
+                }
+                return null
+            },
             reset: async (iconName: string) => {
-                return
+                const Icon = await utils.getIconVNode(iconName)
+                console.log('Icon', iconName, Icon)
+                icon.value = !!Icon ? markRaw(Icon) : null
             }
         }
 
@@ -29,11 +57,8 @@ export default designComponent({
 
         return {
             render: () => {
-                return (
-                    <i class="pl-icon"
-                       {...{innerHTML: '<button>hello world</button>'}}
-                    />
-                )
+                const {value: Icon} = icon
+                return !!Icon ? <Icon/> : null
             }
         }
     },
--- story/pages/normal/icon.vue
+++ story/pages/normal/icon.vue
@@ -2,8 +2,14 @@
     <div class="demo-icon">
         <div style="font-size: 40px;color: #4caf50">
             <span>普通的文本：NORMAL normal</span>
-            <pl-icon icon="add-box-fill"/>
+            <pl-icon icon="el-icon-alarm-clock"/>
+            <pl-icon icon="el-icon-arrow-down"/>
         </div>
+        <button @click="showFlag = true">
+            应用图标
+            <pl-icon icon="apps-line" v-if="showFlag"/>
+        </button>
+
     </div>
 </template>
 
