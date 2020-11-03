0eca4a218ff1b1027650a5385c38777cc89538d5
韦胜健 <martsforever@aliyun.com>
2020-11-03 17:04:59 +0800

    update: 优化plugin的安装方式，确保plugin只安装一次

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import {App} from "vue"
 import {ComponentPlugin} from "./shims";
+import {installPlugin} from "./utils/installPlugin";
 
 import Button from "./packages/button";
 import Input from "./packages/input";
 import Icon from './packages/icon';
 import Loading from './packages/loading'
 import LoadingMask from './packages/loading-mask'
-import {installPlugin} from "./utils/installPlugin";
 
 function install(app: App) {
     installPlugin(app, plugins)
