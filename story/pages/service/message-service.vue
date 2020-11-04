b6f720678bc1a416689baba726d79f5a793aaaf3
韦胜健 <martsforever@aliyun.com>
2020-11-04 14:55:10 +0800

    准备实现 ServiceRegistry

---
+++ story/pages/service/message-service.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="message-service">
+        message-service
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "message-service"
+    }
+</script>
+
+<style lang="scss">
+
+</style>
