2eb52a280248c49c402d42ccf4449cd5022e4c6e
韦胜健 <martsforever@aliyun.com>
2020-11-06 15:47:15 +0800

    在实现 loading-mask 指令之前，先把dialog服做了

---
+++ src/packages/loading-directive/loading-directive.ts
@@ -0,0 +1,19 @@
+import {App, Directive} from 'vue';
+
+const LoadingDirective: Directive = {
+    mounted() {
+
+    },
+    updated() {
+
+    },
+    beforeUnmount() {
+
+    },
+}
+
+export default {
+    install: (app: App) => {
+
+    }
+}
