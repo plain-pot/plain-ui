05b65132b5adc2c38c7e456196dfedb3ac9c010a
韦胜健 <martsforever@aliyun.com>
2020-11-06 17:49:12 +0800

    按钮文本字体有点偏下

--- story/app.vue
+++ story/app.vue
@@ -42,16 +42,6 @@
 </script>
 
 <style lang="scss">
-    html, body {
-        margin: 0;
-        padding: 0;
-    }
-
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-    }
 
     .app {
         .app-head {
