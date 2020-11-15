2d5f0a1657e4dae3040b9cde3d7374b3cde4e028
韦胜健 <martsforever@aliyun.com>
2020-11-15 12:09:58 +0800

    update: 实现了虚拟列表的动态高度的功能

--- story/menu.ts
+++ story/menu.ts
@@ -68,7 +68,7 @@ export const MENUS: MenuGroup[] = [
     {
         name: '视图', children: [
             {name: 'List', title: '列表', page: '/normal/list', complete: true},
-            {name: 'VirtualList', title: '虚拟列表', page: '/normal/virtual-list', complete: false},
+            {name: 'VirtualList', title: '虚拟列表', page: '/normal/virtual-list', complete: true},
             {name: 'Progress', title: '进度条', page: '/normal/progress', complete: false},
             {name: 'Tree', title: '树形组件', page: '/normal/tree', complete: false},
             {name: 'Virtual Tree', title: '虚拟树', page: '/normal/virtual-tree', complete: false},
