b84fb48b1a9a7b504714d048cacd4104222bee48
韦胜健 <martsforever@aliyun.com>
2020-11-18 10:48:23 +0800

    update:adjust  dropdown group style

--- story/pages/normal/dropdown.vue
+++ story/pages/normal/dropdown.vue
@@ -160,6 +160,32 @@
                     </pl-dropdown-menu>
                 </template>
             </pl-dropdown>
+
+            <pl-dropdown>
+                <pl-button>无标题（仅有分割线）</pl-button>
+                <template #popper>
+                    <pl-dropdown-menu>
+                        <pl-dropdown-group>
+                            <pl-dropdown-option label="新建 Create" @click="$message('新建')" icon="el-icon-document-add"/>
+                            <pl-dropdown-option label="编辑 Edit" @click="$message('编辑')" disabled icon="el-icon-document"/>
+                            <pl-dropdown-option label="删除 Delete" @click="$message('删除')" icon="el-icon-document-remove"/>
+                        </pl-dropdown-group>
+                        <pl-dropdown-group>
+                            <pl-dropdown-option label="导入 Import" @click="$message('导入')" disabled icon="el-icon-download"/>
+                            <pl-dropdown-option label="导出 Export" @click="$message('导出')" icon="el-icon-upload1"/>
+                        </pl-dropdown-group>
+                        <pl-dropdown-group>
+                            <pl-dropdown-option label="筛选 Filter" @click="$message('筛选')" disabled icon="el-icon-crop"/>
+                            <pl-dropdown-option label="排序 Sort" @click="$message('排序')" icon="el-icon-s-fold"/>
+                        </pl-dropdown-group>
+                        <pl-dropdown-group>
+                            <pl-dropdown-option label="多选 Select" @click="$message('多选')"/>
+                            <pl-dropdown-option label="设置 Config" @click="$message('多选')"/>
+                        </pl-dropdown-group>
+                    </pl-dropdown-menu>
+                </template>
+            </pl-dropdown>
+
         </demo-row>
 
         <demo-row title="指示图标">
@@ -245,6 +271,27 @@
                     </pl-dropdown-menu>
                 </template>
             </pl-dropdown>
+        </demo-row>
+
+        <demo-row title="无根节点">
+            <pl-button-group>
+                <pl-button label="新建" icon="el-icon-plus"/>
+                <pl-button label="编辑" icon="el-icon-edit"/>
+                <pl-button label="删除" icon="el-icon-delete"/>
+                <pl-dropdown>
+                    <pl-button>
+                        <span>更多</span>
+                        <pl-icon icon="el-icon-arrow-down"/>
+                    </pl-button>
+                    <template #popper>
+                        <pl-dropdown-menu>
+                            <pl-dropdown-option label="高级筛选" @click="$message('高级查询')" icon="el-icon-search"/>
+                            <pl-dropdown-option label="高级排序" @click="$message('高级排序')" icon="el-icon-sort"/>
+                            <pl-dropdown-option label="批量删除" @click="$message('批量删除')" icon="el-icon-folder-delete"/>
+                        </pl-dropdown-menu>
+                    </template>
+                </pl-dropdown>
+            </pl-button-group>
         </demo-row>
     </div>
 </template>
