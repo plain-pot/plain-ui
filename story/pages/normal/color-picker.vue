2630dc9ea8b3b06a2d59f6b37abc662f45c618b7
韦胜健 <martsforever@aliyun.com>
2020-11-23 19:54:04 +0800

    update:  用class来管理好像是没问题的

---
+++ story/pages/normal/color-picker.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="color-picker">
+        <demo-row title="基本用法">
+            <pl-input v-model="state.text"/>
+            <pl-input v-model="state.myDate.year"/>
+            {{state.myDate.displayString}}
+        </demo-row>
+    </div>
+</template>
+
+<script>
+
+    class MyDate {
+        year;
+        month;
+        day;
+
+        get displayString() {
+            console.log('displayString')
+            return `${this.year}-${this.month}-${this.day}`
+        }
+
+        constructor(year, month, day) {
+            this.year = year
+            this.month = month
+            this.day = day
+        }
+    }
+
+    import {reactive, watch} from 'vue'
+
+    export default {
+        name: "color-picker",
+        setup() {
+            const state = reactive({
+                text: 'hello world',
+                myDate: new MyDate(2020, 11, 20)
+            })
+
+            watch(() => state.myDate.displayString, val => {
+                console.log('displayString change', val)
+            })
+
+            return {
+                state,
+            }
+        },
+    }
+</script>
+
+<style lang="scss">
+
+</style>
