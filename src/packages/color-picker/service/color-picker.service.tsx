a30bf074f4ee0e63ae6028abde51ff7605e31f66
韦胜健 <martsforever@aliyun.com>
2020-11-26 20:39:23 +0800

    update: complete color-picker-service

--- src/packages/color-picker/service/color-picker.service.tsx
+++ src/packages/color-picker/service/color-picker.service.tsx
@@ -2,26 +2,14 @@ import {createAgentGetter} from "../../popper/edit/createAgentGetter";
 import {App} from 'vue';
 
 const ColorPickerServiceGetter = createAgentGetter({
-    name: 'cascade',
+    name: 'color-picker',
     render(attrs: any) {
         return <pl-color-panel{...attrs}/>
     },
-    defaultPopperAttrs: {
-        transition: 'pl-transition-scale',
-        noContentPadding: true,
+    defaultRenderAttrs: {
+        onChange() {this.hide()},
     },
-    /*defaultRenderAttrs: {
-        onChange(...args) {
-            console.log('defaultRenderAttrs onChange', {context: this, args})
-        },
-    },
-    defaultPopperAttrs: {
-        onOpen() {
-            console.log('defaultRenderAttrs onOpen', {context: this})
-        },
-    },*/
 })
-
 
 export default {
     install(app: App) {
@@ -29,7 +17,8 @@ export default {
             beforeCreate(): void {
                 Object.defineProperty(this, '$colorPicker', {
                     get() {
-                        return ColorPickerServiceGetter(this)
+                        const service = ColorPickerServiceGetter(this)
+                        return service
                     },
                 })
             }
