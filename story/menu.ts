4226c54569aa1af3450a71a00767576f909206c7
韦胜健 <martsforever@aliyun.com>
2020-11-27 17:36:31 +0800

    update: 感觉做完Cascade组件之后，先做树组件靠谱一点，CascadeNode不知道是不是靠谱的

--- story/menu.ts
+++ story/menu.ts
@@ -140,7 +140,7 @@ export const MENUS: MenuGroup[] = [
     },
     {
         name: '测试', children: [
-            {name: 'Test', title: '测试手动渲染实例', page: '/normal/test', complete: false},
+            {name: 'Test', title: '测试手动渲染实例', page: '/test/test', complete: false},
             {name: 'Drag', title: '测试拖拽节点', page: '/test/test-drag', complete: false},
             {name: 'Svg', title: '测试svg', page: '/test/test-svg', complete: false},
             {name: 'Canvas', title: '测试canvas', page: '/test/test-canvas', complete: false},
---
+++ story/pages/test/test.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="demo-test">
+        cascadeData:{{cascadeData}}
+        <br>
+        <pl-button label="change" @click="change"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "test",
+        data() {
+            return {
+                node: {
+                    id: '112',
+                    name: 'angel',
+                    get childrenData() {
+                        return this.children ? this.children.map(item => `[${item}]`) : 'nothing'
+                    }
+                },
+            }
+        },
+        computed: {
+            cascadeData() {
+                return this.node.childrenData
+            },
+        },
+        methods: {
+            change() {
+                this.node.children = ['a', 's']
+            }
+        },
+    }
+</script>
+
+<style lang="scss">
+
+</style>
