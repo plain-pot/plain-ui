0e6c538c1584fbe8e4a6427319c5174b2ce432cc
韦胜健 <martsforever@aliyun.com>
2020-11-27 18:26:43 +0800

    update: complete cascade service

--- src/packages/cascade/service/cascade-service.tsx
+++ src/packages/cascade/service/cascade-service.tsx
@@ -1,6 +1,7 @@
 import {createAgentGetter} from "../../popper/edit/createAgentGetter";
 import './cascade-service.scss'
 import {App} from 'vue';
+import {delay} from 'plain-utils/utils/delay';
 
 export const CascadeServiceGetter = createAgentGetter({
     name: 'cascade',
@@ -11,8 +12,10 @@ export const CascadeServiceGetter = createAgentGetter({
         transition: 'pl-transition-popper-drop',
     },
     defaultRenderAttrs: {
-        onChange() {
+        async onChange() {
             if (!(this.state.option.serviceOption.renderAttrs as any).selectBranch) {
+                // 因为change之后，会引发 cascade panels 重新渲染没有宽度以及高度，导致过度动画失效，这里再change之后延迟再关闭
+                await delay(120)
                 this.hide()
             }
         },
