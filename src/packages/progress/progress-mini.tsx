d86918800b72732ab0cd22a06135323e32037b3b
韦胜健 <martsforever@aliyun.com>
2020-12-05 00:17:46 +0800

    dev: progress mini， 使用 svg重构

--- src/packages/progress/progress-mini.tsx
+++ src/packages/progress/progress-mini.tsx
@@ -1,35 +1,54 @@
 import {designComponent} from "../../use/designComponent";
 import {PROGRESS_DEFAULT_PROPS} from "./progress.utils";
-import {useRefs} from "../../use/useRefs";
-import {reactive, computed, watch, nextTick, onMounted} from 'vue';
-import {throttle} from "plain-utils/utils/throttle";
+import {computed} from 'vue';
+import {useStyles} from "../../use/useStyles";
+import {unit} from 'plain-utils/string/unit';
 
 export const ProgressMini = designComponent({
     name: 'pl-progress-mini',
     props: {
-        size: {default: 16},
+        size: {default: 28},
         ...PROGRESS_DEFAULT_PROPS,
-        innerColor: {type: String, default: 'rgba(0,0,0,0.6)'},
+        innerColor: {type: String, default: 'rgba(0,0,0,0.25)'},
         reverse: {type: Boolean},
     },
     setup({props}) {
-        const {refs} = useRefs({
-            canvas: HTMLCanvasElement,
+
+        const styles = useStyles(style => {
+            style.height = unit(props.size)
+            style.width = unit(props.size)
         })
 
-        const state = reactive({
-            ctx: null as any,
-            animatePercent: 0,
-            cancelAnimate: null as null | number,
+        const classes = computed(() => [
+            'pl-progress-mini',
+            {
+                [`pl-progress-mini-status-${props.status}`]: !!props.status,
+            },
+        ])
+
+        const percent = computed(() => props.modelValue == null ? 0 : props.modelValue / 100)
+
+        const radius = computed(() => 50 - 2 / 2)
+
+        const pathD = computed(() => {
+            let d = [`M 50 1`] as (string | number)[]
+            const r = radius.value
+            const p = percent.value /*小于1的百分比数字*/
+
+            const degrees = Math.PI * 2 * p
+
+            d.push(`A ${r} ${r} 0`)
+            d.push(p > 0.5 ? 1 : 0)
+            d.push(1)
+            d.push(`${50 + r * Math.sin(degrees)} ${50 - r * Math.cos(degrees)}`)
+
+            d.push(`L 50 50 L 50 1`)
+
+            return d.join(' ')
         })
 
-        /*---------------------------------------computer-------------------------------------------*/
 
-        const arcParam = computed(() => {
-            return [props.size / 2, props.size / 2, props.size / 2, 0]
-        })
-
-        const color = computed(() => {
+        const pathStroke = computed(() => {
             switch (props.status) {
                 case 'success':
                     return props.successColor
@@ -40,85 +59,16 @@ export const ProgressMini = designComponent({
             }
         })
 
-        const percent = computed(() => {
-            return props.reverse ? ((100 - state.animatePercent) / 100) : (state.animatePercent / 100)
-        })
-
-        const classes = computed(() => [
-            'pl-progress-mini',
-            {
-                [`pl-progress-mini-status-${props.status}`]: !!props.status,
-            },
-        ])
-
-        const utils = {
-            animateDrawing: throttle((percent: number) => {
-                if (!!state.cancelAnimate) {
-                    cancelAnimationFrame(state.cancelAnimate)
-                }
-
-                let time = 300
-                let startTime = Date.now()
-
-                let n = state.animatePercent
-                let k = (percent - n) / time
-
-                const run = () => {
-                    let nowTime = Date.now()
-                    let deltaTime = nowTime - startTime
-
-                    if (deltaTime > time) {
-                        state.cancelAnimate = null
-                        state.animatePercent = percent
-                        utils.draw()
-                        return
-                    }
-
-                    state.animatePercent = Number((deltaTime * k + n).toFixed(2))
-                    utils.draw()
-
-                    state.cancelAnimate = requestAnimationFrame(run)
-                }
-
-                run()
-            }, 300, {trailing: true, leading: true}),
-            draw: () => {
-                state.ctx.clearRect(0, 0, props.size, props.size)
-                utils.drawSector()
-            },
-            drawSector: () => {
-                state.ctx.save();
-
-                state.ctx.fillStyle = color.value
-                state.ctx.beginPath()
-                state.ctx.moveTo(props.size / 2, props.size / 2)
-                state.ctx.arc(...arcParam.value, (percent.value) * Math.PI * 2, false);
-                state.ctx.closePath();
-                state.ctx.fill();
-                state.ctx.restore();
-            }
-        }
-
-        const methods = {
-            reload: () => {
-                utils.animateDrawing(props.modelValue || 0)
-            },
-        }
-
-        watch(() => props.modelValue, methods.reload)
-        watch(() => props.status, () => {
-            nextTick().then(() => utils.draw())
-        })
-
-        onMounted(() => {
-            state.ctx = refs.canvas.getContext("2d")
-            methods.reload()
-        })
-
         return {
             render: () => (
-                <div class={classes.value}>
-                    <canvas class="pl-progress-mini-canvas" ref="canvas" width={props.size} height={props.size}/>
+                <div class={classes.value} style={styles.value}>
+                    <svg viewBox="0 0 100 100">
+                        <path
+                            d={pathD.value}
+                            stroke="none"
+                            fill={pathStroke.value}
+                        />
+                    </svg>
                 </div>
             )
         }
