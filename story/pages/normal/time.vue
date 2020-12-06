cf31993d44bccb65df8a7b4158fa93e8d4651663
韦胜健 <martsforever@aliyun.com>
2020-12-06 17:09:40 +0800

    dev: time, base-panel, 联动滚动的时候，一直有抖动的问题

--- story/pages/normal/time.vue
+++ story/pages/normal/time.vue
@@ -31,7 +31,7 @@
 
         </demo-row>
 
-        <demo-row title="time-base-panel">
+        <demo-row title="time-base-panel" group>
             <demo-row title="基本用法">
                 <demo-line>
                     {{val[6]}}
@@ -39,17 +39,73 @@
                 <pl-time-base-panel v-model="val[6]"/>
                 <pl-time-base-panel v-model="val[6]"/>
             </demo-row>
+
+            <demo-row title="最大最小值">
+                <demo-line>
+                    min:{{val[8]}}
+                    max:{{val[7]}}
+                </demo-line>
+                <demo-line>
+                    <pl-button label="clear" @click="val['9'] = null"/>
+                    {{val[9]}}
+                </demo-line>
+                <pl-time-base-panel :max="val[7]" :min="val[8]" v-model="val[9]"/>
+            </demo-row>
+
+            <demo-row title="选择的模式">
+                <demo-row title="仅选择：时">
+                    <demo-line>
+                        {{val[10]}}
+                    </demo-line>
+                    <pl-time-base-panel v-model="val[10]" :layout="['h']" valueFormat="HH"/>
+                    <pl-time-base-panel v-model="val[10]" :layout="['h']" valueFormat="HH"/>
+                </demo-row>
+                <demo-row title="仅选择：时分">
+                    <demo-line>
+                        {{val[11]}}
+                    </demo-line>
+                    <pl-time-base-panel v-model="val[11]" :layout="['h','m']" valueFormat="HH:mm" max="15:15" min="08:08"/>
+                    <pl-time-base-panel v-model="val[11]" :layout="['h','m']" valueFormat="HH:mm"/>
+                </demo-row>
+                <demo-row title="仅选择：分秒">
+                    <demo-line>
+                        <pl-button mode="text" label="clear" @click="val['12'] = null"/>
+                        {{val[12]}}
+                    </demo-line>
+                    <pl-time-base-panel v-model="val[12]" :layout="['m','s']" valueFormat="mm:ss" :checkDisabled="checkDisabled" @change="onMSChange"/>
+                    <pl-time-base-panel v-model="val[12]" :layout="['m','s']" valueFormat="mm:ss"/>
+                </demo-row>
+            </demo-row>
+
+            <demo-row title="自定义选项">
+                <demo-row title="自定义选项以及值限制示例">
+                    <demo-line>
+                        <pl-button mode="text" label="clear" @click="val['13'] = null"/>
+                        {{val[13]}}
+                    </demo-line>
+                    <pl-time-base-panel v-model="val[13]" :layout="['h','m']" valueFormat="HH:mm" :custom="custom" @change="onCustomChange"/>
+                    <pl-time-base-panel v-model="val[13]" :layout="['h','m']" valueFormat="HH:mm" :custom="custom" @change="onCustomChange"/>
+                </demo-row>
+            </demo-row>
+
         </demo-row>
 
     </div>
 </template>
 
 <script>
+    import {zeroize} from "plain-utils/string/zeroize";
+
     export default {
         name: "demo-time",
         data() {
             return {
-                val: {},
+                val: {
+                    6: '20:15:10',
+
+                    7: '12:12:12',
+                    8: '08:08:08',
+                },
                 timeBaseColumn: {
                     custom: () => {
                         let start = 0
@@ -64,6 +120,82 @@
                 },
             }
         },
+        methods: {
+            checkDisabled(num, layout, value) {
+                if (layout === 'm') {
+                    return num > 15 || num < 8
+                }
+                if (layout === 's') {
+                    if (value.isNull) {
+                        // 没有值的时候，【秒】选项列表任何值不可以选择
+                        return true
+                    } else {
+                        if (value.minute >= 15) {
+                            return num > 15
+                        }
+                        if (value.minute <= 8) {
+                            return num < 8
+                        }
+                    }
+                }
+
+                return false
+            },
+            onMSChange(value) {
+                let maxmm = 15
+                let maxss = 15
+                let minmm = 8
+                let minss = 8
+
+                if (!!value) {
+                    let [mm, ss] = value.split(':')
+                    mm = Number(mm)
+                    ss = Number(ss)
+                    if (mm > maxmm || (mm === maxmm && ss > maxss)) {
+                        this.$set(this.val, 12, `${this.$plain.utils.zeroize(maxmm)}:${this.$plain.utils.zeroize(maxss)}`)
+                    }
+                    if (mm < minmm || (mm === minmm && ss < minss)) {
+                        this.$set(this.val, 12, `${this.$plain.utils.zeroize(minmm)}:${this.$plain.utils.zeroize(minss)}`)
+                    }
+                }
+            },
+            custom(layout, value) {
+
+                let five = [0, 5, 10, 15, 20, 25, 30, 25, 40, 45, 50, 55]
+                let ten = [0, 10, 20, 30, 40, 50]
+
+                if (layout === 'h') {
+                    return [8, 9, 10, 11, 13, 15, 17]
+                }
+                if (layout === 'm') {
+                    if (value.isNull) {
+                        return []
+                    }
+                    return value.hour > 12 ? ten : five
+                }
+            },
+            onCustomChange(value) {
+                if (!!value) {
+                    let [HH, mm] = value.split(':')
+                    HH = Number(HH)
+                    mm = Number(mm)
+                    if (HH > 12) {
+                        if ([15, 30].indexOf(mm) === -1) {
+                            mm = 15
+                        }
+                    } else {
+                        if ([30].indexOf(mm) === -1) {
+                            mm = 30
+                        }
+                    }
+
+                    const newValue = `${zeroize(HH)}:${zeroize(mm)}`
+                    if (newValue !== value) {
+                        this.val['13'] = newValue
+                    }
+                }
+            },
+        }
     }
 </script>
 
