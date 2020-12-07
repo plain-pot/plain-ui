f255b547b8e34e3e9c9d4f0a5d688d9d3dcf1895
韦胜健 <martsforever@aliyun.com>
2020-12-07 14:51:28 +0800

    update: PlainDate, 优化类型推导，但是增加了类型的冗余代码

--- src/utils/PlainDate.ts
+++ src/utils/PlainDate.ts
@@ -8,7 +8,74 @@ enum CompareMode {
     yearmonth = 'yearmonth',
 }
 
-export class PlainDate {
+type Pub = {
+    displayFormat: string
+    valueFormat: string
+
+    setValue(value: string | undefined): void,
+    setDisplayValue(value: string): void,
+    setYear(year: number): void,
+    setMonthDate(month: number, date: number): void,
+    setHour(hour: number): void,
+    setMinute(minute: number): void,
+    setSecond(second: number): void,
+    setTime(time: number): void,
+    setHms(hour: number | PlainDateType, minute?: number, second?: number): void,
+    setYMD(year: number | PlainDateType, month?: number, date?: number): void,
+
+    format(dateObject: Date | null): string | null,
+    parseDisplayString(displayString: string | null): Date | null,
+    parseValueString(valueString: string | null): Date | null,
+    copy(): PlainDateType,
+}
+
+type Diff =
+    {
+        dateObject: Date
+        isNull: false
+        year: number
+        month: number
+        date: number
+        hour: number
+        minute: number
+        second: number
+        day: number
+        time: number
+        displayString: string
+        valueString: string
+        timeString: string
+        dateString: string
+        Y: number
+        YM: number
+        YMD: number
+        YMDHms: number
+        Hms: number
+    } |
+    {
+        dateObject: null
+        isNull: true
+        year: null
+        month: null
+        date: null
+        hour: null
+        minute: null
+        second: null
+        day: null
+        time: null
+        displayString: null
+        valueString: null
+        timeString: null
+        dateString: null
+        Y: null
+        YM: null
+        YMD: null
+        YMDHms: null
+        Hms: null
+    };
+
+type PlainDateType = Diff & Pub
+
+class WrapDate {
 
     displayFormat: string                                                       // 显示值格式字符串
     valueFormat: string                                                         // 实际值格式字符串
@@ -80,8 +147,8 @@ export class PlainDate {
         return fecha.parse(dateString, formatString)
     }
 
-    static today(displayFormat: string, valueFormat: string): PlainDate {
-        let today = new PlainDate(null, displayFormat, valueFormat)
+    static today(displayFormat: string, valueFormat: string): WrapDate {
+        let today = new WrapDate(null, displayFormat, valueFormat)
         today.setTime(new Date().getTime())
         return today
     }
@@ -90,59 +157,59 @@ export class PlainDate {
 
     /*---------------------------------------methods-------------------------------------------*/
 
-    setValue(value: string | undefined) {this.dateObject = !value ? null : PlainDate.parse(String(value), this.valueFormat)}
+    setValue(value: string | undefined) {this.dateObject = !value ? null : WrapDate.parse(String(value), this.valueFormat)}
 
-    setDisplayValue(value: string) {this.dateObject = !value ? null : this.dateObject = PlainDate.parse(String(value), this.displayFormat)}
+    setDisplayValue(value: string) {this.dateObject = !value ? null : this.dateObject = WrapDate.parse(String(value), this.displayFormat)}
 
     setYear(year: number): void {
-        if (this.isNull) this.dateObject = PlainDate.defaultDate()
+        if (this.isNull) this.dateObject = WrapDate.defaultDate()
         this.dateObject!.setFullYear(year)
     }
 
     setMonthDate(month: number, date: number): void {
-        if (this.isNull) this.dateObject = PlainDate.defaultDate()
+        if (this.isNull) this.dateObject = WrapDate.defaultDate()
         this.dateObject!.setMonth(month, date)
     }
 
     setHour(hour: number): void {
-        if (this.isNull) this.dateObject = PlainDate.defaultDate()
+        if (this.isNull) this.dateObject = WrapDate.defaultDate()
         this.dateObject!.setHours(hour)
     }
 
     setMinute(minute: number): void {
-        if (this.isNull) this.dateObject = PlainDate.defaultDate()
+        if (this.isNull) this.dateObject = WrapDate.defaultDate()
         this.dateObject!.setMinutes(minute)
     }
 
     setSecond(second: number): void {
-        if (this.isNull) this.dateObject = PlainDate.defaultDate()
+        if (this.isNull) this.dateObject = WrapDate.defaultDate()
         this.dateObject!.setSeconds(second)
     }
 
     setTime(time: number): void {
-        if (this.isNull) this.dateObject = PlainDate.defaultDate()
+        if (this.isNull) this.dateObject = WrapDate.defaultDate()
         this.dateObject!.setTime(time)
     }
 
-    setHms(hour: number | PlainDate, minute?: number, second?: number): void {
+    setHms(hour: number | WrapDate, minute?: number, second?: number): void {
         if (typeof hour === "number") {
             this.setHour(hour)
             if (minute != null) this.setMinute(minute)
             if (second != null) this.setSecond(second)
         } else {
-            const pd = hour as PlainDate
+            const pd = hour as WrapDate
             this.setHour(pd.hour!)
             this.setMinute(pd.minute!)
             this.setSecond(pd.second!)
         }
     }
 
-    setYMD(year: number | PlainDate, month?: number, date?: number) {
+    setYMD(year: number | WrapDate, month?: number, date?: number) {
         if (typeof year === 'string') {
             this.setYear(year)
             if (month != null && date != null) this.setMonthDate(month, date)
         } else {
-            const pd = year as PlainDate
+            const pd = year as WrapDate
             this.setYear(pd.year!)
             this.setMonthDate(pd.month!, pd.date!)
         }
@@ -150,20 +217,27 @@ export class PlainDate {
 
     format(dateObject: Date | null): string | null {
         if (!dateObject) return null
-        return PlainDate.format(dateObject, this.displayFormat)
+        return WrapDate.format(dateObject, this.displayFormat)
     }
 
     parseDisplayString(displayString: string | null): Date | null {
         if (!displayString) return null
-        return PlainDate.parse(displayString, this.displayFormat)
+        return WrapDate.parse(displayString, this.displayFormat)
     }
 
     parseValueString(valueString: string | null): Date | null {
         if (!valueString) return null
-        return PlainDate.parse(valueString, this.valueFormat)
+        return WrapDate.parse(valueString, this.valueFormat)
     }
 
-    copy(): PlainDate {
-        return new PlainDate(this.valueString, this.displayFormat, this.valueFormat)
+    copy(): WrapDate {
+        return new WrapDate(this.valueString, this.displayFormat, this.valueFormat)
     }
+}
+
+export const PlainDate = WrapDate as any as (new(...args: ConstructorParameters<typeof WrapDate>) => PlainDateType) & {
+    defaultDate(): Date,
+    format(dateObject: Date, formatString: string): string,
+    parse(dateString: string, formatString: string): Date | null,
+    today(displayFormat: string, valueFormat: string): WrapDate,
 }
