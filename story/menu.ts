d30cd80a168a4c4abd486c204a40f72080ff38d8
韦胜健 <martsforever@aliyun.com>
2020-12-07 13:46:16 +0800

    bug: time， complete time

--- story/menu.ts
+++ story/menu.ts
@@ -46,7 +46,7 @@ export const MENUS: MenuGroup[] = [
             {name: 'ColorPicker', title: '颜色选择', page: '/normal/color-picker', complete: true},
 
             {name: 'Cascade', title: '级联选择', page: '/normal/cascade', complete: true},
-            {name: 'Time', title: '时间选择', page: '/normal/time', complete: false},
+            {name: 'Time', title: '时间选择', page: '/normal/time', complete: true},
             {name: 'Date', title: '日期选择', page: '/normal/date', complete: false},
             {name: 'Upload', title: '文件上传', page: '/normal/upload', complete: false},
             {name: 'Img', title: '图片上传', page: '/normal/img', complete: false},
