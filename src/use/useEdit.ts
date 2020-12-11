b483c8449eb43acecb638e9fbf36a41cfac06f18
韦胜健 <martsforever@aliyun.com>
2020-12-11 11:05:48 +0800

    update: useEdit, 优化onBlur以及onChange

--- src/use/useEdit.ts
+++ src/use/useEdit.ts
@@ -1,5 +1,6 @@
 import {useFunctionWrapper} from "./useFunctionWrapper";
-import {inject, reactive, computed, provide, onBeforeUnmount} from 'vue';
+import {computed, inject, onBeforeUnmount, provide, reactive} from 'vue';
+import {ComponentEvent} from "./useEvent";
 
 export const EDIT_PROVIDER = '@@EDIT_PROVIDER'
 
@@ -21,56 +22,58 @@ interface EditProvideData {
     onChange?: (...args: any[]) => void,
 }
 
-export const useEdit = useFunctionWrapper('edit', (ctx: any, option: {
-    adjust?: (data: EditProvideData) => void | EditProvideData
-} = {}) => {
+export const useEdit = useFunctionWrapper(
+    'edit',
+    (ctx, option: { adjust?: (data: EditProvideData) => void | EditProvideData } = {}) => {
 
-    const parentEditComputed = inject(EDIT_PROVIDER, null) as null | { value: EditProvideData }
+        const parentEditComputed = inject(EDIT_PROVIDER, null) as null | { value: EditProvideData }
 
-    const editState = reactive({loading: null as null | boolean})
+        const editState = reactive({loading: null as null | boolean})
 
-    const editComputed = computed(() => {
-        const {
-            disabled,
-            readonly,
-            loading,
-            placeholder
-        } = ctx.props as EditProvideData
+        const editComputed = computed(() => {
+            const {
+                disabled,
+                readonly,
+                loading,
+                placeholder
+            } = ctx.props as any as EditProvideData
 
-        let data = {disabled, readonly, loading, placeholder}
+            let data = {disabled, readonly, loading, placeholder}
 
-        if (data.disabled == null) data.disabled = !!parentEditComputed ? parentEditComputed.value.disabled : false
-        if (data.readonly == null) data.readonly = !!parentEditComputed ? parentEditComputed.value.readonly : false
-        if (data.placeholder == null) data.placeholder = !!parentEditComputed ? parentEditComputed.value.placeholder : null
-        if (editState.loading == null) {
-            data.loading = data.loading != null ? data.loading : (!!parentEditComputed ? parentEditComputed.value.loading : false)
-        } else {
-            data.loading = editState.loading
+            if (data.disabled == null) data.disabled = !!parentEditComputed ? parentEditComputed.value.disabled : false
+            if (data.readonly == null) data.readonly = !!parentEditComputed ? parentEditComputed.value.readonly : false
+            if (data.placeholder == null) data.placeholder = !!parentEditComputed ? parentEditComputed.value.placeholder : null
+            if (editState.loading == null) {
+                data.loading = data.loading != null ? data.loading : (!!parentEditComputed ? parentEditComputed.value.loading : false)
+            } else {
+                data.loading = editState.loading
+            }
+
+            if (!!option.adjust) data = option.adjust(data) || data
+
+            return {
+                ...data,
+                editable: !data.disabled && !data.readonly && !data.loading
+            }
+        });
+
+        provide(EDIT_PROVIDER, editComputed)
+
+        const event = (ctx as any)._event as ComponentEvent<any> | undefined
+
+        if (!!parentEditComputed) {
+            if (!!parentEditComputed.value.onBlur && !!event && !!event.on.blur) {
+                event.on.blur(parentEditComputed.value.onBlur)
+                onBeforeUnmount(() => event.off.blur(parentEditComputed.value.onBlur!))
+            }
+            if (!!parentEditComputed.value.onChange && !!event && !!event.on.change) {
+                event.on.change(parentEditComputed.value.onChange)
+                onBeforeUnmount(() => event.off.change(parentEditComputed.value.onChange!))
+            }
         }
-
-        if (!!option.adjust) data = option.adjust(data) || data
 
         return {
-            ...data,
-            editable: !data.disabled && !data.readonly && !data.loading
+            editState,
+            editComputed,
         }
-    });
-
-    provide(EDIT_PROVIDER, editComputed)
-
-    if (!!parentEditComputed) {
-        if (!!parentEditComputed.value.onBlur && !!ctx._event && !!ctx._event.on.blur) {
-            ctx._event.on.blur(parentEditComputed.value.onBlur)
-            onBeforeUnmount(ctx._event.off.blur(parentEditComputed.value.onBlur))
-        }
-        if (!!parentEditComputed.value.onChange && !!ctx._event && !!ctx._event.on.change) {
-            ctx._event.on.change(parentEditComputed.value.onBlur)
-            onBeforeUnmount(ctx._event.off.change(parentEditComputed.value.onBlur))
-        }
-    }
-
-    return {
-        editState,
-        editComputed,
-    }
-})
+    })
