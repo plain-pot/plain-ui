f9d28213d8baa530c819f3810e59379a793c1bfe
韦胜健 <martsforever@aliyun.com>
2020-12-12 11:34:50 +0800

    dev: form, form grid

---
+++ story/pages/form/form-grid.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="demo-form-grid">
+        form-grid
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "form-grid"
+    }
+</script>
+
+<style lang="scss">
+
+</style>
