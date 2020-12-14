450ecb2e3061cb3630c5be8d259fc59706c2e638
韦胜健 <martsforever@aliyun.com>
2020-12-14 13:00:32 +0800

    update: adjust installPlugin

--- src/utils/installPlugin.ts
+++ src/utils/installPlugin.ts
@@ -3,18 +3,18 @@ import {App} from 'vue';
 
 const installMap = new WeakMap<App, ComponentPlugin[]>()
 
-export function installPlugin(app: App, plugin: ComponentPlugin | ComponentPlugin[]) {
+export function installPlugin(app: App, plugins: ComponentPlugin | ComponentPlugin[]) {
+    /*已经安装过的插件*/
     let installedPlugins = installMap.get(app)
     if (!installedPlugins) {
         /*这个app第一次注册*/
         installedPlugins = []
         installMap.set(app, installedPlugins)
     }
-    const plugins = Array.isArray(plugin) ? plugin : [plugin]
-    plugins.forEach(plugin => {
+    (Array.isArray(plugins) ? plugins : [plugins]).forEach(plugin => {
         if (installedPlugins!.indexOf(plugin) === -1) {
+            installedPlugins!.push(plugin)// 这句代码一定要在app.use面前
             app.use(plugin)
-            installedPlugins!.push(plugin)
         }
-    })
+    });
 }
