f8c197be8d14daa9189d03fb8d9411c34cb7dc93
韦胜健 <martsforever@aliyun.com>
2020-12-17 20:18:21 +0800

    update: table, 重写 table组件

--- src/packages/table/index.ts
+++ src/packages/table/index.ts
@@ -1,13 +1,11 @@
 import {createComponentPlugin} from "../../utils/createComponentPlugin";
 import table from './table'
 import {VirtualTable} from "./virtual-table";
-import PlcPlugin from './plc-core'
 
 export default createComponentPlugin(table, {
     exposeComponents: {
         VirtualTable,
     },
     plugins: [
-        PlcPlugin,
     ]
 })
