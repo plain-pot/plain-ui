2a08739118f490b123f46a68f50f5c142044bade
韦胜健 <martsforever@aliyun.com>
2020-12-18 23:13:44 +0800

    update: table, 基本完成 useTableNode

--- src/packages/table/core/table.utils.ts
+++ src/packages/table/core/table.utils.ts
@@ -2,8 +2,13 @@ import {StyleProps} from "../../../use/useStyle";
 import {SimpleObject} from "../../../shims";
 import {PropType} from 'vue';
 import {TablePlc} from "../plc/core/plc.type";
+import {TableNode} from "./useTableNode";
 
 export type TablePropsConfig = (plcList: TablePlc[]) => Record<string, any>
+export type TablePropsGetChildren = (node: TableNode, cb: (...args: any[]) => void) => void
+export type TablePropsIsCheckable = (node: TableNode) => boolean
+export type TablePropsIsLeaf = (node: TableNode) => boolean
+export type TablePropsFilterNodeMethod = (node: TableNode) => boolean
 
 export enum TablePlcAlign {
     left = 'left',
@@ -32,4 +37,18 @@ export const TableProps = {
     showRows: {type: Number, default: 10},                      // 表格显示的行数，当表体的行数超过这个值时，将会出现表体内部滚动，这个属性实际上就是用来设值表格高度
 
     config: {type: Function as PropType<TablePropsConfig>},     // 配置列信息函数
+
+    /*---------------------------------------tree-------------------------------------------*/
+    lazy: {type: Boolean},                                      // 是否懒加载数据
+    according: {type: Boolean},                                 // 是否开启手风琴模式，只打开一个兄弟节点
+    loading: {type: Boolean, default: null},                    // 是否开启表格的加载状态
+
+    keyField: {type: String},                                   // 每一个树节点用来标识的唯一树形
+    childrenField: {type: String},                              // 树节点对应子节点数据对应字段
+    getChildren: {type: Function as PropType<TablePropsGetChildren>},// 加载子节点数据的函数，仅当 lazy 为true时有效
+    isCheckable: {type: Function as PropType<TablePropsIsCheckable>},// 当即将选中树节点时，判断是否可以选中该树节点
+    isLeaf: {type: Function as PropType<TablePropsIsLeaf>},     // 判断树节点是否为叶子节点的函数，仅在lazy模式有效
+    filterNodeMethod: {type: Function as PropType<TablePropsFilterNodeMethod>},// 对树节点进行筛选的方法，返回true表示可以显示，返回false表示隐藏
+    checkStrictly: {type: Boolean},                             // 在显示复选框的情况下，是否严格遵循父子互不关联的做法，默认为false
+    autoExpandParent: {type: Boolean, default: true},           // 是否展开节点的时候，自动展开父节点
 }
--- src/packages/table/core/useTableNode.ts
+++ src/packages/table/core/useTableNode.ts
@@ -1,10 +1,85 @@
 import {SimpleObject} from "../../../shims";
 import {TreeNodeCheckStatus} from "../../tree/utils/tree-constant";
-import {FormValidateResult, FormValidateReturn} from "../../form/form.validate";
-import {ExtractPropTypes} from 'vue';
+import {FormValidate, FormValidateResult, FormValidateReturn} from "../../form/form.validate";
+import {computed, ExtractPropTypes, reactive, ref} from 'vue';
 import {TableProps} from "./table.utils";
 import {useModel} from "../../../use/useModel";
 import {createKeyHandler} from "../../../utils/createKeyHandler";
+import {deepcopy} from "plain-utils/object/deepcopy";
+
+export function useTableNode(
+    {
+        props,
+        emit,
+        getValidate,
+    }: {
+        props: ExtractPropTypes<typeof TableProps>,
+        emit: { onUpdateData: (data?: SimpleObject[]) => void },
+        getValidate: () => FormValidate,
+    }
+) {
+
+    /*---------------------------------------state-------------------------------------------*/
+
+    /*因为可能会发生拖拽节点，懒加载数据等操作，这里table内保存一份数据快照*/
+    const dataModel = useModel(() => props.data, emit.onUpdateData)
+
+    const utils = {
+        getNodeByData: tableNodeGetter({props, getValidate})(),
+        getNodeState: () => {
+            const nodeMap = {} as Record<string, TableNode>;
+            const iterator = ({data, level, parentRef}: { data: SimpleObject, level: number, parentRef: () => TableNode }): TableNode => {
+                const node = utils.getNodeByData({data, level, parentRef})
+                nodeMap[node.key] = node
+                const childrenData = !props.childrenField ? null : (data[props.childrenField!] as SimpleObject[])
+                if (!!childrenData) {
+                    node.children = childrenData.map(child => iterator({data: child, level: level + 1, parentRef: node.selfRef}))
+                }
+                return node
+            }
+            const root = iterator({
+                data: {
+                    [props.keyField || 'id']: '@@$$root',
+                    [props.childrenField || 'children']: dataModel.value
+                },
+                level: 0,
+                parentRef: null as any,
+            })
+            return {
+                nodeMap,
+                root
+            }
+        },
+    }
+
+    const nodeState = utils.getNodeState()
+    const root = ref(nodeState.root)
+    const nodeMap = ref(nodeState.nodeMap)
+
+    const state = reactive({
+        /*虚拟根节点*/
+        root,
+        /*通过key找到node的映射*/
+        nodeMap,
+        /*拍平的数组*/
+        flatList: computed(() => {
+            let list: TableNode[] = []
+            if (!state.root) {
+                return []
+            }
+            iterateNodes({
+                nodes: state.root.children,
+                iterateChildren: (treeNode) => treeNode.expand,
+                handler: (treeNode) => list.push(treeNode),
+            },)
+            list = list.filter((treeNode) => !!treeNode.isVisible)
+            list.forEach((node, index) => node.index = index)
+            return list
+        }),
+    })
+
+    return state
+}
 
 export type TableNode = {
 
@@ -22,7 +97,6 @@ export type TableNode = {
     loaded: boolean,                                    // 节点是否已经加载完毕
 
     readonly childrenData?: SimpleObject[]              // 子节点数据数组
-    readonly label?: string,                            // 节点显示文本
     readonly checkStatus: TreeNodeCheckStatus,          // 节点选中状态
     readonly isCheckable: boolean,                      // 节点是否可以选择
     readonly isLeaf: boolean,                           // 节点是否为叶子节点
@@ -50,35 +124,160 @@ export type TableNode = {
 
 }
 
-function tableNodeGetter(props: ExtractPropTypes<typeof TableProps>) {
+/**
+ * 产生一个用来获取TableNode的工具函数
+ * @author  韦胜健
+ * @date    2020/12/18 23:09
+ */
+function tableNodeGetter({props, getValidate}: { props: ExtractPropTypes<typeof TableProps>, getValidate: () => FormValidate }) {
     const keyManager = createKeyHandler('table')
+    /*之所以要返回一个函数再执行一遍，是因为map要重新创建，以便不同的table使用不同的map做缓存*/
     return () => {
         const map = new WeakMap<object, TableNode>()
         const get = ({data, level, parentRef}: { data: any, level: number, parentRef: () => TableNode }) => {
             let node: (TableNode | undefined) = map.get(data)
-            const key = keyManager(data)
+            const key = keyManager(data, props.keyField)
             if (!!node) {
+                node.key !== key && (node.key = key);
+                node.data !== data && (node.data = data);
+                node.level !== level && (node.level = level);
+                node.parentRef !== parentRef && (node.parentRef = parentRef);
+            } else {
+                node = {
+                    key, data, level, parentRef, index: 0,
+                    selfRef() {return this},
+                    // 这里先不计算，在resetData的时候才计算
+                    children: [],
 
+                    expand: false,
+                    check: false,
+                    loading: false,
+                    loaded: true,
+
+                    get childrenData() {return (!props.childrenField || !this.data) ? null : this.data[props.childrenField]},
+                    /* todo 这里并不会立即执行，当渲染VNodeChild的时候这里才会执行，所以不存在checkStatus.value不存在的问题*/
+                    // get checkStatus() {return checkStatus.value[this.key]},
+                    get checkStatus() {return TreeNodeCheckStatus.uncheck},
+                    get isCheckable() {return !props.isCheckable || props.isCheckable(this)},
+                    get isLeaf() {return !!props.isLeaf ? props.isLeaf(this) : !this.childrenData},
+                    get isVisible() {return !props.filterNodeMethod ? true : (props.filterNodeMethod(this) || (!!this.children && this.children.some(child => child.isVisible)))},
+
+                    /*将自身节点移除*/
+                    removeSelf() {
+                        const parentChildrenData = this.parentRef()!.childrenData!
+                        parentChildrenData.splice(parentChildrenData.indexOf(this.data), 1)
+                    },
+                    /*将目标节点插入当前节点前面*/
+                    previousSibling(node: TableNode) {
+                        let parentChildrenData = this.parentRef()!.getReactiveChildrenData()
+                        node.parentRef = this.parentRef
+                        node.level = this.level
+                        parentChildrenData.splice(parentChildrenData.indexOf(this.data), 0, node.data)
+                    },
+                    /*将目标节点插入当前节点后面*/
+                    nextSibling(node: TableNode) {
+                        let parentChildrenData = this.parentRef()!.getReactiveChildrenData()
+                        node.parentRef = this.parentRef
+                        node.level = this.level
+                        parentChildrenData.splice(parentChildrenData.indexOf(this.data) + 1, 0, node.data)
+                    },
+                    /*将目标节点插入当前节点子节点中*/
+                    unshiftChild(node: TableNode) {
+                        let childrenData = this.getReactiveChildrenData()
+                        node.parentRef = () => this
+                        node.level = this.level + 1
+                        childrenData.unshift(node.data)
+                    },
+                    /*获取响应式的子节点数据*/
+                    getReactiveChildrenData(): any[] {
+                        let childrenData = this.childrenData
+                        if (!childrenData) {
+                            childrenData = []
+                            this.data[props.childrenField!] = childrenData
+                        }
+                        return childrenData
+                    },
+
+                    /*当前是否为合计行*/
+                    isSummary: false,
+                    /*当前是否开启编辑状态*/
+                    edit: false,
+                    /*当前编辑行数据*/
+                    editRow: data,
+                    /*校验结果信息*/
+                    validateResult: null,
+
+                    /*开启编辑状态（不做任何处理）*/
+                    openEdit() {this.edit = true},
+                    /*关闭编辑状态（不做任何处理）*/
+                    closeEdit() {this.edit = false},
+                    /*开启编辑状态（先判断当前是否可编辑，深度赋值一份data赋值给editRow，并且立即执行校验）*/
+                    enableEdit() {
+                        if (this.edit) return
+                        this.editRow = deepcopy(this.data)
+                        this.openEdit()
+                        this.validate()
+                    },
+                    /*取消编辑状态*/
+                    cancelEdit() {this.edit && this.closeEdit()},
+                    /*校验数据*/
+                    async validate() {
+                        const {methods: {validate}} = getValidate()
+                        const {validateMessage, validateResult, validateResultMap} = await validate(this.editRow)
+                        this.validateResult = validateResult || null
+                        return !validateMessage ? null : {
+                            validateMessage,
+                            validateResult,
+                            validateResultMap
+                        }
+                    },
+                    /*保存编辑，将editRow以及newRow（请求得到的新对象）覆盖data*/
+                    async saveEdit(newRow) {
+                        if (!this.edit) return
+                        const {data, editRow} = this
+                        Object.keys({...data, ...editRow, ...newRow}).forEach(k => this.data[k] = newRow[k])
+                    },
+                }
             }
+            return node
         }
         return get
     }
 }
 
-export function useTableNode(
+/**
+ * 遍历所有的treeNode
+ * @author  韦胜健
+ * @date    2020/3/30 19:30
+ * @param   treeNodes               要遍历的数据
+ * @param   handler                 处理函数
+ * @param   iterateChildren         判断是否遍历其子节点数据
+ * @param   iterateChildrenFirst    先遍历子节点
+ */
+function iterateNodes(
     {
-        props,
-        emit,
+        nodes,
+        handler,
+        iterateChildren,
+        iterateChildrenFirst,
     }: {
-        props: ExtractPropTypes<typeof TableProps>,
-        emit: { onUpdateData: (data?: SimpleObject[]) => void }
-    }
-) {
-
-    /*---------------------------------------state-------------------------------------------*/
-
-    /*因为可能会发生拖拽节点，懒加载数据等操作，这里table内保存一份数据快照*/
-    const dataModel = useModel(() => props.data, emit.onUpdateData)
-
-
+        nodes?: TableNode[] | Readonly<TableNode[]> | null,
+        handler: (node: TableNode) => void,
+        iterateChildren?: (node: TableNode) => boolean,
+        iterateChildrenFirst?: boolean,
+    },
+): void {
+    if (!nodes) return
+    nodes.forEach(treeNode => {
+        !iterateChildrenFirst && handler(treeNode);
+        if (!!treeNode.children && (!iterateChildren || iterateChildren(treeNode))) {
+            iterateNodes({
+                nodes: treeNode.children,
+                handler,
+                iterateChildren,
+                iterateChildrenFirst,
+            })
+        }
+        iterateChildrenFirst && handler(treeNode);
+    })
 }
