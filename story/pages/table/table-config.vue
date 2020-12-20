61c9b91acf71f42d7e16bc7ff611f15b8457fbef
韦胜健 <martsforever@aliyun.com>
2020-12-20 11:07:26 +0800

    update: table,  切换列隐藏的时候，渲染的数据不太对

--- story/pages/table/table-config.vue
+++ story/pages/table/table-config.vue
@@ -17,7 +17,7 @@
             </pl-form>
         </demo-row>
         <demo-row title="不分组">
-            <pl-table debugPlc :config="config" :data="data" colDraggable>
+            <pl-table debugPlc :config="config" :data="data">
                 <plc field="id" title="编号" width="200px"/>
                 <!--这里虽然通过props设置了宽度，但是因为 在config 中也配置了这一列的宽度，所以这里配置的不生效-->
                 <plc field="size" title="大小" :width="plc.width"/>
@@ -84,6 +84,7 @@
 
 <style lang="scss">
     .demo-table-config {
+
         .pl-table {
             margin-right: 0;
         }
