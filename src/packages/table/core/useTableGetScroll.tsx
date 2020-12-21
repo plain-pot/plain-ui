b2403c6e266e76c03455586ca068564f2fab7058
韦胜健 <martsforever@aliyun.com>
2020-12-21 17:26:20 +0800

    update: plc, 可编辑在开启虚拟滚动的情况下，比较卡

---
+++ src/packages/table/core/useTableGetScroll.tsx
@@ -0,0 +1,12 @@
+import {PlainScroll} from "../../scroll/scroll";
+
+export function useTableGetScroll(onVirtualMounted: (cb: (data: { scroll: PlainScroll }) => void) => void, onChange?: (scroll: PlainScroll) => void) {
+    let val: PlainScroll
+    onVirtualMounted(({scroll}) => {
+        val = scroll;
+        !!onChange && onChange(scroll)
+    })
+    return {
+        getScroll: () => val!
+    }
+}
