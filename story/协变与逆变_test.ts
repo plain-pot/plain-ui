f4abfcd325a7549001565af7eda0a85eb5329d4a
韦胜健 <martsforever@aliyun.com>
2020-12-21 11:52:11 +0800

    update: table, designPlc，搞不定这个东西

--- story/协变与逆变_test.ts
+++ story/协变与逆变_test.ts
@@ -1,12 +1,13 @@
+import {defineComponent} from "vue"
+
 function getRenderReturn<_,
     Refer,
     OptionProps,
-    SetupProps extends OptionProps,
     >(
     option: {
         props?: OptionProps,
-        setup: (props: SetupProps) => Refer,
-        render: (this: Refer) => any,
+        setup: (this: OptionProps) => Refer,
+        render: (refer: Refer) => any,
     }): Refer {
     return {} as any
 }
@@ -16,16 +17,32 @@ const a = getRenderReturn({
         name: "111",
         age: 11,
     },
-    setup: (props: { name: string, age: number }) => {
-        console.log(props.age.toPrecision(0))
-        console.log(props.name.anchor('1'))
+    setup() {
+        console.log(this.age.toPrecision(0))
+        console.log(this.name.anchor('1'))
         return {
-            props,
             sayHello: () => 123
         }
     },
-    render() {
-        console.log(this.props.age.toFixed(0))
-        this.sayHello()
+    render(refer) {
+        // console.log(refer.props.age.toFixed(0))
+        // refer.sayHello()
     }
+})
+
+defineComponent({
+    props: {
+        type: {type: HTMLDivElement, required: true}
+    },
+    data() {
+        return {
+            type2: this.type,
+            name: '111',
+            age: 111,
+        }
+    },
+    render() {
+        console.log(this.type2.scrollLeft)
+        console.log(this.name.charCodeAt(0))
+    },
 })
