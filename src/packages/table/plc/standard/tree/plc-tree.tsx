4657ec6d1b351e2ae85dc25a212d32ede0ec9e24
韦胜健 <martsforever@aliyun.com>
2020-12-22 16:00:47 +0800

    update: table, plc-tree, 基本的展开收起功能

--- src/packages/table/plc/standard/tree/plc-tree.tsx
+++ src/packages/table/plc/standard/tree/plc-tree.tsx
@@ -4,8 +4,9 @@ import {TableNode} from "../../../core/useTableNode";
 import {TreeDropType} from "../../../../tree/utils/tree-constant";
 import {useScopedSlots} from "../../../../../use/useScopedSlots";
 import {Plc} from "../../core/plc.type";
-import {SimpleObject} from "../../../../../shims";
+import {SimpleObject, StyleProperties} from "../../../../../shims";
 import {injectPlainTable} from "../../../table";
+import {unit} from "plain-utils/string/unit";
 
 /*只显示展开收起按钮的时候的基本宽度，不算content宽度*/
 const size = 30
@@ -24,6 +25,10 @@ export default designPlc({
         rowDraggable: {type: Boolean},                                      // 行是否可以拖拽排序
         allowRowDraggable: {type: Function as PropType<(node: TableNode) => boolean>},// 行是否可以被拖拽放置到其他位置
         allowRowDroppable: {type: Function as PropType<(startNode: TableNode, moveNode: TableNode, dropType: TreeDropType) => boolean>},// 行是否可以被放置（放到子节点中，放到前面，放到后面）
+
+        folderCollapseIcon: {type: String, default: 'el-icon-folder-s'},
+        folderExpandIcon: {type: String, default: 'el-icon-folder-opened'},
+        leafIcon: {type: String, default: 'el-icon-document'},
     },
     setup(props, ctx) {
 
@@ -57,6 +62,26 @@ export default designPlc({
             return expand * (level - 1) + (content + expand + check + draggable)
         })
 
+        const utils = {
+            getExpanderAttrs: (node: TableNode) => {
+                return {
+                    style: {marginLeft: unit((node.level - 1) * size)} as any,
+                }
+            },
+        }
+
+        const handler = {
+            clickExpandIcon: async (e: MouseEvent, node: TableNode) => {
+                e.stopPropagation()
+                if (node.isLeaf) {return}
+                await table.toggleExpand(node.key)
+            },
+            clickCheckbox: async (e: MouseEvent, node: TableNode) => {
+                e.stopPropagation()
+                await table.toggleCheck(node.key)
+            }
+        }
+
         onMounted(() => {
             watch(() => maxShowLevel.value, () => {
                 plc.state.width = width.value
@@ -64,7 +89,10 @@ export default designPlc({
         })
 
         return {
+            props,
             scopedSlots,
+            handler,
+            utils,
         }
     },
 }, {
@@ -72,8 +100,13 @@ export default designPlc({
     default: ({refer, node, plc, row}) => {
         return (
             <>
-                <div class="plc-tree-node-expander">
-                    <pl-button mode="text" size="normal" icon={node.expand ? 'el-icon-caret-bottom' : 'el-icon-caret-right'}/>
+                <div class="plc-tree-node-expander" {...refer.utils.getExpanderAttrs(node)}>
+                    <pl-button
+                        loading={node.loading}
+                        mode="text"
+                        size="normal"
+                        icon={node.isLeaf ? refer.props.leafIcon : node.expand ? refer.props.folderExpandIcon : refer.props.folderCollapseIcon}
+                        onClick={(e: MouseEvent) => refer.handler.clickExpandIcon(e, node)}/>
                 </div>
                 {refer.scopedSlots.content.isExist() && (
                     <div class="plc-tree-node-content">
