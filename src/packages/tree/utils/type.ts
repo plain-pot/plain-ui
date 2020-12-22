d4514639ab2702fe80187f0efebfb3884f117749
韦胜健 <martsforever@aliyun.com>
2020-12-22 12:05:24 +0800

    update: plc, tree 还差抽离 drag 的内容

--- src/packages/tree/utils/type.ts
+++ src/packages/tree/utils/type.ts
@@ -27,21 +27,11 @@ export type TreeNode = {
 }
 
 export namespace TreePropsType {
-
     export interface renderContent {(data: { node: TreeNode, index: number }): VNodeChild}
 
     export interface nodeIcon {(node: TreeNode): string}
 
-    export interface filterNodeMethod {(node: TreeNode): boolean}
-
-    export interface isLeaf {(node: TreeNode): boolean}
-
-    export interface isCheckable {(node: TreeNode): boolean}
-
-    export interface getChildren {(node: TreeNode, cb: (...args: any[]) => void): void}
-
     export interface allowDrag {(node: TreeNode): boolean}
 
     export interface allowDrop {(startNode: TreeNode, moveNode: TreeNode, dropType: TreeDropType): boolean}
-
 }
