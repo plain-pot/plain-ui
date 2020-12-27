f1a2cc2ff0e720f60d478b8b5fc52939dc3d38f7
韦胜健 <martsforever@aliyun.com>
2020-12-27 22:25:49 +0800

    adjust: plc-color-picker default render

--- src/packages/table/plc/edit/plc-color-picker.tsx
+++ src/packages/table/plc/edit/plc-color-picker.tsx
@@ -3,5 +3,11 @@ import {designPlc} from "../core/designPlc";
 export default designPlc({
     name: 'plc-color-picker',
 }, {
+    default: ({row, plc}) => {
+        return !!plc.props.field && <>
+            <pl-color-button color={row[plc.props.field]} style="margin-right:6px;"/>
+            {row[plc.props.field]}
+        </>
+    },
     edit: ({row, plc}) => !plc.props.field ? null : <pl-color-picker v-model={row[plc.props.field]}/>
 },)
