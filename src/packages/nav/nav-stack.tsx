e69a625a6cecd82f9af4bcc576dbe76537eb4dda
韦胜健 <martsforever@aliyun.com>
2021-01-05 11:38:46 +0800

    感觉得开个新的页面来操作nav

---
+++ src/packages/nav/nav-stack.tsx
@@ -0,0 +1,30 @@
+import {designComponent} from "../../use/designComponent";
+import {PropType} from 'vue';
+import {GetPage, Stack} from "./NavigatorManager";
+import {NavPage} from "./nav-page";
+
+export const NavStack = designComponent({
+    name: "pl-nav-stack",
+    props: {
+        stack: {type: Object as PropType<Stack>, required: true},
+        getPage: {type: Function as PropType<GetPage>, required: true},
+    },
+    setup({props}) {
+        return {
+            render: () => (
+                <div class="pl-nav-stack">
+                    {props.stack.pages.map((page, index) => {
+                        return (
+                            <NavPage
+                                key={page.id}
+                                page={page}
+                                getPage={props.getPage}
+                                isLast={index === props.stack.pages.length - 1}
+                            />
+                        )
+                    })}
+                </div>
+            )
+        }
+    },
+})
--- src/packages/nav/nav.tsx
+++ src/packages/nav/nav.tsx
@@ -2,7 +2,7 @@ import {designComponent} from "../../use/designComponent";
 import {PropType} from 'vue';
 import {NavigatorManager} from "./NavigatorManager";
 import './nav.scss'
-import {NavPage} from "./nav-page";
+import {NavStack} from "./nav-stack";
 
 export default designComponent({
     name: 'pl-nav',
@@ -17,20 +17,8 @@ export default designComponent({
             render: () => {
                 return (
                     <div class="pl-nav" style={{width: props.width, height: props.height}}>
-                        {props.nav.state.stacks.map(stack => (
-                            <div class="pl-nav-stack">
-                                {stack.pages.map((page, index) => {
-                                    return (
-                                        <NavPage
-                                            key={page.id}
-                                            page={page}
-                                            getPage={props.nav.utils.getPage}
-                                            isLast={index === stack.pages.length - 1}
-                                        />
-                                    )
-                                })}
-                            </div>
-                        ))}
+                        {props.nav.state.stacks
+                            .map(stack => (<NavStack stack={stack} getPage={props.nav.utils.getPage} key={stack.id}/>))}
                     </div>
                 )
             }
