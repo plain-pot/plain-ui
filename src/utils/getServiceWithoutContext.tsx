1f41a00d7f5b7b107dfb1c239ba5a593befe4e03
韦胜健 <martsforever@aliyun.com>
2021-01-06 11:50:14 +0800

    service: 打个标记

--- src/utils/getServiceWithoutContext.tsx
+++ src/utils/getServiceWithoutContext.tsx
@@ -1,12 +1,14 @@
 import {ComponentPublicInstance, createApp} from 'vue';
 import Root from '../packages/root'
 import {installPlugin} from "./installPlugin";
+import {addClass} from "plain-utils/dom/addClass";
 
 const getIns = (() => {
     let ins: ComponentPublicInstance | undefined;
     return () => {
         if (!ins) {
             const container = document.createElement('div')
+            addClass(container, 'service-without-context')
             document.body.appendChild(container)
             const app = createApp({render() {return <pl-root/>}})
             installPlugin(app, Root)
