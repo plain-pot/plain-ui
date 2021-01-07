a249f0541e6454cc7f23f3923cd6392a0350e40e
韦胜健 <martsforever@aliyun.com>
2021-01-07 15:46:43 +0800

    micro: 主应用和子应用分别独立运行

--- pro/main.tsx
+++ pro/main.tsx
@@ -2,6 +2,31 @@ import {createApp} from 'vue'
 import App from './app.vue'
 import PlainUi from "../src/index";
 
-const app = createApp(App)
-app.use(PlainUi)
-app.mount('#app')
+/* eslint-disable */
+
+function designMainApplication(init: () => void) {
+    if (!(window as any).HAS_MAIN_APPLICATION) {
+        /*
+        *  当前访问主应用运行，主应用自己初始化
+        */
+        (window as any).HAS_MAIN_APPLICATION = 'MAIN_APPLICATION'
+        init();
+    }
+    return (config: { assetPublicPath: string }) => {
+        /*
+        *  否则为子应用运行，子应用通过加载主应用之后初始化
+        */
+        __webpack_public_path__ = config.assetPublicPath
+        init()
+    }
+}
+
+/*---------------------------------------start-------------------------------------------*/
+
+export default designMainApplication(() => {
+    const app = createApp(App)
+    app.use(PlainUi)
+    app.mount('#app')
+})
+
+/* eslint-enable */
