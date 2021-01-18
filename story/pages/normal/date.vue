9d2b0d0a81979e31abe74ccd2103a25f399133eb
韦胜健 <martsforever@aliyun.com>
2021-01-18 21:19:45 +0800

    date: 年份单选多选

--- story/pages/normal/date.vue
+++ story/pages/normal/date.vue
@@ -2,9 +2,18 @@
     <div>
         <h1> 重写Date组件</h1>
         <demo-row title="年份">
-            <pl-date-panel-year v-model="val[0]"/>
-            <pl-date-panel-year v-model="val[0]"/>
+            <demo-row title="单选">
+                <pl-date-panel-year v-model="val[0]"/>
+                <pl-date-panel-year v-model="val[0]"/>
+                {{val[0]}}
+            </demo-row>
+            <demo-row title="多选">
+                <pl-date-panel-year v-model="val[1]" multiple/>
+                <pl-date-panel-year v-model="val[1]" multiple/>
+                {{val[1]}}
+            </demo-row>
         </demo-row>
+
     </div>
 </template>
 
