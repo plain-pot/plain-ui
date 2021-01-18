544ef1be31498ae9a12f00c26e3f5f61d3c4c92e
韦胜健 <martsforever@aliyun.com>
2021-01-18 21:50:16 +0800

    date: 月份基本选择功能

--- src/packages/date/panel/date-panel-month.tsx
+++ src/packages/date/panel/date-panel-month.tsx
@@ -1,7 +1,10 @@
 import {designComponent} from "../../../use/designComponent";
-import {DateItemData, DatePublicEmits, DatePublicProps, DateView, DefaultDateFormatString, SlideTransitionDirection} from "../date.utils";
-import {PropType, computed} from 'vue';
+import {DateItemData, DatePanelItemWrapper, DatePanelWrapper, DatePublicEmits, DatePublicProps, DateView, DefaultDateFormatString, SlideTransitionDirection} from "../date.utils";
+import {PropType, computed, Transition} from 'vue';
 import {useDate, UseDateJudgementView} from "../useDate";
+import {PlButton} from "../../button/button";
+import {StyleSize} from "../../../use/useStyle";
+import {PlDatePanelYear} from "./date-panel-year";
 
 export const PlDatePanelMonth = designComponent({
     name: 'pl-date-panel-month',
@@ -24,6 +27,7 @@ export const PlDatePanelMonth = designComponent({
             today,
             getStatus,
             handler,
+            viewModel,
         } = useDate({
             props,
             emit,
@@ -39,7 +43,7 @@ export const PlDatePanelMonth = designComponent({
              */
             prevYear() {
                 state.slide = SlideTransitionDirection.prev
-                setSelectDate(today.useYear(today.year - 1))
+                setSelectDate(state.selectDate.useYear(state.selectDate.year - 1))
             },
             /**
              * 切换到下一年
@@ -47,8 +51,8 @@ export const PlDatePanelMonth = designComponent({
              * @date    2020/4/15 11:12
              */
             nextYear() {
-                state.slide = SlideTransitionDirection.prev
-                setSelectDate(today.useYear(today.year - 1))
+                state.slide = SlideTransitionDirection.next
+                setSelectDate(state.selectDate.useYear(state.selectDate.year + 1))
             },
         }
 
@@ -56,22 +60,68 @@ export const PlDatePanelMonth = designComponent({
             const {selectDate} = state
             let list: DateItemData[] = [];
             [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].forEach(i => {
-                /*today.useYMD(today);
-                tempPd.setYear(selectDate.year!)
-                tempPd.setMonthDate(i, 1)
-                const ipd = tempPd.copy()
-                const item = {
-                    label: months.value[i],
-                    ipd,
-                    ...getStatus(ipd),
-                }
-                list.push(item)*/
+                const pd = today.useYMD([selectDate.year, i, 1]);
+                const item = {label: months.value[i], pd, ...getStatus(pd)}
+                list.push(item)
             })
             return list
         })
 
+        const externalHandler = {
+            /**
+             * 处理年份面板选择年份变化动作
+             * @author  韦胜健
+             * @date    2020/4/15 11:14
+             */
+            onSelectYearChange(val?: string | string[]) {
+                viewModel.value = DateView.month
+                setSelectDate(state.selectDate.useYear(val as any as number))
+            },
+        }
+
         return {
-            render: () => 11
+            render: () => {
+
+                const Month = DatePanelWrapper({
+                    left: <PlButton icon="el-icon-d-arrow-left" mode="text" size={StyleSize.mini} onClick={methods.prevYear}/>,
+                    center: <span onClick={() => viewModel.value = DateView.year}>{state.selectDate.year}</span>,
+                    right: <PlButton icon="el-icon-d-arrow-right" mode="text" size={StyleSize.mini} onClick={methods.nextYear}/>,
+                    content: (
+                        <Transition name={`pl-transition-slide-${state.slide}`}>
+                            <ul {...{
+                                class: 'pl-date-base-panel-month-list',
+                                key: state.selectDate.year!,
+                                direction: 'vertical'
+                            }}>
+                                {monthList.value.map(item => (DatePanelItemWrapper({
+                                    item,
+                                    onClick: handler.onClick,
+                                    onMouseenter: handler.onMouseenter,
+                                    Node: <li class="pl-date-base-panel-month-item" key={item.label}/>,
+                                })))}
+                            </ul>
+                        </Transition>
+                    )
+                }) as any
+
+                const Wrapper = (
+                    <div {...{
+                        class: 'pl-date-base-panel pl-date-base-panel-month-wrapper',
+                        direction: props.direction,
+                    }}>
+                        <Transition name={`pl-transition-slide-${viewModel.value === DateView.year ? 'prev' : 'next'}`}>
+                            {viewModel.value === DateView.month ? <Month {...{class: 'pl-date-base-panel-month', direction: 'horizontal'}}/> : (
+                                <PlDatePanelYear
+                                    modelValue={String(state.selectDate.year)}
+                                    onUpdateModelValue={externalHandler.onSelectYearChange}
+                                    direction="horizontal"/>
+                            )}
+                        </Transition>
+                    </div>
+                ) as any
+
+                return <Wrapper/>
+            }
         }
     },
 })
