95b50a410016f4a0fd01f730e02c893026d9a9e4
韦胜健 <martsforever@aliyun.com>
2021-01-20 10:42:42 +0800

    date: 准备测试 date组件

--- src/packages/date/date.tsx
+++ src/packages/date/date.tsx
@@ -1,16 +1,259 @@
 import {designComponent} from "../../use/designComponent";
 import './date.scss'
+import {DateEmitRangeType, DatePublicEmits, DatePublicProps, DefaultDateFormatString} from "./date.utils";
+import {EditProps} from "../../use/useEdit";
+import {StyleProps} from "../../use/useStyle";
+import {useModel} from "../../use/useModel";
+import {DatePanel} from "./panel/date-panel";
+import {delay} from "plain-utils/utils/delay";
+import {useEditPopperAgent} from "../popper/edit/useEditPopperAgent";
+import {DateServiceGetter} from "./date.service";
+import {useDateTime} from "../date-time-input/useDateTime";
+import {computed, PropType} from 'vue';
+import {PDate, plainDate} from "./plainDate";
+import {PlInput} from "../input/input";
+import {PlInputInnerTags} from "../input/input-inner-tags";
+import {PlIcon} from "../icon/icon";
+import {PlDateTimeInput} from "../date-time-input/date-time-input";
 
 export const PlDate = designComponent({
     name: 'pl-date',
-    props: {},
-    setup() {
+    props: {
+        ...StyleProps,
+        ...EditProps,
+        ...DatePublicProps,
+        panel: {type: String as PropType<DatePanel>, default: DatePanel.date},
+        collapseTags: {type: Boolean, default: true},
+    },
+    emits: {
+        ...DatePublicEmits,
+        onBlur: (e: Event) => true,
+        onFocus: (e: Event) => true,
+    },
+    setup({props, event: {emit}}) {
+
+        const model = useModel(() => props.modelValue as any, emit.onUpdateModelValue)
+        const startModel = useModel(() => props.start, emit.onUpdateStart)
+        const endModel = useModel(() => props.end, emit.onUpdateEnd)
+
+        const serviceHandler = {
+            onChange: (val: string | undefined, type: DateEmitRangeType) => {
+                if (!props.range) {
+                    model.value = val
+                } else {
+                    if (type === 'start') {
+                        startModel.value = val
+                    } else {
+                        endModel.value = val
+                        if (props.panel !== DatePanel.date || !props.datetime) {
+                            agentState.methods.hide()
+                        }
+                    }
+                }
+            },
+            onMousedown: async (e: MouseEvent, type: DateEmitRangeType) => {
+                agentState.state.focusCounter++
+                await delay(0)
+                if (props.multiple) {
+                    refs.plInput!.methods.focus()
+                } else if (!props.range) {
+                    refs.valueInput!.methods.focus()
+                } else {
+                    if (type === 'start') {
+                        refs.startInput!.methods.focus()
+                    } else {
+                        refs.endInput!.methods.focus()
+                    }
+                }
+            },
+        }
+
+        const agentState = useEditPopperAgent({
+            event: {emit},
+            serviceGetter: DateServiceGetter,
+            option: {
+                reference: () => refs.plInput as any,
+                renderAttrs: () => ({
+                    ...(Object.keys(DatePublicProps).reduce((ret: any, key) => {
+                        ret[key] = (props as any)[key]
+                        return ret
+                    }, {})),
+                    panel: props.panel,
+                    modelValue: model.value,
+                    start: startModel.value,
+                    end: endModel.value,
+                    ...serviceHandler,
+                })
+            },
+        })
+
+        const {
+            refs,
+            handler,
+            inputValue,
+        } = useDateTime({
+            value: model,
+            start: startModel,
+            end: endModel,
+            props,
+            agentState,
+            emit,
+        })
+
+        const format = computed(() => ({
+            displayFormat: props.displayFormat || DefaultDateFormatString[props.panel],
+            valueFormat: props.valueFormat || DefaultDateFormatString[props.panel],
+        }))
+
+        const today = plainDate.today(format.value.displayFormat, format.value.valueFormat)
+
+        const formatData = computed(() => {
+            let vpd: PDate | null = null;
+            let vpds: PDate[] | null = null;
+            let spd: PDate | null = null;
+            let epd: PDate | null = null;
+            if (props.multiple) {
+                let value = model.value as string[] | undefined
+                if (!!value) {
+                    vpds = value.map(item => plainDate(item, format.value))
+                }
+            } else {
+                if (!props.range) {
+                    const value = model.value as string | undefined
+                    if (!!value) vpd = plainDate(value, format.value)
+                } else {
+                    if (!!startModel.value) spd = plainDate(startModel.value, format.value)
+                    if (!!endModel.value) epd = plainDate(endModel.value, format.value)
+                }
+            }
+            return {vpd, vpds, spd, epd}
+        })
+
+        const maxmin = computed(() => {
+            return {
+                max: !props.max ? null : plainDate(props.max, format.value),
+                min: !props.min ? null : plainDate(props.min, format.value),
+            }
+        })
+
+        const inputAttrs = computed(() => {
+            const isDates = props.multiple
+            return {
+                class: [
+                    'pl-date',
+                    {
+                        'pl-input-custom': !isDates,
+                        'pl-input-tags': isDates,
+                    },
+                ],
+                modelValue: Array.isArray(inputValue.value) ? inputValue.value.join('') : inputValue.value,
+                suffixIcon: 'el-icon-date',
+                clearIcon: true,
+                isFocus: agentState.state.focusCounter > 0,
+                width: isDates ? undefined : null,
+                inputInnerTabindex: isDates ? 0 : null,
+                clearHandler: handler.clearHandler,
+
+                ...agentState.inputHandler,
+                onClickInput: handler.clickInput,
+            }
+        })
+
+        /*---------------------------------------custom handler-------------------------------------------*/
+
+        const customHandler = {
+            onInputChange(val: string | undefined, type: 'value' | 'start' | 'end') {
+                const {spd, epd} = formatData.value
+                let jdView = props.datetime ? 'YMDHms' as 'YMDHms' : 'YMD' as 'YMD'
+                /*没有值的话，如果是单值则请控值，否则什么也不做*/
+                if (!val) {
+                    if (type === 'value') {model.value = undefined}
+                    return
+                }
+                let pd = today.useDisplay(val)
+                /*输入值格式不正确，，什么是也不做*/
+                if (!pd.getDayJs().isValid()) {return;}
+                /*检查最大最小值*/
+                const {max, min} = maxmin.value
+                if (!!max && max[jdView] < pd[jdView]) pd = max
+                if (!!min && min[jdView] > pd[jdView]) pd = min
+                switch (type) {
+                    case 'value':
+                        model.value = pd.getValue()
+                        break
+                    case 'start':
+                        startModel.value = pd.getDisplay()
+                        if (!epd || (pd[jdView] > epd[jdView])) {endModel.value = startModel.value}
+                        break
+                    case 'end':
+                        endModel.value = pd.getDisplay()
+                        if (!spd || (pd[jdView] < spd[jdView])) {startModel.value = endModel.value}
+                        break
+                }
+            },
+            onClickItemCloseIcon(index: number, e: MouseEvent) {
+                e.stopPropagation()
+                e.preventDefault()
+                if (!agentState.editComputed.value.editable) {
+                    return
+                }
+                const value = model.value as string[]
+                value.splice(index, 1)
+                model.value = [...value]
+            },
+        }
+
         return {
-            render: () => (
-                <div>
-                    pl-date
-                </div>
-            )
+            render: () => {
+                return (
+                    <PlInput ref="plInput"{...inputAttrs.value as any}>
+                        <div {...{class: 'pl-input-custom-inner', range: props.range}}>
+                            {props.multiple && (<PlInputInnerTags
+                                data={formatData.value.vpds || []}
+                                collapseTags={props.collapseTags}
+                                v-slots={{
+                                    default: ({item, index}: { item: PDate, index: number }) => (<>
+                                        <span>{item.getDisplay()}</span>
+                                        <PlIcon icon="el-icon-close" {...{onClick: (e: MouseEvent) => customHandler.onClickItemCloseIcon(index, e)}}/>
+                                    </>)
+                                }}
+                            />)}
+
+                            {!props.multiple && (
+                                !props.range ? (<PlDateTimeInput
+                                        modelValue={formatData.value.vpd ? formatData.value.vpd.getDisplay() : undefined}
+                                        ref="valueInput"
+                                        {...{onChange: (val: string) => customHandler.onInputChange(val, 'value')}}
+                                        displayFormat={format.value.displayFormat}
+                                        onFocus={handler.customInputFocus}
+                                        onBlur={handler.customInputBlur}
+                                    />) :
+                                    <>
+                                        <PlDateTimeInput
+                                            width="100"
+                                            modelValue={formatData.value.spd ? formatData.value.spd.getDisplay() : undefined}
+                                            ref="startInput"
+                                            displayFormat={format.value.displayFormat}
+                                            {...{onChange: (val: string) => customHandler.onInputChange(val, 'start')}}
+                                            onFocus={handler.customInputFocus}
+                                            onBlur={handler.customInputBlur}
+                                        />
+                                        <span>~</span>
+                                        <PlDateTimeInput
+                                            width="100"
+                                            modelValue={formatData.value.epd ? formatData.value.epd.getDisplay() : undefined}
+                                            ref="endInput"
+                                            {...{onChange: (val: string) => customHandler.onInputChange(val, 'end')}}
+                                            displayFormat={format.value.displayFormat}
+                                            onFocus={handler.customInputFocus}
+                                            onBlur={handler.customInputBlur}
+                                        />
+                                    </>
+                            )}
+                        </div>
+                    </PlInput>
+                )
+            }
         }
     },
 })
--- story/pages/normal/date.vue
+++ story/pages/normal/date.vue
@@ -1,145 +1,143 @@
 <template>
     <div>
-        <h1> 重写Date组件</h1>
-        <demo-row title="年份">
-            <demo-row title="单选">
-                <pl-date-panel-year v-model="val[0]"/>
-                <pl-date-panel-year v-model="val[0]"/>
-                <div>value:{{val[0]}}</div>
+        <demo-row title="基础面板">
+            <demo-row title="年份">
+                <demo-row title="单选">
+                    <pl-date-panel-year v-model="val[0]"/>
+                    <pl-date-panel-year v-model="val[0]"/>
+                    <div>value:{{val[0]}}</div>
+                </demo-row>
+                <demo-row title="多选">
+                    <pl-date-panel-year v-model="val[1]" multiple/>
+                    <pl-date-panel-year v-model="val[1]" multiple/>
+                    <div>value:{{val[1]}}</div>
+                </demo-row>
+                <demo-row title="范围选择">
+                    <pl-date-panel-year v-model:start="val[2]" v-model:end="val[3]" range/>
+                    <pl-date-panel-year v-model:start="val[2]" v-model:end="val[3]" range/>
+                    <div>start:{{val[2]}}</div>
+                    <div>end:{{val[3]}}</div>
+                </demo-row>
+                <demo-row title="最大最小值">
+                    <pl-date-panel-year v-model:start="val[2]" v-model:end="val[3]" range max="2050" min="2021"/>
+                    <pl-date-panel-year v-model:start="val[2]" v-model:end="val[3]" range/>
+                    <div>max:2050</div>
+                    <div>min:2021</div>
+                    <div>start:{{val[2]}}</div>
+                    <div>end:{{val[3]}}</div>
+                </demo-row>
             </demo-row>
-            <demo-row title="多选">
-                <pl-date-panel-year v-model="val[1]" multiple/>
-                <pl-date-panel-year v-model="val[1]" multiple/>
-                <div>value:{{val[1]}}</div>
+            <demo-row title="年月">
+                <demo-row title="单选">
+                    <pl-date-panel-month v-model="val[4]"/>
+                    <pl-date-panel-month v-model="val[4]"/>
+                    <div>{{val[4]}}</div>
+                </demo-row>
+                <demo-row title="多选">
+                    <pl-date-panel-month v-model="val[5]" multiple/>
+                    <pl-date-panel-month v-model="val[5]" multiple/>
+                    <div>{{val[5]}}</div>
+                </demo-row>
+                <demo-row title="范围选择">
+                    <pl-date-panel-month v-model:start="val[6]" v-model:end="val[7]" range/>
+                    <pl-date-panel-month v-model:start="val[6]" v-model:end="val[7]" range/>
+                    <div>start:{{val[6]}}</div>
+                    <div>end:{{val[7]}}</div>
+                </demo-row>
+                <demo-row title="最大最小值">
+                    <pl-date-panel-month v-model:start="val[6]" v-model:end="val[7]" range max="2050-08" min="2021-03"/>
+                    <pl-date-panel-month v-model:start="val[6]" v-model:end="val[7]" range/>
+                    <div>max:2050-08</div>
+                    <div>min:2021-03</div>
+                    <div>start:{{val[6]}}</div>
+                    <div>end:{{val[7]}}</div>
+                </demo-row>
             </demo-row>
-            <demo-row title="范围选择">
-                <pl-date-panel-year v-model:start="val[2]" v-model:end="val[3]" range/>
-                <pl-date-panel-year v-model:start="val[2]" v-model:end="val[3]" range/>
-                <div>start:{{val[2]}}</div>
-                <div>end:{{val[3]}}</div>
+            <demo-row title="日期">
+                <demo-row title="单选">
+                    <pl-date-panel-date v-model="val[8]"/>
+                    <pl-date-panel-date v-model="val[8]"/>
+                    <div>value:{{val[8]}}</div>
+                </demo-row>
+                <demo-row title="多选">
+                    <pl-date-panel-date v-model="val[9]" multiple/>
+                    <pl-date-panel-date v-model="val[9]" multiple/>
+                    <div>value:{{val[9]}}</div>
+                </demo-row>
+                <demo-row title="日期时间">
+                    <pl-date-panel-date v-model="val[10]" datetime defaultTime="08:30:00"/>
+                    <pl-date-panel-date v-model="val[10]" datetime/>
+                    <div>value:{{val[10]}}</div>
+                </demo-row>
+                <demo-row title="最大最小值">
+                    <pl-date-panel-date v-model="val[11]" datetime defaultTime="08:30:00" max="2021-05-05 12:00:00" min="2019-05-05 08:30:15"/>
+                    <pl-date-panel-date v-model="val[11]" datetime :firstWeekDay="0"/>
+                    <div>max:2021-05-05 12:00:00</div>
+                    <div>min:2019-05-05 08:30:15</div>
+                    <div>value:{{val[11]}}</div>
+                </demo-row>
             </demo-row>
-            <demo-row title="最大最小值">
-                <pl-date-panel-year v-model:start="val[2]" v-model:end="val[3]" range max="2050" min="2021"/>
-                <pl-date-panel-year v-model:start="val[2]" v-model:end="val[3]" range/>
-                <div>max:2050</div>
-                <div>min:2021</div>
-                <div>start:{{val[2]}}</div>
-                <div>end:{{val[3]}}</div>
+            <demo-row title="日期范围">
+                <demo-row title="基本选择">
+                    <pl-date-panel-range v-model:start="val[12]" v-model:end="val[13]"/>
+                    <pl-date-panel-range v-model:start="val[12]" v-model:end="val[13]"/>
+                    <div>start:{{val[12]}}</div>
+                    <div>end:{{val[13]}}</div>
+                </demo-row>
+                <demo-row title="最大最小值">
+                    <pl-date-panel-range v-model:start="val[14]"
+                                         v-model:end="val[15]"
+                                         datetime
+                                         defaultStartTime="08:30:00"
+                                         defaultEndTime="20:30:30"
+                                         max="2021-05-05 12:00:00"
+                                         min="2019-05-05 08:30:15"/>
+                    <pl-date-panel-range v-model:start="val[14]" v-model:end="val[15]" datetime/>
+                    <div>max:2021-05-05 12:00:00</div>
+                    <div>min:2019-05-05 08:30:15</div>
+                    <div>start:{{val[14]}}</div>
+                    <div>end:{{val[15]}}</div>
+                </demo-row>
             </demo-row>
-        </demo-row>
-        <demo-row title="年月">
-            <demo-row title="单选">
-                <pl-date-panel-month v-model="val[4]"/>
-                <pl-date-panel-month v-model="val[4]"/>
-                <div>{{val[4]}}</div>
+            <demo-row title="周">
+                <demo-row title="单选">
+                    <pl-date-panel-week v-model="val[16]"/>
+                    <pl-date-panel-week v-model="val[16]" :firstWeekDay="0"/>
+                    <div>value:{{val[16]}}</div>
+                </demo-row>
+                <demo-row title="多选">
+                    <pl-date-panel-week v-model="val[17]" multiple/>
+                    <pl-date-panel-week v-model="val[17]" multiple/>
+                    <div>value:{{val[17]}}</div>
+                </demo-row>
+                <demo-row title="范围选择">
+                    <pl-date-panel-week v-model:start="val[18]" v-model:end="val[19]" range max="2021-05-05" min="2019-05-05"/>
+                    <pl-date-panel-week v-model:start="val[18]" v-model:end="val[19]" range/>
+                    <div>max:2021-05-05</div>
+                    <div>min:2019-05-05</div>
+                    <div>start:{{val[18]}}</div>
+                    <div>end:{{val[19]}}</div>
+                </demo-row>
             </demo-row>
-            <demo-row title="多选">
-                <pl-date-panel-month v-model="val[5]" multiple/>
-                <pl-date-panel-month v-model="val[5]" multiple/>
-                <div>{{val[5]}}</div>
-            </demo-row>
-            <demo-row title="范围选择">
-                <pl-date-panel-month v-model:start="val[6]" v-model:end="val[7]" range/>
-                <pl-date-panel-month v-model:start="val[6]" v-model:end="val[7]" range/>
-                <div>start:{{val[6]}}</div>
-                <div>end:{{val[7]}}</div>
-            </demo-row>
-            <demo-row title="最大最小值">
-                <pl-date-panel-month v-model:start="val[6]" v-model:end="val[7]" range max="2050-08" min="2021-03"/>
-                <pl-date-panel-month v-model:start="val[6]" v-model:end="val[7]" range/>
-                <div>max:2050-08</div>
-                <div>min:2021-03</div>
-                <div>start:{{val[6]}}</div>
-                <div>end:{{val[7]}}</div>
-            </demo-row>
-        </demo-row>
-        <demo-row title="日期">
-            <demo-row title="单选">
-                <pl-date-panel-date v-model="val[8]"/>
-                <pl-date-panel-date v-model="val[8]"/>
-                <div>value:{{val[8]}}</div>
-            </demo-row>
-            <demo-row title="多选">
-                <pl-date-panel-date v-model="val[9]" multiple/>
-                <pl-date-panel-date v-model="val[9]" multiple/>
-                <div>value:{{val[9]}}</div>
-            </demo-row>
-            <demo-row title="日期时间">
-                <pl-date-panel-date v-model="val[10]" datetime defaultTime="08:30:00"/>
-                <pl-date-panel-date v-model="val[10]" datetime/>
-                <div>value:{{val[10]}}</div>
-            </demo-row>
-            <demo-row title="最大最小值">
-                <pl-date-panel-date v-model="val[11]" datetime defaultTime="08:30:00" max="2021-05-05 12:00:00" min="2019-05-05 08:30:15"/>
-                <pl-date-panel-date v-model="val[11]" datetime :firstWeekDay="0"/>
-                <div>max:2021-05-05 12:00:00</div>
-                <div>min:2019-05-05 08:30:15</div>
-                <div>value:{{val[11]}}</div>
-            </demo-row>
-        </demo-row>
-
-        <demo-row title="日期范围">
-            <demo-row title="基本选择">
-                <pl-date-panel-range v-model:start="val[12]" v-model:end="val[13]"/>
-                <pl-date-panel-range v-model:start="val[12]" v-model:end="val[13]"/>
-                <div>start:{{val[12]}}</div>
-                <div>end:{{val[13]}}</div>
-            </demo-row>
-            <demo-row title="最大最小值">
-                <pl-date-panel-range v-model:start="val[14]"
-                                     v-model:end="val[15]"
-                                     datetime
-                                     defaultStartTime="08:30:00"
-                                     defaultEndTime="20:30:30"
-                                     max="2021-05-05 12:00:00"
-                                     min="2019-05-05 08:30:15"/>
-                <pl-date-panel-range v-model:start="val[14]" v-model:end="val[15]" datetime/>
-                <div>max:2021-05-05 12:00:00</div>
-                <div>min:2019-05-05 08:30:15</div>
-                <div>start:{{val[14]}}</div>
-                <div>end:{{val[15]}}</div>
-            </demo-row>
-        </demo-row>
-
-        <demo-row title="周">
-            <demo-row title="单选">
-                <pl-date-panel-week v-model="val[16]"/>
-                <pl-date-panel-week v-model="val[16]" :firstWeekDay="0"/>
-                <div>value:{{val[16]}}</div>
-            </demo-row>
-            <demo-row title="多选">
-                <pl-date-panel-week v-model="val[17]" multiple/>
-                <pl-date-panel-week v-model="val[17]" multiple/>
-                <div>value:{{val[17]}}</div>
-            </demo-row>
-            <demo-row title="范围选择">
-                <pl-date-panel-week v-model:start="val[18]" v-model:end="val[19]" range max="2021-05-05" min="2019-05-05"/>
-                <pl-date-panel-week v-model:start="val[18]" v-model:end="val[19]" range/>
-                <div>max:2021-05-05</div>
-                <div>min:2019-05-05</div>
-                <div>start:{{val[18]}}</div>
-                <div>end:{{val[19]}}</div>
-            </demo-row>
-        </demo-row>
-
-        <demo-row title="季度">
-            <demo-row title="单选">
-                <pl-date-panel-quarter v-model="val[20]"/>
-                <pl-date-panel-quarter v-model="val[20]"/>
-                <div>value:{{val[20]}}</div>
-            </demo-row>
-            <demo-row title="多选">
-                <pl-date-panel-quarter v-model="val[21]" multiple/>
-                <pl-date-panel-quarter v-model="val[21]" multiple/>
-                <div>value:{{val[21]}}</div>
-            </demo-row>
-            <demo-row title="范围选择">
-                <pl-date-panel-quarter v-model:start="val[22]" v-model:end="val[23]" range max="2021-05" min="2019-05"/>
-                <pl-date-panel-quarter v-model:start="val[22]" v-model:end="val[23]" range/>
-                <div>max:2021-05</div>
-                <div>min:2019-05</div>
-                <div>start:{{val[22]}}</div>
-                <div>end:{{val[23]}}</div>
+            <demo-row title="季度">
+                <demo-row title="单选">
+                    <pl-date-panel-quarter v-model="val[20]"/>
+                    <pl-date-panel-quarter v-model="val[20]"/>
+                    <div>value:{{val[20]}}</div>
+                </demo-row>
+                <demo-row title="多选">
+                    <pl-date-panel-quarter v-model="val[21]" multiple/>
+                    <pl-date-panel-quarter v-model="val[21]" multiple/>
+                    <div>value:{{val[21]}}</div>
+                </demo-row>
+                <demo-row title="范围选择">
+                    <pl-date-panel-quarter v-model:start="val[22]" v-model:end="val[23]" range max="2021-05" min="2019-05"/>
+                    <pl-date-panel-quarter v-model:start="val[22]" v-model:end="val[23]" range/>
+                    <div>max:2021-05</div>
+                    <div>min:2019-05</div>
+                    <div>start:{{val[22]}}</div>
+                    <div>end:{{val[23]}}</div>
+                </demo-row>
             </demo-row>
         </demo-row>
 
