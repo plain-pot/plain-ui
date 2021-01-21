b0daf19e76c83ce88392d53afdf8d4a16a21d97b
韦胜健 <martsforever@aliyun.com>
2021-01-21 17:45:46 +0800

    dialog service: 第一次打开的时候，动画不正常

--- src/packages/image/service/index.tsx
+++ src/packages/image/service/index.tsx
@@ -7,6 +7,8 @@ import {getServiceWithoutContext} from "../../../utils/getServiceWithoutContext"
 import {imageCompress} from "./image.service.utils";
 import {PlIcon} from "../../icon/icon";
 import {TooltipDirective} from "../../tooltip/tooltip-directive";
+import {useRefs} from "../../../use/useRefs";
+import {useStyles} from "../../../use/useStyles";
 
 interface ImageServicePreviewOption {
     urls: string[],
@@ -15,17 +17,57 @@ interface ImageServicePreviewOption {
 const Service = createDefaultService({
     name: 'pl-image-preview-service',
     setup: (option: ImageServicePreviewOption) => {
+
+        const step = {
+            scale: 0.2,
+            rotate: 90,
+        }
+
         const isShow = ref(false)
         const state = reactive({
             current: 0,                                 // 当前显示的图片索引
             option,
+            adjust: {
+                scale: null as null | number,           // 当前图片缩放大小
+                top: null as null | number,             // 当前图片拖拽top距离
+                left: null as null | number,            // 当前图片拖拽left距离
+                rotate: null as null | number,          // 当前图片旋转距离
+            },
+        })
+
+        const imgStyles = useStyles(style => {
+            const {scale, top, left, rotate} = state.adjust
+            let transform: string[] = []
+            if (!!scale) {
+                transform.push(`scale(${scale})`)
+            }
+            if (!!rotate) {
+                transform.push(`rotate(${rotate}deg)`)
+            }
+            if (transform.length > 0) {
+                style.transform = transform.join(',')
+            }
+            if (!!top) {
+                style.top = `${top}px`
+            }
+            if (!!left) {
+                style.left = `${left}px`
+            }
         })
 
         const show = () => {
             state.current = 0
+            resetAdjust()
             isShow.value = true
         }
         const hide = () => isShow.value = false
+
+        const resetAdjust = () => state.adjust = {
+            scale: null as null | number,           // 当前图片缩放大小
+            top: null as null | number,             // 当前图片拖拽top距离
+            left: null as null | number,            // 当前图片拖拽left距离
+            rotate: null as null | number,          // 当前图片旋转距离
+        }
 
         const service = async (option: ImageServicePreviewOption) => {
             state.option = option
@@ -45,10 +87,18 @@ const Service = createDefaultService({
             {
                 label: '放大',
                 icon: 'el-icon-zoom-in',
+                onClick: () => {
+                    let {scale} = state.adjust
+                    state.adjust.scale = (scale == null ? 1 : scale) + step.scale
+                }
             },
             {
                 label: '缩小',
                 icon: 'el-icon-zoom-out',
+                onClick: () => {
+                    let {scale} = state.adjust
+                    state.adjust.scale = (scale == null ? 1 : scale) - step.scale
+                }
             },
             {
                 label: '关闭',
@@ -64,6 +114,51 @@ const Service = createDefaultService({
             }
         ])
 
+        const dragImg = (() => {
+            let freezeState = {
+                startTop: 0,
+                startLeft: 0,
+                startX: 0,
+                startY: 0,
+            }
+            const mousedown = (e: MouseEvent) => {
+                freezeState = {
+                    startTop: state.adjust.top || 0,
+                    startLeft: state.adjust.left || 0,
+                    startX: e.clientX,
+                    startY: e.clientY,
+                }
+                document.addEventListener('mousemove', mousemove)
+                document.addEventListener('mouseup', mouseup)
+            }
+            const mousemove = (e: MouseEvent) => {
+                state.adjust.left = e.clientX - freezeState.startX + freezeState.startLeft
+                state.adjust.top = e.clientY - freezeState.startY + freezeState.startTop
+            }
+            const mouseup = (e: MouseEvent) => {
+                document.removeEventListener('mousemove', mousemove)
+                document.removeEventListener('mouseup', mouseup)
+            }
+            const dragstart = (e: DragEvent) => {
+                e.stopPropagation()
+                e.preventDefault()
+            }
+            return {
+                mousedown,
+                dragstart,
+                imgStyles,
+            }
+        })();
+
+        const handler = {
+            onClickImg: (e: MouseEvent) => {
+                e.stopPropagation()
+            },
+            onDblclickImg: () => {
+                hide()
+            },
+        }
+
         return {
             refer: {
                 isShow,
@@ -73,7 +168,20 @@ const Service = createDefaultService({
             render: () => (
                 <Transition name="pl-image-preview">
                     <div class={classes.value} v-show={isShow.value}>
-                        {!!state.option.urls[state.current] && <img class="pl-image-preview-service-img" src={state.option.urls[state.current]}/>}
+                        <div class="pl-image-preview-service-img-wrapper">
+                            {!!state.option.urls[state.current] && (
+                                <img
+                                    ref="img"
+                                    style={imgStyles.value}
+                                    class="pl-image-preview-service-img"
+                                    src={state.option.urls[state.current]}
+                                    onClick={handler.onClickImg}
+                                    onDblclick={handler.onDblclickImg}
+                                    onMousedown={dragImg.mousedown}
+                                    onDragstart={dragImg.dragstart}
+                                />
+                            )}
+                        </div>
                         <div class="pl-image-preview-service-button-group">
                             {buttons.map(btn => withDirectives(<div class="pl-image-preview-service-button" key={btn.label} onClick={() => !!btn.onClick && btn.onClick()}>
                                 <PlIcon icon={btn.icon}/>
