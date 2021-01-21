f91f63e7c64f714646355800a7fa9f91ee44c23f
韦胜健 <martsforever@aliyun.com>
2021-01-21 16:18:14 +0800

    dialog service: 完成压缩图片的功能

--- story/pages/service/image-service.vue
+++ story/pages/service/image-service.vue
@@ -4,6 +4,12 @@
             <pl-button @click="getImageFile()">获取单个文件</pl-button>
             <pl-button @click="getImageFile(true)">获取多个文件</pl-button>
         </demo-row>
+        <demo-row title="压缩图片">
+            <pl-button @click="testMeasureCompress">按照尺寸压缩</pl-button>
+            <pl-button @click="testSizeCompress">按照容量压缩</pl-button>
+            <img :src="compressData.before" v-if="compressData.before">
+            <img :src="compressData.after" v-if="compressData.after">
+        </demo-row>
     </div>
 </template>
 
@@ -13,11 +19,65 @@
 
     export default {
         name: "image-service",
+        data() {
+            return {
+                compressData: {
+                    before: null,
+                    after: null,
+                },
+            }
+        },
         methods: {
             async getImageFile(multiple) {
                 const data = await Image.$image.choose(multiple)
                 const files = Array.isArray(data) ? data : [data]
                 this.$notice(files.map(f => f.name).join(','))
+            },
+            async testMeasureCompress() {
+                const before = await this.$$file.readAsDataURL(await Image.$image.choose())
+                const config = await new Promise((resolve) => {
+                    const state = {
+                        maxHeight: null,
+                        maxWidth: null,
+                    }
+                    const dialog = this.$dialog({
+                        confirmButton: true,
+                        cancelButton: true,
+                        closeOnConfirm: false,
+                        render: () => (<div>
+                            <pl-form centerWhenSingleColumn={true}>
+                                <pl-form-item label="最大高度">
+                                    <pl-number v-model={state.maxHeight}/>
+                                </pl-form-item>
+                                <pl-form-item label="最大宽度">
+                                    <pl-number v-model={state.maxWidth}/>
+                                </pl-form-item>
+                            </pl-form>
+                        </div>),
+                        onConfirm: () => {
+                            if (!state.maxHeight && !state.maxWidth) {
+                                return this.$message.error('最大宽度与最大高度不能同时为空！')
+                            }
+                            resolve(state)
+                            dialog.close()
+                        },
+                    })
+                })
+                const after = await Image.$image.compress(before, config)
+                this.compressData = {before: null, after}
+            },
+            async testSizeCompress() {
+                const before = await this.$$file.readAsDataURL(await Image.$image.choose())
+                const maxSize = await new Promise((resolve) => {
+                    this.$dialog({
+                        title: '最大存储（单位MB）',
+                        editType: 'input',
+                        onConfirm: resolve,
+                        confirmButton: true,
+                    })
+                })
+                const after = await Image.$image.compress(before, {maxSize})
+                this.compressData = {before: null, after}
             },
         },
     }
