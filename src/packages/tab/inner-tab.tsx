d65116f29e6a7e37a8cef43b446e3c028e763db3
韦胜健 <martsforever@aliyun.com>
2021-01-31 20:08:10 +0800

    tabs: 基本的切换tab的功能

--- src/packages/tab/inner-tab.tsx
+++ src/packages/tab/inner-tab.tsx
@@ -1,5 +1,5 @@
 import {designComponent} from "../../use/designComponent";
-import {PropType} from 'vue';
+import {PropType, reactive, nextTick, watch, Transition} from 'vue';
 import {PlTabComponent} from "./tab";
 
 export const PlInnerTab = designComponent({
@@ -8,11 +8,44 @@ export const PlInnerTab = designComponent({
         active: {type: Boolean},
     },
     setup({props}) {
+
+        const state = reactive({
+            show: false,
+            init: props.item.props.init,
+        })
+
+        const methods = {
+            show: async () => {
+                if (state.show) return
+                else {
+                    if (!state.init) {
+                        state.init = true
+                        await nextTick()
+                    }
+                    state.show = true
+                }
+            },
+            hide: async () => {
+                if (!state.show) return
+                else {
+                    state.show = false
+                    if (props.item!.props.destroyOnHide) {
+                        await nextTick()
+                        state.init = false
+                    }
+                }
+            }
+        }
+
+        watch(() => props.active, async (val) => {val ? await methods.show() : await methods.hide()}, {immediate: true})
+
         return {
             render: () => (
-                <div class="pl-inner-tab" v-show={props.active}>
-                    {props.item.slots.default()}
-                </div>
+                <Transition name="pl-transition-tab">
+                    <div class="pl-inner-tab" v-show={props.active}>
+                        {!!state.init && props.item.slots.default()}
+                    </div>
+                </Transition>
             )
         }
     },
