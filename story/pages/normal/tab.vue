481282542a4955a3fcac4fe0233ffff9700248f0
韦胜健 <martsforever@aliyun.com>
2021-01-31 12:46:50 +0800

    update: 准备实现 tab

---
+++ story/pages/normal/tab.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="demo-tab">
+        <demo-row title="基本用法">
+            demo-tab
+        </demo-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "demo-tab"
+    }
+</script>
+
+<style lang="scss">
+
+</style>
