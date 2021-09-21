3bfb1ff7744b0058769c7ad60caa3b3311c96998
韦胜健 <martsforever@aliyun.com>
2021-09-21 11:20:29 +0800

    refactor: button and button gorup

--- src/packages/PlButton/index.tsx
+++ src/packages/PlButton/index.tsx
@@ -1,20 +1,146 @@
-import {designComponent} from "plain-ui-composition";
-import {ButtonHTMLAttributes} from 'vue'
+import {computed, designComponent, PropType, reactive, useClasses, useNumber, useRefs, useStyles, watch} from 'plain-ui-composition'
+import './button.scss'
+import {DEFAULT_STATUS, StyleProps, useStyle} from "../../use/useStyle";
+import {throttle} from 'plain-utils/utils/throttle'
+import {unit} from 'plain-utils/string/unit'
+import {PlLoading} from "../PlLoading";
+import {PlIcon} from "../PlIcon";
+import {EditProps, useEdit} from "../../use/useEdit";
+import {ButtonModeProvider} from "../PlButtonGroup";
+import {useClickWave} from "../../directives/ClickWave";
+
+console.log('load button component')
 
 export const PlButton = designComponent({
-    inheritPropsType: {} as ButtonHTMLAttributes,
+    name: 'pl-button',
     props: {
-        label: {type: String},
-        status: {type: String},
-        mode: {type: String},
+        mode: {type: String},                                   // fill,stroke,text
+        label: {type: String},                                  // 按钮文本
+        width: {type: [String, Number]},                        // 按钮宽度
+        icon: {type: String},                                   // 按钮图标
+        active: {type: Boolean},                                // 按钮是否高亮
+        noPadding: {type: Boolean},                             // 按钮是否无边距
+        block: {type: Boolean},                                 // 块级元素
+        throttleClick: {type: [Boolean, Number]},               // click节流
+        asyncHandler: {type: Function as PropType<(e: MouseEvent) => void>},    // 异步处理函数，会自动开启关闭loading状态
+
+        ...EditProps,
+        ...StyleProps,
+
+        /*native*/
+        type: {type: String, default: 'button'},
+        nativeAttrs: {type: Object},
+    },
+    inheritPropsType: HTMLButtonElement,
+    emits: {
+        onClick: (e: MouseEvent) => true,
     },
     slots: ['default'],
-    setup({props, slots}) {
-        return () => (
-            <button>
-                {slots.default(props.label)}
-            </button>
+    setup({props, event: {emit}, slots}) {
+
+        const {refs, onRef} = useRefs({
+            el: HTMLButtonElement,
+        })
+        const {numberState} = useNumber(props, ['width'])
+
+        const {styleComputed} = useStyle({status: DEFAULT_STATUS})
+        const {editState, editComputed} = useEdit()
+        const buttonGroup = ButtonModeProvider.inject()
+        const otherComputed = computed(() => ({
+            mode: props.mode || (!!buttonGroup ? buttonGroup.value.mode : 'fill')
+            // mode: 'fill',
+        }))
+        const state = reactive({
+            handleClick: null as ((e: MouseEvent) => void) | null,
+            handleClickInner: async (e: MouseEvent) => {
+                e.stopPropagation()
+                if (!editComputed.value.editable) {
+                    return
+                }
+                if (!props.asyncHandler) {
+                    return emit.onClick(e)
+                }
+                editState.loading = true
+                try {
+                    await props.asyncHandler(e)
+                } catch (e) {
+                    console.error(e)
+                } finally {
+                    editState.loading = null
+                }
+            },
+        })
+
+        watch(
+            () => props.throttleClick,
+            (val) => {
+                if (!val) {
+                    return state.handleClick = state.handleClickInner
+                }
+                if (val === true) {
+                    val = 1000
+                }
+                state.handleClick = throttle(state.handleClickInner, val, {trailing: false})
+            },
+            {immediate: true}
         )
+
+        const classes = useClasses(() => ([
+            'pl-button',
+            'plain-click-node',
+
+            `pl-button-mode-${otherComputed.value.mode}`,
+            `pl-button-status-${styleComputed.value.status}`,
+            `pl-button-shape-${styleComputed.value.shape}`,
+            `pl-button-size-${styleComputed.value.size}`,
+
+            {
+                'pl-button-icon': !!props.icon,
+                'pl-button-active': !!props.active,
+                'pl-button-noPadding': !!props.noPadding,
+
+                'pl-button-loading': !!editComputed.value.loading,
+                'pl-button-has-icon': !!props.icon,
+                'pl-button-block': !!props.block,
+                'pl-button-disabled': !!editComputed.value.disabled,
+                'pl-button-icon-only': !!props.icon && !props.label,
+            },
+        ]))
+
+        const styles = useStyles(style => {
+            style.width = unit(numberState.width)
+        })
+
+        useClickWave({elGetter: () => refs.el, optionsGetter: () => ({size: 'large', disabled: !editComputed.value.editable}),})
+
+        return {
+            refer: {
+                refs,
+            },
+            render: () => {
+                return (
+                    <button
+                        ref={onRef.el}
+                        style={styles.value}
+                        class={classes.value}
+                        type={props.type as any}
+                        disabled={editComputed.value.disabled!}
+                        {...{
+                            ...(props.nativeAttrs || {}),
+                            onClick: state.handleClick!,
+                        }}
+                    >
+                        {!!editComputed.value.loading && <PlLoading type="gamma"/>}
+                        {
+                            slots.default(<>
+                                {(!!props.icon && !editComputed.value.loading) ? <PlIcon icon={props.icon}/> : null}
+                                {props.label ? <span>{props.label}</span> : null}
+                            </>)
+                        }
+                    </button>
+                )
+            }
+        }
     },
 })
 
--- src/packages/PlButtonGroup/index.tsx
+++ src/packages/PlButtonGroup/index.tsx
@@ -1,18 +1,65 @@
-import {designComponent} from "plain-ui-composition";
+import {computed, designComponent, inject, provide, useClasses, useRefs} from 'plain-ui-composition'
+import './button-group.scss'
+import {DEFAULT_STATUS, StyleMode, StyleProps, useStyle} from "../../use/useStyle";
+import {EditProps, useEdit} from "../../use/useEdit";
+
+interface ButtonModeProviderValue {
+    value: { mode: string }
+}
+
+export const BUTTON_GROUP_PROVIDER = '@@BUTTON_GROUP_PROVIDER'
+
+export const ButtonModeProvider = (() => {
+    return {
+        provide: (data: ButtonModeProviderValue) => {
+            provide(BUTTON_GROUP_PROVIDER, data)
+        },
+        inject: () => {
+            return inject(BUTTON_GROUP_PROVIDER, null) as ButtonModeProviderValue | null
+        }
+    }
+})()
 
 export const PlButtonGroup = designComponent({
+    name: 'pl-button-group',
     props: {
-        size: {type: String},
-        status: {type: String},
-        mode: {type: String},
+        mode: {type: String, default: StyleMode.fill},                  // fill,stroke,text
+        vertical: {type: Boolean},                                      // 是否为纵向按钮组
+
+        ...EditProps,
+        ...StyleProps,
     },
     slots: ['default'],
+    inheritPropsType: HTMLDivElement,
     setup({props, slots}) {
-        return () => (
-            <div>
-                {slots.default()}
-            </div>
-        )
+        const {refs, onRef} = useRefs({el: HTMLDivElement})
+        const {styleComputed} = useStyle({status: DEFAULT_STATUS})
+        const {editComputed} = useEdit()
+
+        const parentGroup = ButtonModeProvider.inject()
+        const otherComputed = computed(() => ({mode: !!parentGroup ? parentGroup.value.mode : props.mode}))
+        ButtonModeProvider.provide(otherComputed)
+
+        const classes = useClasses(() => ([
+            `pl-button-group`,
+            `pl-button-group-status-${styleComputed.value.status}`,
+            `pl-button-group-mode-${otherComputed.value.mode}`,
+            `pl-button-group-shape-${styleComputed.value.shape}`,
+            `pl-button-group-size-${styleComputed.value.size}`,
+            `pl-button-group-direction-${props.vertical ? 'vertical' : 'horizontal'}`,
+            {
+                'pl-button-group-disabled': !!editComputed.value.disabled,
+            },
+        ]))
+
+        return {
+            refer: {refs,},
+            render: () => (
+                <div class={classes.value} ref={onRef.el}>
+                    {slots.default()}
+                </div>
+            )
+        }
     },
 })
 
