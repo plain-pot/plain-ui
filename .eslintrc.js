8fa185c7513f8306aee42642f9b70e53a8bdbc44
韦胜健 <martsforever@aliyun.com>
2021-09-21 10:27:00 +0800

    refactor: adjust eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'off',
     '@typescript-eslint/no-var-requires': 'off',
     '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/explicit-module-boundary-types':'off',
     'prefer-const': 'off',
   }
 }
