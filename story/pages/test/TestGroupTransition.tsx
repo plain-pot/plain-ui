11b68c828bab694a0759777e439441a95a033fe5
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-09-28 16:57:22 +0800

    refactor: table

---
+++ story/pages/test/TestGroupTransition.tsx
@@ -0,0 +1,10 @@
+import './TestGroupTransition.scss'
+import {designPage} from "plain-design-composition";
+
+export const demo1 = designPage(() => {
+    return () => (
+        <div class="demo-btn">
+            this is button, <span>this is hello world</span>
+        </div>
+    )
+})
