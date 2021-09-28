9d2289025dc9a414ae58a84a5315c35dbf1c63db
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-09-28 18:17:13 +0800

    refactor: complete menu

--- story/app/menu.utils.ts
+++ story/app/menu.utils.ts
@@ -148,7 +148,7 @@ const MenuData: MenuGroup[] = [
             {name: 'Plc', title: '列组件', page: '/table/DemoTableColumns', complete: true},
             {name: 'Expand', title: '展开列', page: '/table/DemoTableExpand', complete: true},
             {name: 'Span', title: '合并单元格', page: '/table/DemoTableSpan', complete: true},
-            {name: 'Tree', title: '树形表格', page: '/table/DemoTableTree', complete: false},
+            {name: 'Tree', title: '树形表格', page: '/table/DemoTableTree', complete: true},
             {name: 'Row Draggable', title: '行拖拽排序', page: '/table/DemoTableRowDraggable', complete: true},
             {name: 'Col Draggable', title: '列拖拽排序', page: '/table/DemoTableColDraggable', complete: true},
             {name: 'Table class-style', title: '行列样式', page: '/table/DemoTableClassAndStyle', complete: true},
