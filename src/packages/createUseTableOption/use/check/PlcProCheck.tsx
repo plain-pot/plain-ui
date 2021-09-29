20d5933822472f71dab8c53217f2e82395c4ea84
å 韦胜韦胜健 <martsforever@aliyun.com>
2021-09-29 11:34:47 +0800

    refactor: plc pro check avoid refer conflict

--- src/packages/createUseTableOption/use/check/PlcProCheck.tsx
+++ src/packages/createUseTableOption/use/check/PlcProCheck.tsx
@@ -129,7 +129,6 @@ export default designComponent({
         return {
             refer: {
                 ...refer,
-                isCheckable,
                 isCheck,
                 toggle,
                 ...methods,
